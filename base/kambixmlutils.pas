36927c65d247d8c0551bf811e1e484c733a5c6b1
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-02-04 21:37:34 +0000

    + DOMGetOneChildElement, DOMGetTextData useful in "The Castle" to parse levels/index.xml
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/kambi_vrml_game_engine/trunk@1497 afe8dcc5-9094-451b-8773-5f495042a737

--- base/kambixmlutils.pas
+++ base/kambixmlutils.pas
@@ -36,6 +36,32 @@ function DOMGetSingleAttribute(const Element: TDOMElement;
 function DOMGetBooleanAttribute(const Element: TDOMElement;
   const AttrName: string; var Value: boolean): boolean;
 
+{ This returns the @italic(one and only) child element of this Element.
+  If given Element has none or more than one child elements,
+  returns @nil. This is handy for parsing XML in cases when you
+  know that given element must contain exactly one other element
+  in correct XML file. }
+function DOMGetOneChildElement(const Element: TDOMElement): TDOMElement;
+
+{ This returns the text data contained in this element.
+
+  This is suitable if an element is supposed to contain only some text.
+  It raises an error if an element contains any other element as child.
+
+  It concatenates all text data nodes that are direct children
+  of this element. So if there are no text data nodes, it returns
+  empty string without raising any error.
+
+  AFAIK it's uncommon but
+  possible to have here more than one text node. Normally, more than one
+  text nodes occur because they are separated by other child elements,
+  but we already eliminated this possibility (i.e. we raise error
+  in this case). Still, if you operated on DOM tree, e.g. deleted
+  some elements, or inserted some text nodes, then I think it's possible
+  that you will have more than one text node within this element.
+  So this procedure should still work OK in this case. }
+function DOMGetTextData(const Element: TDOMElement): string;
+
 implementation
 
 uses SysUtils, KambiUtils;
@@ -99,6 +125,53 @@ begin
       Value := false else
       Result := false;
   end;
+end;
+
+function DOMGetOneChildElement(const Element: TDOMElement): TDOMElement;
+var
+  Children: TDOMNodeList;
+  Node: TDOMNode;
+  I: Integer;
+begin
+  Result := nil;
+  Children := Element.ChildNodes;
+  try
+    for I := 0 to Integer(Children.Count) - 1 do
+    begin
+      Node := Children.Item[I];
+      if Node.NodeType = ELEMENT_NODE then
+      begin
+        if Result = nil then
+          Result := Node as TDOMElement else
+        begin
+          { More than one element in Children. }
+          Result := nil;
+          Exit;
+        end;
+      end;
+    end;
+  finally Children.Release end;
+end;
+
+function DOMGetTextData(const Element: TDOMElement): string;
+var
+  Children: TDOMNodeList;
+  Node: TDOMNode;
+  I: Integer;
+begin
+  Result := '';
+  Children := Element.ChildNodes;
+  try
+    for I := 0 to Integer(Children.Count) - 1 do
+    begin
+      Node := Children.Item[I];
+      case Node.NodeType of
+        TEXT_NODE: Result += (Node as TDOMText).Data;
+        ELEMENT_NODE: raise Exception.CreateFmt(
+          'Child elements not allowed within element <%s>', [Element.TagName]);
+      end;
+    end;
+  finally Children.Release end;
 end;
 
 end.
