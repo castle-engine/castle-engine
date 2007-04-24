923333e601c2aeca46754c9df92fd7ec5f17579c
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-04-24 02:29:04 +0000

    * shadows implementation is now switchable from the menu at runtime (instead of having to adjust $defines and recompile)
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/kambi_vrml_game_engine/trunk@1672 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels.gl/examples/shadow_volume_test/shadow_volume_test.dpr
+++ 3dmodels.gl/examples/shadow_volume_test/shadow_volume_test.dpr
@@ -20,14 +20,14 @@
 
 { A program to demonstrate and test shadows by shadow volumes algorithm.
 
-  Be aware that code of this program is a little messy at some places,
-  because one of the goals of this program is to experiment and test with various
-  variants of shadow volume making. The most practical version to be
-  used right now is to define USE_WRAP (requires OpenGL >= 1.4 or EXT_stencil_wrap
-  extension, available almost everywhere) and
-  USE_STENCIL_OP_SEPARATE (requires OpenGL >= 2.0).
-  If you have OpenGL < 2.0, define CULL_USING_OPENGL instead of
-  USE_STENCIL_OP_SEPARATE.
+  Code of this program is a little more complicated than the ccde of
+  a simple straightforward "shadows demo" would be, because one of the goals of
+  this program is to experiment and test various
+  variants of shadow volume making. A real program using shadows
+  should just use siStencilOpSeparate approach, eventually
+  (as a fallback, if siStencilOpSeparate is not available --- it requires
+  OpenGL >= 2.0) siGLCullFace2Passes. All other approaches are practically
+  useles, and implemented here only for testing purposes.
 
   This program displays two models:
   - static scene
@@ -62,30 +62,8 @@
 
 program shadow_volume_test;
 
-{ This requires only 1 stencil bit. But it's absolutely incorrect
-  method, and will work good only for the simplest shadow casters
-  (e.g. ./shadow_volume_test_extrasimple.sh --- one triangle). }
-{ $define XOR_TRICK}
-
 { This is highly adviced. Requires gl >= 1.4 or EXT_stencil_wrap extension }
 {$define USE_WRAP}
-
-{ This uses OpenGL to cull front/back faces.
-  Although OpenGL culling is for sure faster than culling done by our
-  code, on the other hand I have to pass the same set of faces two times.
-  While when I cull by my own code, in the second pass I already
-  detected which faces are back. So practically when culling with my own
-  code, I have 2 times less culling tests to do.
-
-  Note that glStencilOpSeparate (avail in OpenGL >= 2.0)
-  allows to combine these two advantages: only one pass will
-  be needed, and OpenGL can detect back/front faces. }
-{$define CULL_USING_OPENGL}
-
-{ This means that glStencilOpSeparate will be used.
-  Requires OpenGL >= 2.0, but allows only one pass, and OpenGL detects
-  front/back faces. So this is the most desirable and practical approach. }
-{$define USE_STENCIL_OP_SEPARATE}
 
 {$apptype CONSOLE}
 
@@ -95,6 +73,51 @@ uses GLWindow, GLW_Navigated, OpenGLh, KambiGLUtils, VRMLFlatSceneGL,
   KambiClassUtils, KambiFilesUtils, KambiStringUtils, VRMLCameraUtils,
   ShadowTests, GLWinMessages;
 
+type
+  { Shadow implementations. Roughly ordered from the worse to the best. }
+  TShadowsImplementation =
+  (
+    { No shadows. Now, *that's* really fast :) }
+    siNone,
+
+    { Use 1 pass rendering all shadow quads,
+      use 1 bit of stencil buffer, invert on hit.
+
+      This requires only 1 stencil bit. But it's absolutely incorrect
+      method, and will work good only for the simplest shadow casters
+      (e.g. ./shadow_volume_test_extrasimple.sh --- one triangle). }
+    siInvertTrick,
+
+    { Render front shadow quads incrementing stencil, then render
+      back shadow quads decrementing stencil. Use our own code
+      to detect which faces are front and which are back.
+
+      Note that this implementation always renders all edges (not only silhouette
+      edges), so it's very non-optimal for manifold scenes.
+      Besides, siGLCullFace2Passes (even when it renders all edges)
+      was found to be as fast, so this implementation is just useless
+      pratically --- use only for testing purposes. }
+    siEngineCullFace2Passes,
+
+    { Like siEngineCullFace2Passes, but
+      uses OpenGL to cull front/back faces.
+      Although OpenGL culling is for sure faster than culling done by our
+      code, on the other hand I have to pass the same set of faces two times.
+      While when I cull by my own code, in the second pass I already
+      detected which faces are back. So practically when culling with my own
+      code, I have 2 times less culling tests to do.
+
+      Note that glStencilOpSeparate (avail in OpenGL >= 2.0)
+      allows to combine these two advantages: only one pass will
+      be needed, and OpenGL can detect back/front faces. }
+    siGLCullFace2Passes,
+
+    { This means that glStencilOpSeparate will be used.
+      Requires OpenGL >= 2.0, but allows only one pass, and OpenGL detects
+      front/back faces. So this is the most desirable and practical approach. }
+    siStencilOpSeparate
+  );
+
 var
   Scene, ShadowCaster: TVRMLFlatSceneGL;
   ShadowCasterNav: TMatrixExaminer;
@@ -103,31 +126,12 @@ var
   ShowShadowQuads: boolean = false;
   IsRenderSilhouetteEdges: boolean = false;
 
+  ShadowsImplementation: TShadowsImplementation = siStencilOpSeparate;
+  ShadowsImplementationRadioGroup: TMenuItemRadioGroup;
+  ShadowsImplementationRadio:
+    array [TShadowsImplementation] of TMenuItemRadio;
+
 { glw callbacks ------------------------------------------------------------ }
-
-const
-  { Which stencil bits should be used by shadow volume algorithm ?
-
-    Not only *while rendering* shadow quads but also *after this rendering*
-    value in stencil buffer may be > 1 (so you really need more than 1 bit
-    to hold it in stencil buffer).
-
-    Why ? It's obvious that *while rendering* (e.g. right after rendering
-    all front quads) this value may be > 1. But when the point
-    is in the shadow because it's inside more than one shadow
-    (cast by 2 different shadow quads) then even *after rendering*
-    this point will have value > 1.
-
-    So it's important that this constant spans a couple of bits.
-    More precisely, it should be the maximum number of possibly overlapping
-    front shadow quads from any possible camera view.
-
-    If it's not enough, USE_WRAP will help a little --- but inevitably
-    it will show some artifacts at some places.
-    Run ./shadow_volume_test_parallels.sh to see such nasty case.
-
-    For XOR_TRICK it should always be 1 --- that's the point of XOR_TRICK. }
-  StencilShadowBits = {$ifdef XOR_TRICK} $1 {$else} $FF {$endif};
 
 procedure Draw(glwin: TGLWindow);
 
@@ -175,76 +179,135 @@ procedure Draw(glwin: TGLWindow);
     end;
   end;
 
-var
-  StencilOpIncrWrap, StencilOpDecrWrap: TGLenum;
+  { Rendering with hard shadows by SV algorithm. }
+  procedure RenderWithShadows;
+  var
+    StencilOpIncrWrap, StencilOpDecrWrap: TGLenum;
+    StencilShadowBits: TGLuint;
+  begin
+    StencilOpIncrWrap := {$ifdef USE_WRAP} GL_INCR_WRAP_EXT {$else} GL_INCR {$endif};
+    StencilOpDecrWrap := {$ifdef USE_WRAP} GL_DECR_WRAP_EXT {$else} GL_DECR {$endif};
+
+    if (ShadowsImplementation = siStencilOpSeparate) and
+       (glStencilOpSeparate = nil) then
+    begin
+      ShadowsImplementation := siGLCullFace2Passes;
+      ShadowsImplementationRadioGroup.Selected :=
+        ShadowsImplementationRadio[ShadowsImplementation];
+
+      MessageOK(Glwin, 'glStencilOpSeparate not available ' +
+        '(OpenGL version required is >= 2.0), falling back to ' +
+        'GLCullFace2Passes implementation', taMiddle);
+    end;
+
+    { Which stencil bits should be used by shadow volume algorithm ?
+
+      Not only *while rendering* shadow quads but also *after this rendering*
+      value in stencil buffer may be > 1 (so you really need more than 1 bit
+      to hold it in stencil buffer).
+
+      Why ? It's obvious that *while rendering* (e.g. right after rendering
+      all front quads) this value may be > 1. But when the point
+      is in the shadow because it's inside more than one shadow
+      (cast by 2 different shadow quads) then even *after rendering*
+      this point will have value > 1.
+
+      So it's important that this constant spans a couple of bits.
+      More precisely, it should be the maximum number of possibly overlapping
+      front shadow quads from any possible camera view.
+
+      If it's not enough, USE_WRAP will help a little --- but inevitably
+      it will show some artifacts at some places.
+      Run ./shadow_volume_test_parallels.sh to see such nasty case.
+
+      For siInvertTrick it should always be 1 ---
+      that's the point of siInvertTrick. }
+    if ShadowsImplementation <> siInvertTrick then
+      StencilShadowBits := $FF else
+      StencilShadowBits := 1;
+
+    glStencilMask(StencilShadowBits);
+
+    RenderEverything;
+    glEnable(GL_STENCIL_TEST);
+      { Note that stencil buffer is set to all 0 now. }
+
+      { Calculate shadows to the stencil buffer.
+        Don't write anything to depth or color buffers. }
+      glSetDepthAndColorWriteable(GL_FALSE);
+
+        glStencilFunc(GL_ALWAYS, 0, 0);
+        case ShadowsImplementation of
+          siStencilOpSeparate:
+            begin
+              glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_KEEP, StencilOpIncrWrap);
+              glStencilOpSeparate(GL_BACK , GL_KEEP, GL_KEEP, StencilOpDecrWrap);
+              RenderAllShadowQuads;
+            end;
+          siGLCullFace2Passes, siEngineCullFace2Passes:
+            begin
+              { For each fragment that passes depth-test, *increase* it's stencil
+                value by 1. Render front facing shadow quads. }
+              glStencilOp(GL_KEEP, GL_KEEP, StencilOpIncrWrap);
+              if ShadowsImplementation = siGLCullFace2Passes then
+              begin
+                glEnable(GL_CULL_FACE);
+                glCullFace(GL_BACK);
+                RenderAllShadowQuads;
+              end else
+                RenderFrontShadowQuads;
+
+              { For each fragment that passes depth-test, *decrease* it's stencil
+                value by 1. Render back facing shadow quads. }
+              glStencilOp(GL_KEEP, GL_KEEP, StencilOpDecrWrap);
+              if ShadowsImplementation = siGLCullFace2Passes then
+              begin
+                glCullFace(GL_FRONT);
+                RenderAllShadowQuads;
+                glDisable(GL_CULL_FACE);
+              end else
+                RenderBackShadowQuads;
+            end;
+          siInvertTrick:
+            begin
+              glStencilOp(GL_KEEP, GL_KEEP, GL_INVERT);
+              RenderAllShadowQuads;
+            end;
+          else raise Exception.Create('Unknown ShadowsImplementation value');
+        end;
+
+      glSetDepthAndColorWriteable(GL_TRUE);
+
+      { Now render everything once again, with lights turned on.
+        But render only things not in shadow. }
+      glClear(GL_DEPTH_BUFFER_BIT);
+      glPushAttrib(GL_LIGHTING_BIT);
+        LightSet.RenderLights;
+        { setup stencil : don't modify stencil, stencil test passes only for =0 }
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+        glStencilFunc(GL_EQUAL, 0, StencilShadowBits);
+
+        RenderEverything;
+      glPopAttrib;
+    glDisable(GL_STENCIL_TEST);
+  end;
+
 begin
   glClear(GL_DEPTH_BUFFER_BIT or GL_COLOR_BUFFER_BIT or GL_STENCIL_BUFFER_BIT);
   glLoadMatrix(Glw.Navigator.Matrix);
 
-  StencilOpIncrWrap := {$ifdef USE_WRAP} GL_INCR_WRAP_EXT {$else} GL_INCR {$endif};
-  StencilOpDecrWrap := {$ifdef USE_WRAP} GL_DECR_WRAP_EXT {$else} GL_DECR {$endif};
-
-  { Normal rendering, without shadows, would go now simply like
-      LightSet.RenderLights;
-      RenderEverything;
-    Rendering with hard shadows by SV algorithm below: }
-  RenderEverything;
-  glEnable(GL_STENCIL_TEST);
-    { Note that stencil buffer is set to all 0 now. }
-
-    { Calculate shadows to the stencil buffer.
-      Don't write anything to depth or color buffers. }
-    glSetDepthAndColorWriteable(GL_FALSE);
-      glStencilFunc(GL_ALWAYS, 0, 0);
-      {$ifdef USE_STENCIL_OP_SEPARATE}
-        glStencilOpSeparate(GL_FRONT, GL_KEEP, GL_KEEP, StencilOpIncrWrap);
-        glStencilOpSeparate(GL_BACK , GL_KEEP, GL_KEEP, StencilOpDecrWrap);
-        RenderAllShadowQuads;
-      {$else USE_STENCIL_OP_SEPARATE}
-        {$ifndef XOR_TRICK}
-
-          { For each fragment that passes depth-test, *increase* it's stencil
-            value by 1. Render front facing shadow quads. }
-          glStencilOp(GL_KEEP, GL_KEEP, StencilOpIncrWrap);
-          {$ifdef CULL_USING_OPENGL}
-          glEnable(GL_CULL_FACE);
-          glCullFace(GL_BACK);
-          RenderAllShadowQuads;
-          {$else}
-          RenderFrontShadowQuads;
-          {$endif}
-
-          { For each fragment that passes depth-test, *decrease* it's stencil
-            value by 1. Render back facing shadow quads. }
-          glStencilOp(GL_KEEP, GL_KEEP, StencilOpDecrWrap);
-          {$ifdef CULL_USING_OPENGL}
-          glCullFace(GL_FRONT);
-          RenderAllShadowQuads;
-          glDisable(GL_CULL_FACE);
-          {$else}
-          RenderBackShadowQuads;
-          {$endif}
-
-        {$else XOR_TRICK}
-          glStencilOp(GL_KEEP, GL_KEEP, StencilOpIncrWrap);
-          RenderAllShadowQuads;
-        {$endif XOR_TRICK}
-      {$endif USE_STENCIL_OP_SEPARATE}
-
-    glSetDepthAndColorWriteable(GL_TRUE);
-
-    { Now render everything once again, with lights turned on.
-      But render only things not in shadow. }
-    glClear(GL_DEPTH_BUFFER_BIT);
+  if ShadowsImplementation = siNone then
+  begin
+    { For only ShadowsImplementation = siNone, we could just render
+      LightSet, no need to push/pop GL_LIGHTING_BIT. But if user will
+      later switch ShadowsImplementation to something else, we can't
+      let lights affect it. }
     glPushAttrib(GL_LIGHTING_BIT);
       LightSet.RenderLights;
-      { setup stencil : don't modify stencil, stencil test passes only for =0 }
-      glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-      glStencilFunc(GL_EQUAL, 0, StencilShadowBits);
-
       RenderEverything;
     glPopAttrib;
-  glDisable(GL_STENCIL_TEST);
+  end else
+    RenderWithShadows;
 
   if ShowShadowQuads then
   begin
@@ -275,8 +338,6 @@ begin
   glEnable(GL_LIGHTING);
   glEnable(GL_DEPTH_TEST);
   glClearColor(0.1, 0.1, 0.1, 0.0);
-
-  glStencilMask(StencilShadowBits);
 
   { Tests:
   glLightModelv(GL_LIGHT_MODEL_AMBIENT, Vector4Single(0.6, 0.6, 0.6, 1)); }
@@ -351,7 +412,53 @@ begin
   end;
 end;
 
-{ main ------------------------------------------------------------ }
+{ menu ----------------------------------------------------------------------- }
+
+procedure MenuCommand(glwin: TGLWindow; MenuItem: TMenuItem);
+begin
+  case MenuItem.IntData of
+    Ord(Low(TShadowsImplementation)) ..
+    Ord(High(TShadowsImplementation)):
+      begin
+        ShadowsImplementation := TShadowsImplementation(MenuItem.IntData);
+        Glwin.PostRedisplay;
+      end;
+  end;
+end;
+
+function CreateMainMenu: TMenu;
+var
+  M: TMenu;
+
+  procedure AppendShadowsImplementationRadio(const Caption: string;
+    SI: TShadowsImplementation);
+  begin
+    ShadowsImplementationRadio[SI] := TMenuItemRadio.Create(Caption, Ord(SI),
+      ShadowsImplementation = SI, true);
+    M.Append(ShadowsImplementationRadio[SI]);
+    if ShadowsImplementationRadioGroup = nil then
+      ShadowsImplementationRadioGroup := ShadowsImplementationRadio[SI].Group else
+      ShadowsImplementationRadio[SI].Group := ShadowsImplementationRadioGroup;
+  end;
+
+begin
+  Result := TMenu.Create('Main menu');
+  M := TMenu.Create('_Method');
+    ShadowsImplementationRadioGroup := nil;
+    AppendShadowsImplementationRadio('_No shadows', siNone);
+    AppendShadowsImplementationRadio('_Invert trick (usable only with' +
+      ' simplest shadow casters)', siInvertTrick);
+    AppendShadowsImplementationRadio('2 passes, cull faces using our _engine ' +
+      '(currently ignores manifold, so it''s bad)',
+      siEngineCullFace2Passes);
+    AppendShadowsImplementationRadio('2 passes, cull faces using _OpenGL',
+      siGLCullFace2Passes);
+    AppendShadowsImplementationRadio('_StencilOpSeparate (best choice, ' +
+      'requires OpenGL >= 2.0)', siStencilOpSeparate);
+    Result.Append(M);
+end;
+
+{ main ----------------------------------------------------------------------- }
 
 var
   CamPos, CamDir, CamUp, GravityUp: TVector3Single;
@@ -406,6 +513,8 @@ begin
     VectorAdjustToLengthTo1st(CamDir, Box3dAvgSize(Scene.BoundingBox)*0.02);
     Glw.NavWalker.Init(CamPos, CamDir, CamUp, GravityUp, 0.0, 0.0);
 
+    Glw.MainMenu := CreateMainMenu;
+    Glw.OnMenuCommand := @MenuCommand;
     Glw.AutoRedisplay := true;
     Glw.StencilBufferBits := 8;
     Glw.OnInit := @InitGL;
