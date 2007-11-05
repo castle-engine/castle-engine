ea25c6d109d50742f6115c7ab56b3be5d9505ba4
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-11-05 03:04:44 +0000

    + RawUrlDecode procedure
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@2157 afe8dcc5-9094-451b-8773-5f495042a737

--- net/kambiurlutils.pas
+++ net/kambiurlutils.pas
@@ -29,9 +29,35 @@ interface
   If no #anchor existed, Anchor is set to ''. }
 procedure URLExtractAnchor(var URL: string; out Anchor: string);
 
+{ Returns S with all sequences like %xx replaced with their actual
+  8-bit characters.
+
+  The intention is that this is similat to PHP function with the same name.
+
+  To account for badly encoded strings, invalid encoded URLs do not
+  raise an error --- they are only reported to DataNonFatalError.
+  So you can simply ignore them, or write a warning about them for user.
+  This is done because often you will use this with
+  URLs provided by the user, read from some file etc., so you can't be sure
+  whether they are correctly encoded, and raising error unconditionally
+  is not OK. (Considering the number of bad HTML pages on WWW.)
+
+  The cases of badly encoded strings are:
+
+  @unorderedList(
+    @item("%xx" sequence ends unexpectedly at the end of the string.
+      That is, string ends with "%" or "%x". In this case we simply
+      keep "%" or "%x" in resulting string.)
+
+    @item("xx" in "%xx" sequence is not a valid hexadecimal number.
+      In this case we also simply keep "%xx" in resulting string.)
+  )
+}
+function RawUrlDecode(const S: string): string;
+
 implementation
 
-uses KambiStringUtils;
+uses SysUtils, KambiStringUtils, DataErrors;
 
 procedure URLExtractAnchor(var URL: string; out Anchor: string);
 var
@@ -43,6 +69,84 @@ begin
     Anchor := SEnding(URL, HashPos + 1);
     SetLength(URL, HashPos - 1);
   end;
+end;
+
+function RawUrlDecode(const S: string): string;
+
+  { Assume Position <= Length(S).
+    Check is S[Positon] is a start of %xx sequence:
+    - if not, exit false
+    - if yes, but %xx is invalid, report DataNonFatalError and exit false
+    - if yes and %xx is valid, set DecodedChar and exit true }
+  function ValidSequence(const S: string; Position: Integer;
+    out DecodedChar: char): boolean;
+  const
+    ValidHexaChars = ['a'..'f', 'A'..'F', '0'..'9'];
+
+    { Assume C is valid hex digit, return it's value (in 0..15 range). }
+    function HexDigit(const C: char): Byte;
+    begin
+      if C in ['0'..'9'] then
+        Result := Ord(C) - Ord('0') else
+      if C in ['a'..'f'] then
+        Result := 10 + Ord(C) - Ord('a') else
+      if C in ['A'..'F'] then
+        Result := 10 + Ord(C) - Ord('A');
+    end;
+
+  begin
+    Result := S[Position] = '%';
+    if Result then
+    begin
+      if Position + 2 > Length(S) then
+      begin
+        DataNonFatalError(Format(
+          'URL "%s" incorrectly encoded, %%xx sequence ends unexpectedly', [S]));
+        Exit(false);
+      end;
+
+      if (not (S[Position + 1] in ValidHexaChars)) or
+         (not (S[Position + 2] in ValidHexaChars)) then
+      begin
+        DataNonFatalError(Format(
+          'URL "%s" incorrectly encoded, %s if not a valid hexadecimal number',
+          [S, S[Position + 1] + S[Position + 2]]));
+        Exit(false);
+      end;
+
+      Byte(DecodedChar) := (HexDigit(S[Position + 1]) shl 4) or
+                            HexDigit(S[Position + 2]);
+    end;
+  end;
+
+var
+  I, ResultI: Integer;
+  DecodedChar: char;
+begin
+  { Allocate Result string at the beginning, to save time later for
+    memory reallocations. We can do this, since we know that final
+    Result is shorter or equal to S. }
+  SetLength(Result, Length(S));
+
+  ResultI := 1;
+  I := 1;
+
+  while I <= Length(S) do
+  begin
+    if ValidSequence(S, I, DecodedChar) then
+    begin
+      Result[ResultI] := DecodedChar;
+      Inc(ResultI);
+      Inc(I, 3);
+    end else
+    begin
+      Result[ResultI] := S[I];
+      Inc(ResultI);
+      Inc(I);
+    end;
+  end;
+
+  SetLength(Result, ResultI - 1);
 end;
 
 end.
