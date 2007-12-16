4e78fb4420b7eae15c302fa474c19ee08f272ed7
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-12-16 04:42:28 +0000

    * bump_mapping on Mesa tested, is OK
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@2347 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels.gl/vrmlopenglrenderer.pas
+++ 3dmodels.gl/vrmlopenglrenderer.pas
@@ -552,7 +552,6 @@ type
       - with mixed models, some line sets, some points, some spheres/boxes,
         some indexedfaceset with normalMap specified,
         some indexedfaceset with normalMap not specified.
-      - test on Mesa
       - fix flat normals artifacts on fountain core on fountain level
         with bump mapping
       }
