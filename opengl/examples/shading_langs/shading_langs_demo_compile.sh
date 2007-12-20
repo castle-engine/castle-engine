70d77481e514fc8b07d3b05f6b3e9ce08859de46
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-12-20 06:58:47 +0000

    + shading_langs_demo_compile.sh to easily compile
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@2357 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ opengl/examples/shading_langs/shading_langs_demo_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+# Hack to allow calling this script from it's dir.
+if [ -f shading_langs_demo.pasprogram ]; then
+  cd ../../../
+fi
+
+# Call this from ../../ (or just use `make examples').
+
+fpc -dRELEASE -dUSE_GL_GLU_UNITS @kambi.cfg opengl/examples/shading_langs/shading_langs_demo.pasprogram
