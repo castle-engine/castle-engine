c0149013ef6a4f7abb3ed0dc962584f8cf0068be
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-01-09 20:49:48 +0000

    * toon shader: Ok, once you realize that on fglrx you can't use const floats, I know how to perfectly workaround toon GLSL shader for this. Done.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@2478 afe8dcc5-9094-451b-8773-5f495042a737

--- opengl/examples/shading_langs/glsl_toon_shading.fs
+++ opengl/examples/shading_langs/glsl_toon_shading.fs
@@ -1,34 +1,60 @@
 // Kambi changed these to be const in fragment code
 // uniform vec4 Colour1,Colour2,Colour3,Colour4; // "shades"
 
-/* Note: this doesn't work like it should on Radeon with closed ATI
-   drivers (fglrx) on Linux MacBookPro. This is most probably OpenGL
-   bug there (as this code works on the same graphic card under Mac OS X).
-   See ../../../../kambi_vrml_test_suite/vrml_2/kambi_extensions/shaders/glsl_toon_shading_2_helper.fs
-   for color_from_intensity_alt function that workarounds this. */
-
 varying vec3 normal;
 void main()
 {
   float intensity;
   vec4 colour;
+
   // Normalize the normal, again
   vec3 n = normalize(normal);
+
   // Normalize light direction and convert to a vec3
   vec3 lDir = normalize(vec3(gl_LightSource[0].position));
+
   // Compute light intensity using dot product
   intensity = dot(lDir,n);
-  // Compute light intensity using dot product
-  intensity = dot(lDir,n);
+
   // Decide which shade to used based on intensity
-  if (intensity > 0.95)
-     colour = vec4(1, 1, 0, 1);
-  else if (intensity > 0.5)
-     colour = vec4(0.75, 0.75, 0, 1);
-  else if (intensity > 0.25)
-     colour = vec4(0.5, 0.5, 0, 1);
-  else
-     colour = vec4(0.25, 0.25, 0, 1);
+
+  /* Kambi: originally, this was like
+     "intensity > 0.95" etc.
+     But on fglrx (Radeon closed-source drivers on chantal)
+     it looks like constant floats are simply rounded up.
+     (Seems that on Mesa, the same problem is present... possibly this
+     is some shortcoming of older GLSL versions ?)
+
+     So I code it to avoid using const floats,
+     "intensity * 100 > 95" is equivalent to "intensity > 90 / 100"
+     and it works Ok.
+     I also avoid using vector color constants, that's why
+     color is calculated so strangely.
+
+     Full original code, for clarity:
+
+       if (intensity > 0.95)
+	  colour = vec4(1, 1, 0, 1);
+       else if (intensity > 0.5)
+	 colour = vec4(0.75, 0.75, 0, 1);
+       else if (intensity > 0.25)
+	  colour = vec4(0.5, 0.5, 0, 1);
+       else
+	  colour = vec4(0.25, 0.25, 0, 1);
+
+  */
+
+  colour = vec4(1, 1, 0, 1);
+  if (intensity * 100 <= 95)
+  {
+    colour /= 4.0;
+    if (intensity * 100 > 50)
+      colour *= 3.0; else
+    if (intensity * 100 > 25)
+      colour *= 2.0;
+    /* else stays as vec4(0.25, 0.25, 0, 1); */
+  }
+
   // Finally, set destination colour
   gl_FragColor = colour;
 }
