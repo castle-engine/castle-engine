2b793cccd888dce6cec823dd711a0d0c482f42f6
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-05-31 13:48:34 +0000

    * fixed to make Pascal filenames lowercase (since txt filenames in 2/ are no longer lowercase)
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@2803 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels/doc/x3d_nodes/run_x3d_nodes_spec_to_pascal.sh
+++ 3dmodels/doc/x3d_nodes/run_x3d_nodes_spec_to_pascal.sh
@@ -3,5 +3,5 @@
 for F in 2/*.txt; do
   echo "---- $F";
   BBB=`basename "$F" .txt`
-  x3d_nodes_spec_to_pascal "$BBB" < "$F" > x3d_"$BBB".inc
+  x3d_nodes_spec_to_pascal "$BBB" < "$F" > x3d_`stringoper LowerCase "$BBB"`.inc
 done
