7a73caaafec0a533a1231f184bb0b642d4307fce
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-09-05 06:06:22 +0000

    r3685@chantal:  michalis | 2008-09-04 05:02:50 +0200  * made shadow_volume_test_compile.sh work when called from current dir
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@3279 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels.gl/examples/shadow_volume_test/shadow_volume_test_compile.sh
+++ 3dmodels.gl/examples/shadow_volume_test/shadow_volume_test_compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
+# Hack to allow calling this script from it's dir.
+if [ -f shadow_volume_test.pasprogram ]; then
+  cd ../../../
+fi
+
 # Call this from ../../../ (or just use `make examples').
 
 fpc -dRELEASE @kambi.cfg \
