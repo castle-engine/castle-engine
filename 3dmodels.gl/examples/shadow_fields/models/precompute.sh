aea5e74726cf7c52ec2a9315c5b3e8d166181666
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-12-01 17:40:12 +0000

    + teapot to shadow_fields/models/
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@3904 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels.gl/examples/shadow_fields/models/precompute.sh
+++ 3dmodels.gl/examples/shadow_fields/models/precompute.sh
@@ -4,3 +4,4 @@ set -eu
 precompute_shadow_field --light sphere.wrl
 precompute_shadow_field --light stretched_cube.wrl
 precompute_shadow_field humanoid_stand.wrl
+precompute_shadow_field teapot.x3dv
