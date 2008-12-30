d2277201c8ee9938e22b844b84ad6c20b02aac90
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-12-30 22:28:08 +0000

    * stringoper svn ignore, + stringoper_compile.sh script
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@4029 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ base/tools/stringoper_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+# Hack to allow calling this script from it's dir.
+if [ -f stringoper.pasprogram ]; then
+  cd ../../
+fi
+
+# Call this from ../../ (or just use `make examples').
+
+fpc -dRELEASE @kambi.cfg base/tools/stringoper.pasprogram
