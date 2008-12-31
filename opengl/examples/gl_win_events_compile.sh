f54c041eafd95b7f253405059905324bf1672121
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-12-31 02:01:35 +0000

    * call gl_win_events_compile.sh from it's own dir
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@4030 afe8dcc5-9094-451b-8773-5f495042a737

--- opengl/examples/gl_win_events_compile.sh
+++ opengl/examples/gl_win_events_compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
+# Hack to allow calling this script from it's dir.
+if [ -f gl_win_events.pasprogram ]; then
+  cd ../../
+fi
+
 # Call this from ../../ (or just use `make examples').
 
 fpc -dRELEASE @kambi.cfg opengl/examples/gl_win_events.pasprogram
