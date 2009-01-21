7d1614616d90cb3e89a65bedd115bbb1448cbbea
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-01-21 02:30:53 +0000

    dynamic_ambient_occlusion: * shader must be fragment shader (done now, although it renders to tex and we don't do anything with it now...) * fill for debug rest of tex with red color
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@4135 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ 3dmodels.gl/examples/dynamic_ambient_occlusion/dynamic_ambient_occlusion.fs
@@ -0,0 +1,61 @@
+uniform sampler2D tex_elements_position_area;
+uniform sampler2D tex_elements_normal;
+uniform int tex_elements_size;
+
+uniform float area_scale;
+uniform vec3 position_scale;
+uniform vec3 position_shift;
+
+uniform int elements_count;
+
+/* ATI (Radeon) on Linux (fglrx) doesn't tolerate const floats in code. */
+uniform float zero_5;
+uniform float pi;
+
+void main(void)
+{
+  /* calculate current element properties.
+     We know which element is "current" from gl_FragCoord. */
+  vec2 current_st = gl_FragCoord.xy;
+  int current_index = int(current_st.x) + int(current_st.y) * tex_elements_size;
+
+  if (current_index < elements_count)
+  {
+    current_st /= tex_elements_size; /* make ST coords of the texture in 0..1 */
+    vec4 current_pos_area = texture2D(tex_elements_position_area, current_st);
+    vec3 current_pos = (current_pos_area.xyz + position_shift) * position_scale;
+
+    float color = 1.0;
+
+    for (int i = 0; i < elements_count; i++)
+    {
+      if (i != current_index)
+      {
+        vec2 element_st = vec2(
+          float(i % tex_elements_size),
+          float(i / tex_elements_size));
+        element_st /= tex_elements_size; /* make ST coords of the texture in 0..1 */
+
+        vec4 element_pos_area = texture2D(tex_elements_position_area, element_st);
+        vec3 element_pos = (element_pos_area.xyz + position_shift) * position_scale;
+        float element_area = element_pos_area.w * area_scale;
+
+        vec3 element_normal = texture2D(tex_elements_normal, element_st).xyz;
+        element_normal = (element_normal - zero_5) * 2.0;
+
+        vec3 direction_to_current = current_pos - element_pos;
+        float cos_emitter_angle = dot(normalize(direction_to_current), element_normal);
+        float sqr_distance = dot(direction_to_current, direction_to_current);
+
+        color -= (element_area * cos_emitter_angle / (4.0 * pi * sqr_distance));
+      }
+    }
+
+    gl_FragColor = vec4(color, color, color, 1.0);
+  }
+  else
+  {
+    /* Only for testing, to mark colors not corresponding to any element. */
+    gl_FragColor = vec4(1, 0, 0, 1.0);
+  }
+}
