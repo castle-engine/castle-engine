7d751330e08af9fcc57046431a10493a61792a05
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-11-10 13:33:22 +0000

    + TElevationKamScript: calculates elevation height from explicit math expression (KambiScript) given by user.
    + TElevation.Height spec done, this must be available for all elevation classes. Simple working implementation for TElevationGrid done.
    
    * Drawing terrain splitted into DrawGrid (old, uses GridHeight) or DrawElevation (new, uses just Height)
    
    * projection near/far adjusted by ScaleSize
    
    * uses Examine mode to view terrain
    
    + TODOs, comments for terrain prog:
      - nicer UI
      - new view, walk mode over infinite terrain
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@4913 afe8dcc5-9094-451b-8773-5f495042a737

--- 3dmodels.gl/examples/terrain/elevations.pas
+++ 3dmodels.gl/examples/terrain/elevations.pas
@@ -2,19 +2,27 @@ unit Elevations;
 
 interface
 
-uses SysUtils, Classes;
+uses SysUtils, Classes, KambiScript;
 
 type
-  { Elevation (height for each X, Y) data.
-
-    The specification of this is a little uncertain for now,
-    in the future I want to derive both TElevationSRTM (data from stored
-    1201 x 1201 grid) and TElevationRandom (data calculated from random
-    noise) from this. Still unsure how the final class interface should
-    look like, to be comfortable and sensible. }
+  { Elevation (height for each X, Y) data. }
   TElevation = class
   public
-    //TODO: function Height(const X, Y: Single): Single;
+    function Height(const X, Y: Single): Single; virtual; abstract;
+  end;
+
+  { Elevation (height for each X, Y) data calculated from KambiScript
+    expression. At construction, pass FunctionExpression,
+    that is KambiScript language expression calculating height
+    based on X, Y. }
+  TElevationKamScript = class(TElevation)
+  private
+    FXVariable, FYVariable: TKamScriptFloat;
+    FFunction: TKamScriptExpression;
+  public
+    constructor Create(const FunctionExpression: string);
+    destructor Destroy; override;
+    function Height(const X, Y: Single): Single; override;
   end;
 
   { Elevation data from a grid of values with specified width * height.
@@ -22,23 +30,32 @@ type
     GridSizeX * GridSizeY heights.
     The idea is that on such elevation, there are special grid points
     where the height data is accurate. Everything else is an interpolation
-    derived from this data.
-
-    GridSizeX, GridSizeY specify grid dimensions.
-    Use GridHeight(0..GridSizeX - 1, 0..GridSizeY - 1) to get height
-    at particular grid point.
-
-    Normal @link(Height) method, taking float arguments, is still usable
-    and implemented in this class. Just be aware that it simply uses the data
-    from GridHeight, interpolating it. So it's (very slightly) slower,
-    and it doesn't really present any more interesting information
-    (in contrast to typical procedural terrain, where there can be always
-    more and more detail at each level). }
+    derived from this data. }
   TElevationGrid = class(TElevation)
   public
+    { Get height of the elevation at specified 2D point.
+
+      This is implemented in TElevationGrid class, using
+      the data returned by GridHeight. For float X in 0..1 range,
+      we return grid values for grid points 0..GridSizeX - 1.
+      Outside 0..1 range, we clamp (that is, take nearest value
+      from 0..1 range) --- this way the elevation seemingly continues
+      into the infinity.
+
+      In comparison to GridHeight, it's (very slightly) slower,
+      and it doesn't really present any more interesting information
+      (in contrast to typical procedural terrain, where there can be always
+      more and more detail at each level). }
+    function Height(const X, Y: Single): Single; override;
+
+    { GridSizeX, GridSizeY specify grid dimensions.
+      Use GridHeight(0..GridSizeX - 1, 0..GridSizeY - 1) to get height
+      at particular grid point.
+      @groupBegin }
     function GridHeight(const X, Y: Cardinal): Single; virtual; abstract;
     function GridSizeX: Cardinal; virtual; abstract;
     function GridSizeY: Cardinal; virtual; abstract;
+    { @groupEnd }
   end;
 
   TElevationSRTM = class(TElevationGrid)
@@ -53,6 +70,55 @@ type
   end;
 
 implementation
+
+uses KambiUtils, KambiScriptParser;
+
+{ TElevationKamScript -------------------------------------------------------- }
+
+constructor TElevationKamScript.Create(const FunctionExpression: string);
+begin
+  inherited Create;
+
+  FXVariable := TKamScriptFloat.Create(false);
+  FXVariable.Name := 'x';
+  FXVariable.OwnedByParentExpression := false;
+
+  FYVariable := TKamScriptFloat.Create(false);
+  FYVariable.Name := 'y';
+  FYVariable.OwnedByParentExpression := false;
+
+  FFunction := ParseFloatExpression(FunctionExpression, [FXVariable, FYVariable]);
+end;
+
+destructor TElevationKamScript.Destroy;
+begin
+  FFunction.FreeByParentExpression;
+  FFunction := nil;
+
+  FreeAndNil(FXVariable);
+  FreeAndNil(FYVariable);
+
+  inherited;
+end;
+
+function TElevationKamScript.Height(const X, Y: Single): Single;
+begin
+  FXVariable.Value := X;
+  FYVariable.Value := Y;
+  Result := (FFunction.Execute as TKamScriptFloat).Value;
+end;
+
+{ TElevationGrid ------------------------------------------------------------- }
+
+function TElevationGrid.Height(const X, Y: Single): Single;
+begin
+  { TODO: for now, just take the nearest point, no bilinear filtering. }
+  Result := GridHeight(
+    Clamped(Round(X * (GridSizeX - 1)), 0, GridSizeX - 1),
+    Clamped(Round(Y * (GridSizeY - 1)), 0, GridSizeY - 1));
+end;
+
+{ TElevationSRTM ------------------------------------------------------------- }
 
 constructor TElevationSRTM.CreateFromFile(const FileName: string);
 var
