b0ac5346f174616c4fd0211403a9e9d2e62932c8
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-12-27 14:11:08 +0000

    * vrml_with_2d_controls remade, to display clickable rect as TClickableRect (descendant of TUIControl)
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@5111 afe8dcc5-9094-451b-8773-5f495042a737

--- examples/lazarus/vrml_with_2d_controls/mainf.pas
+++ examples/lazarus/vrml_with_2d_controls/mainf.pas
@@ -6,23 +6,18 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  KambiVRMLBrowser, OpenGLFonts, GL;
+  KambiVRMLBrowser, OpenGLFonts, GL, UIControls;
 
 type
-
-  { TForm1 }
-
   TForm1 = class(TForm)
     Browser: TKamVRMLBrowser;
-    procedure BrowserClick(Sender: TObject);
     procedure BrowserDraw(Sender: TObject);
     procedure BrowserGLContextClose(Sender: TObject);
     procedure BrowserGLContextInit(Sender: TObject);
-    procedure BrowserResize(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
-    GLFont: TGLBitmapFont_Abstract;
     GLSampleImage, GLSampleImageAlpha: TGLuint;
+    ClickableRect: TUIControl;
   public
     { public declarations }
   end; 
@@ -33,9 +28,72 @@ var
 implementation
 
 uses VRMLErrors, VRMLScene, KambiGLUtils, OpenGLBmpFonts,
-  BFNT_BitstreamVeraSans_Unit, GLImages, Areas;
+  BFNT_BitstreamVeraSans_Unit, GLImages, Areas, KeysMouse, KambiUtils;
 
-{ TForm1 }
+{ TClickableRect ------------------------------------------------------------- }
+
+type
+  { Clickable rectangular area on 2D screen.
+
+    Thanks to making this rectangle a TUIControl descendant,
+    and placing it on Browser.Controls list, VRML scene sensors
+    (like TouchSensor) will not intercept mouse clicks. That is, yellow
+    rectangle obscures anything clickable on VRML scene (like TouchSensor)
+    underneath. }
+  TClickableRect = class(TUIControl)
+  private
+    GLFont: TGLBitmapFont_Abstract;
+  public
+    function MouseDown(const MouseX, MouseY: Integer; Button: KeysMouse.TMouseButton;
+      const MousePressed: KeysMouse.TMouseButtons): boolean; override;
+    function IsDraw2D: boolean; override;
+    procedure Draw2D(const Focused: boolean); override;
+    function PositionInside(const X, Y: Integer): boolean; override;
+    procedure GLContextInit; override;
+    procedure GLContextClose; override;
+  end;
+
+function TClickableRect.MouseDown(const MouseX, MouseY: Integer; Button: KeysMouse.TMouseButton;
+  const MousePressed: KeysMouse.TMouseButtons): boolean;
+begin
+  ShowMessage('Clicked yellow rect !');
+end;
+
+function TClickableRect.IsDraw2D: boolean;
+begin
+  Result := true;
+end;
+
+procedure TClickableRect.Draw2D(const Focused: boolean);
+begin
+  glColor3f(1, 1, 0);
+  glRectf(10, 10, ContainerWidth - 10, 100);
+
+  glColor3f(0.2, 0.2, 0.2);
+  glRasterPos2i(20, 80);
+  GLFont.Print('Sample text drawn on GL area. Yellow rect is clickable.');
+end;
+
+function TClickableRect.PositionInside(const X, Y: Integer): boolean;
+begin
+  { This is the same area that is drawn by glRectf. }
+  Result := Between(X, 10, ContainerWidth - 10) and
+            Between(Y, ContainerHeight - 100, ContainerHeight - 10);
+end;
+
+procedure TClickableRect.GLContextInit;
+begin
+  inherited;
+  GLFont := TGLBitmapFont.Create(@BFNT_BitstreamVeraSans);
+end;
+
+procedure TClickableRect.GLContextClose;
+begin
+  FreeAndNil(GLFont);
+  inherited;
+end;
+
+{ TForm1 --------------------------------------------------------------------- }
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -46,18 +104,14 @@ begin
     {'../../../../kambi_vrml_test_suite/x3d/touch_sensor_tests.x3dv'});
   Browser.Scene.Spatial := [ssRendering, ssDynamicCollisions];
   Browser.Scene.ProcessEvents := true;
+
+  ClickableRect := TClickableRect.Create(Self);
+  Browser.Controls.Insert(0, ClickableRect);
 end;
 
 procedure Draw2D(Draw2DData: Pointer);
 begin
   glLoadIdentity;
-
-  glColor3f(1, 1, 0);
-  glRectf(10, 10, Form1.Browser.Width - 10, 100);
-
-  glColor3f(0.2, 0.2, 0.2);
-  glRasterPos2i(20, 80);
-  Form1.GLFont.Print('Sample text drawn on GL area. Yellow rect is clickable.');
 
   glRasterPos2i(10, 150);
   glCallList(Form1.GLSampleImage);
@@ -77,43 +131,14 @@ begin
   glPopAttrib;
 end;
 
-procedure TForm1.BrowserClick(Sender: TObject);
-begin
-  { Since Browser.IgnoreAreas contain our yellow rect area, we can use
-    it here: FindArea tells us if our click hits this area. }
-  if Browser.IgnoreAreas.FindArea(Browser.MouseX, Browser.MouseY) = 0 then
-    ShowMessage('Clicked yellow rect !');
-end;
-
 procedure TForm1.BrowserGLContextInit(Sender: TObject);
 begin
-  GLFont := TGLBitmapFont.Create(@BFNT_BitstreamVeraSans);
   GLSampleImage := LoadImageToDisplayList('sample_image.png', [], [], 0, 0);
   GLSampleImageAlpha := LoadImageToDisplayList('sample_image_with_alpha.png', [], [], 0, 0);
 end;
 
-procedure TForm1.BrowserResize(Sender: TObject);
-begin
-  { This is the same area that is drawn by glRectf. It has Y coords reverted
-    by "Browser.Height - ", since this is expected by IgnoreAreas (it requires
-    points in the same coord system as MouseX/Y).
-
-    Using Browser.IgnoreAreas is needed, otherwise VRML scene sensors
-    (like TouchSensor) could intercept mouse clicks (that is, when you
-    would click on your yellow rectangle, you could also click on TouchSensor
-    under it).
-
-    This depends on Browser.Width/Height, so it's safest to redo this in
-    Browser.OnResize. }
-  Browser.IgnoreAreas.Count := 0;
-  Browser.IgnoreAreas.Add(AreaCorners(10, Browser.Height - 10,
-    Browser.Width - 10, Browser.Height - 100));
-end;
-
 procedure TForm1.BrowserGLContextClose(Sender: TObject);
 begin
-  FreeAndNil(GLFont);
-
   { Calling glFreeDisplayList is not needed here (when GL context is closed,
     all display lists are freed anyway). I just do it for elegance. }
   glFreeDisplayList(GLSampleImage);
@@ -122,6 +147,5 @@ end;
 
 initialization
   {$I mainf.lrs}
-
 end.
 
