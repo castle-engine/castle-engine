349ba35c69494d9bc2e243f778295a2c4ad14810
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-01-29 01:52:31 +0000

    * fix demo_camera, and lazarus counterpart camera.lpi, to use SceneManager
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@5372 afe8dcc5-9094-451b-8773-5f495042a737

--- examples/lazarus/camera/mainf.pas
+++ examples/lazarus/camera/mainf.pas
@@ -6,16 +6,14 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  KambiGLControl, Cameras;
+  KambiGLControl, Cameras, KambiSceneManager;
 
 type
   TMainForm = class(TForm)
     GLControl: TKamOpenGLControl;
     Camera: TExamineCamera;
-    procedure GLControlDraw(Sender: TObject);
-    procedure GLControlResize(Sender: TObject);
+    SceneManager: TKamSceneManager;
     procedure FormCreate(Sender: TObject);
-    procedure GLControlGLContextInit(Sender: TObject);
   private
     { private declarations }
   public
@@ -27,34 +25,46 @@ var
 
 implementation
 
-uses VectorMath, Boxes3D, GL, GLU, KambiGLUtils;
+uses VectorMath, Boxes3D, GL, KambiGLUtils, Base3D, Frustum;
+
+type
+  TCube = class(TBase3D)
+  public
+    procedure Render(const Frustum: TFrustum;
+      TransparentGroup: TTransparentGroup; InShadow: boolean); override;
+    function BoundingBox: TBox3D; override;
+  end;
+
+procedure TCube.Render(const Frustum: TFrustum;
+  TransparentGroup: TTransparentGroup; InShadow: boolean);
+begin
+  if TransparentGroup in [tgAll, tgOpaque] then
+  begin
+    glPushAttrib(GL_ENABLE_BIT);
+      glEnable(GL_LIGHTING);
+      glEnable(GL_LIGHT0);
+      glEnable(GL_DEPTH_TEST);
+      DrawGLBox(-1, -1, -1, 1, 1, 1, 0, 0, 0, true, false);
+    glPopAttrib;
+  end;
+end;
+
+function TCube.BoundingBox: TBox3D;
+begin
+  Result := Box3D(
+    Vector3Single(-1, -1, -1),
+    Vector3Single( 1,  1,  1));
+end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  GLControl.Controls.Add(SceneManager);
+
+  SceneManager.Items.Add(TCube.Create(Self));
+
   Camera.Init(Box3d(
     Vector3Single(-1, -1, -1),
     Vector3Single( 1,  1,  1)));
-end;
-
-procedure TMainForm.GLControlGLContextInit(Sender: TObject);
-begin
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-  glEnable(GL_DEPTH_TEST);
-end;
-
-procedure TMainForm.GLControlResize(Sender: TObject);
-begin
-  glViewport(0, 0, GLControl.Width, GLControl.Height);
-  ProjectionGLPerspective(45.0, GLControl.Width / GLControl.Height, 0.1, 100);
-end;
-
-procedure TMainForm.GLControlDraw(Sender: TObject);
-begin
-  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT);
-  glLoadMatrix(Camera.Matrix);
-
-  DrawGLBox(-1, -1, -1, 1, 1, 1, 0, 0, 0, true, false);
 end;
 
 initialization
