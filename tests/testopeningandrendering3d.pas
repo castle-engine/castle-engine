f91baf9b4fb83fd15ec2a1a9881aa7301678b57f
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-03-27 07:41:24 +0000

    * much improved testopeningandrendering3d.pas testing:   test all files within kambi_vrml_test_suite/,   except known errors/ subdir,   ignore warnings,   check GL errors.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@5747 afe8dcc5-9094-451b-8773-5f495042a737

--- tests/testopeningandrendering3d.pas
+++ tests/testopeningandrendering3d.pas
@@ -18,42 +18,88 @@ unit TestOpeningAndRendering3D;
 
 interface
 
-uses fpcunit, testutils, testregistry;
+uses fpcunit, testutils, testregistry, EnumerateFiles,
+  GLWindow, VRMLScene, VRMLGLScene, KambiSceneManager;
 
 type
   TTestOpeningAndRendering3D = class(TTestCase)
+  private
+    { Available only during Test1 }
+    Window: TGLUIWindow;
+    SceneManager: TKamSceneManager;
+    Scene: TVRMLGLScene;
+
+    { FileName empty means to load empty scene. }
+    procedure TestScene(const FileName: string);
+    procedure TestSceneFromEnum(const FileInfo: TEnumeratedFileInfo);
   published
     procedure Test1;
   end;
 
 implementation
 
-uses SysUtils, GLWindow, VRMLScene, VRMLGLScene, KambiSceneManager;
+uses SysUtils, VRMLErrors, KambiUtils, KambiGLUtils;
+
+procedure TTestOpeningAndRendering3D.TestScene(const FileName: string);
+begin
+  Writeln('Testing "' + FileName + '"');
+
+  if FileName = '' then
+    Scene.Load(nil, true) else
+    Scene.Load(FileName);
+
+  SceneManager.Camera.Free;
+  SceneManager.Camera := SceneManager.CreateDefaultCamera(SceneManager);
+
+  { Force preparing and using OpenGL resources for the scene.
+    This way we also check that next Load frees them Ok. }
+  Window.EventBeforeDraw;
+  Window.EventDraw;
+
+  { check GL errors now, otherwise that could be deferred to unrelated
+    CheckGLErrors call later. This way we know what 3D filenam caused the error. }
+  CheckGLErrors;
+end;
+
+procedure TTestOpeningAndRendering3D.TestSceneFromEnum(const FileInfo: TEnumeratedFileInfo);
+var
+  ParentDirName: string;
+begin
+  { do not check files in "errors" subdir, these are known to cause trouble }
+  ParentDirName := ExtractFileName(ExclPathDelim(ExtractFileDir(FileInfo.FullFileName)));
+  if ParentDirName = 'errors' then Exit;
+
+  TestScene(FileInfo.FullFileName);
+end;
 
 procedure TTestOpeningAndRendering3D.Test1;
-var
-  Window: TGLUIWindow;
-  SceneManager: TKamSceneManager;
-  Scene: TVRMLGLScene;
+const
+  VrmlTestSuite = '..' + PathDelim + '..' + PathDelim + 'kambi_vrml_test_suite' + PathDelim;
 
-  { FileName is relative to kambi_vrml_test_suite.
-    Empty means to load empty scene. }
-  procedure TestScene(const FileName: string);
+  procedure TestScenesInDir(const Path: string);
+
+    procedure DoMask(const Mask: string);
+    begin
+      EnumFilesObj(Path + Mask, faReallyAnyFile, @TestSceneFromEnum, [eoRecursive]);
+    end;
+
   begin
-    if FileName = '' then
-      Scene.Load(nil, true) else
-      Scene.Load('../../kambi_vrml_test_suite/' + FileName);
-
-    SceneManager.Camera.Free;
-    SceneManager.Camera := SceneManager.CreateDefaultCamera(SceneManager);
-
-    { Force preparing and using OpenGL resources for the scene.
-      This way we also check that next Load frees them Ok. }
-    Window.EventBeforeDraw;
-    Window.EventDraw;
+    DoMask('*.wrl');
+    DoMask('*.wrz');
+    DoMask('*.wrl.gz');
+    DoMask('*.x3d');
+    DoMask('*.x3dz');
+    DoMask('*.x3d.gz');
+    DoMask('*.x3dv');
+    DoMask('*.x3dvz');
+    DoMask('*.x3dv.gz');
+    DoMask('*.3ds');
+    DoMask('*.dae');
   end;
 
 begin
+  VRMLWarning := @VRMLWarning_Ignore;
+
   Window := TGLUIWindow.Create(nil);
   try
     Scene := TVRMLGLScene.Create(Window);
@@ -68,8 +114,13 @@ begin
     Window.Init;
 
     TestScene('');
-    TestScene('x3d/follow_camera_by_proximity_sensor.x3dv');
-    TestScene('x3d/follow_camera_by_proximity_sensor.x3dv');
+
+    TestScene(VrmlTestSuite + 'x3d' + PathDelim + 'follow_camera_by_proximity_sensor.x3dv');
+    TestScene(VrmlTestSuite + 'x3d' + PathDelim + 'follow_camera_by_proximity_sensor.x3dv');
+
+    TestScenesInDir(VrmlTestSuite);
+
+    //TestScenesInDir(VrmlTestSuite + 'x3d/shadow_maps/');
 
     Window.Close;
   finally FreeAndNil(Window) end;
