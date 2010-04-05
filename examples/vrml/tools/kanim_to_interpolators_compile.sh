3e4984c2d7ee2d193a586a63efed487b46cd22e4
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-04-05 18:12:45 +0000

    * fix kanim_to_interpolators compilation * add kanim_to_interpolators to "make examples"
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@5822 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ examples/vrml/tools/kanim_to_interpolators_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+# Hack to allow calling this script from it's dir.
+if [ -f kanim_to_interpolators.pasprogram ]; then
+  cd ../../../
+fi
+
+# Call this from ../../../ (or just use `make examples').
+
+fpc -dRELEASE @kambi.cfg examples/vrml/tools/kanim_to_interpolators.pasprogram
