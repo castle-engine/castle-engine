717d800e0673347b79d55220ea42708b8da56f73
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-08-16 23:34:32 +0000

    Simplify and rename Render_BindMaterial_2 to Render_MaterialsBegin_2(TNodeMaterial_2).
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@6439 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/opengl/vrmlopenglrenderer.pas
+++ src/vrml/opengl/vrmlopenglrenderer.pas
@@ -1342,7 +1342,7 @@ type
     procedure Render_MaterialsBegin;
     procedure Render_MaterialsEnd;
     procedure Render_BindMaterial_1(MatNum: integer);
-    procedure Render_BindMaterial_2;
+    procedure Render_MaterialsBegin_2(Material: TNodeMaterial_2);
     procedure Render_Material(
       const AmbientColor, DiffuseColor, SpecularColor,
         EmissiveColor: TVector3Single;
