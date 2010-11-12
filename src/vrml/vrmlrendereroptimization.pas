39a6f73f85b9634a404784e801192da9faf4feeb
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-11-12 14:20:58 +0000

    TGLRendererOptimization type description much improved in vrml_engine_doc.
    Also TGLRendererOptimization description removed from Pascal docs in vrmlrendereroptimization.pas unit --- it's better to just link from there to vrml_engine_doc.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@7198 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/vrmlrendereroptimization.pas
+++ src/vrml/vrmlrendereroptimization.pas
@@ -1,95 +1,18 @@
-{ TGLRendererOptimization type.
+{ Rendering optimizations (TGLRendererOptimization type and helpers).
 
-  While this is needed by TVRMLGLScene, it has to be defined in this unit,
-  in non-opengl units category, because it's also used by Object3DAsVRML
-  and VRML animations reader. }
+  Used primarily by TVRMLGLScene class, but declared in a separate unit,
+  as some non-OpenGL stuff (like Object3DAsVRML and VRML animations reader)
+  also process this. }
 unit VRMLRendererOptimization;
 
 interface
 
 type
-  { This is used by @link(TVRMLGLScene.Optimization) to describe
-    what kind of optimization should be done. }
+  { Optimization method, used by @link(TVRMLGLScene.Optimization).
+    See [http://vrmlengine.sourceforge.net/vrml_engine_doc/output/xsl/html/sect.scene_gl.html#section.renderer_optimization]. }
   TGLRendererOptimization = (
-    { No optimization. No OpenGL display lists are constructed.
-      So calling PrepareResources and ChangedAll is very fast.
-      On the other hand, rendering is significantly slower,
-      as display lists often help a lot.
-
-      Use this if you plan to change the scene at runtime a lot
-      (or when you're going to render TVRMLGLScene object
-      very few times, say, only 1-2 frames, in some special situations)
-      Then building display lists would be only a waste of time,
-      since they would have to be rebuild very often. }
     roNone,
-
-    { Treat the scene as a one big static object.
-      One OpenGL display list is used, that renders the whole object.
-      This is great optimization if the scene is static (otherwise
-      rebuilding display lists too often would cost too much time)
-      and you're sure that user will usually see the whole scene
-      (or just a large part of it).
-
-      Note that this nullifies the purpose of
-      @link(TVRMLGLScene.RenderFrustum) (with and without octree) methods.
-      And the purpose
-      of the @link(TVRMLGLScene.Render) method with a parameter <> @nil.
-      That's because the scene will always be rendered fully to OpenGL.
-
-      Also, this is not good if some parts of the scene cannot be put
-      on display list. This concerns things with TVRMLShape.EnableDisplayList
-      = @false, which currently means only MovieTexture nodes.
-      TODO: we should fallback to roShapeDisplayList automatically in such
-      cases. For now, this will result in MovieTexture nodes being static
-      (i.e. movie will not play).
-
-      Also, this is not good if some parts of the scene should not be
-      rendered sometimes --- e.g. self object when rendering GeneratedCubeMapTexture.
-
-      If the scene is static but user usually only looks at some small
-      part of it, then building OctreeRendering for the scene
-      (by adding ssOctreeRendering to TVRMLScene.Spatial)
-      and using roShapeDisplayList and @link(TVRMLGLScene.RenderFrustum)
-      (will automatically use TVRMLScene.OctreeRendering then)
-      may be better. }
     roSceneDisplayList,
-
-    { Build separate OpenGL display list for each @link(TVRMLShape)
-      on list @link(TVRMLScene.Shapes). Use this if
-
-      @orderedList(
-        @item(you will change from time to time only some small parts of
-          the scene (since this will allow to rebuild, on changing, only
-          some small display lists, as opposed to roSceneDisplayList,
-          that has to rebuild large display list even if the change
-          is very local).)
-
-        @item(and/or you know that usually user will not see the whole scene,
-          only a small part of it.
-          See TestShapeVisibility parameter of @link(TVRMLGLScene.Render)
-          and @link(TVRMLGLScene.RenderFrustum).)
-
-        @item(
-          Another advantage of roShapeDisplayList is when you use
-          TVRMLGLAnimation. If part of your animation is actually still
-          (i.e. the same Shape is used, the same nodes inside),
-          and only the part changes --- then the still Shapes will
-          use and share only one display list (only one ---
-          throughout all the time when they are still!).
-          This can be a huge memory saving, which is important because
-          generating many display lists for TVRMLGLAnimation is generally
-          very memory-hungry operation.
-
-          Also when only transformation changes, only one display list will
-          be used (actually, anything inside of
-          TVRMLOpenGLRenderer.RenderShapeBegin, which includes
-          modelview transformation, texture transformation and all lights
-          settings).
-
-          This is achieved by TVRMLOpenGLRendererContextCache
-          methods Shape_Xxx.)
-      )
-    }
     roShapeDisplayList
   );
   PGLRendererOptimization = ^TGLRendererOptimization;
