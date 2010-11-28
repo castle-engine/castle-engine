bc5ac9a5c4dd031f30c0d5333dac7d3cc333742b
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-11-28 15:26:51 +0000

    More cutting off Areas unit.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@7445 afe8dcc5-9094-451b-8773-5f495042a737

--- src/ui/areas.pas
+++ src/ui/areas.pas
@@ -14,22 +14,7 @@
 }
 
 { 2D float rectangles (TArea).
-  Main use is to detect in which
-  2D area on the screen user clicked with mouse.
-
-  The usual use is to create many areas and add them to DefaultAreas manager.
-  Then you can query DefaultAreas.FindArea to find an area containing
-  given X, Y position.
-
-  This is not a terribly interesting unit. In fact, it's
-  a very trivial, short and boring unit. The idea was that
-  I may eventually implement other area shapes (not only rectangles),
-  more intelligent area searching code and generally use this as a basis
-  for simple 2D widgetset in OpenGL. Then things here would get
-  more interesting.
-
-  For now, it's used e.g. by OpenGL menu in GLMenu unit.
-}
+  Main use is to detect in which 2D area on the screen the mouse cursor is. }
 
 unit Areas;
 
@@ -52,7 +37,6 @@ type
 
   TDynAreaArray = class(TDynArray_1)
   public
-
     { FindArea returns index of the area that contains point (X, Y).
 
       @italic(It returns the index of the @bold(last) area, that is:
@@ -69,16 +53,7 @@ const
 
 function Area(const X0, Y0, Width, Height: Single): TArea;
 
-function AreaCorners(X0, Y0, X1, Y1: Single): TArea;
-
-{ TODO: unused anywhere, so untested. }
-{ }
-function AreasSum(const Area1, Area2: TArea): TArea;
-
 function PointInArea(const X, Y: Single; const Area: TArea): boolean;
-
-var
-  DefaultAreas: TDynAreaArray;
 
 {$undef read_interface}
 
@@ -107,38 +82,10 @@ begin
   Result.Height := Height;
 end;
 
-function AreaCorners(X0, Y0, X1, Y1: Single): TArea;
-begin
-  OrderUp(X0, X1);
-  OrderUp(Y0, Y1);
-  Result.X0 := X0;
-  Result.Y0 := Y0;
-  Result.Width := X1 - X0 + 1;
-  Result.Height := Y1 - Y0 + 1;
-end;
-
-function AreasSum(const Area1, Area2: TArea): TArea;
-begin
-  Result.X0 := Min(Area1.X0, Area2.X0);
-  Result.Y0 := Min(Area1.Y0, Area2.Y0);
-  Result.Width := Max(
-    Area1.X0 + Area1.Width - Result.X0,
-    Area2.X0 + Area2.Width - Result.X0);
-  Result.Height := Max(
-    Area1.Y0 + Area1.Height - Result.Y0,
-    Area2.Y0 + Area2.Height - Result.Y0);
-end;
-
 function PointInArea(const X, Y: Single; const Area: TArea): boolean;
 begin
   Result := (X >= Area.X0) and (X <= Area.X0 + Area.Width) and
             (Y >= Area.Y0) and (Y <= Area.Y0 + Area.Height);
 end;
 
-{ initialization / finalization ---------------------------------------------- }
-
-initialization
-  DefaultAreas := TDynAreaArray.Create;
-finalization
-  FreeAndNil(DefaultAreas);
 end.
