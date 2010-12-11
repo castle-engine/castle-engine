09e4d300d40a87218d156def2bb495718c355652
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-12-11 12:44:49 +0000

    Allow running many2vrml_compile.sh from it's own dir.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@7497 afe8dcc5-9094-451b-8773-5f495042a737

--- examples/vrml/many2vrml_compile.sh
+++ examples/vrml/many2vrml_compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
+# Hack to allow calling this script from it's dir.
+if [ -f many2vrml.lpr ]; then
+  cd ../../
+fi
+
 # Call this from ../../ (or just use `make examples').
 
 fpc -dRELEASE @kambi.cfg examples/vrml/many2vrml.lpr
