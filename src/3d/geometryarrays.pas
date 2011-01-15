db00efe789148e42e2efd8ad597e37a4a2c459d8
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-01-15 10:52:43 +0000

    Port texture coordinate to new renderer, for texCoordIndex case now:
     * TGeometryArrays get AddTexCoord, TexCoord, where you can choose dimensions and texture unit
     * renderer: previous RenderCoordinateBegin is now moved almost completely to PrepareAttributes. 
       The actual setting of texture coords is moved from EnableTextureCoordArray to new RenderCoordinateBegin (but it has to be fixed for new renderer anyway).
     * renderer: glTexCoordPointer is actually called to load tex coords for texture units.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@7717 afe8dcc5-9094-451b-8773-5f495042a737

--- src/3d/geometryarrays.pas
+++ src/3d/geometryarrays.pas
@@ -18,11 +18,20 @@ unit GeometryArrays;
 
 interface
 
-uses KambiUtils, VectorMath;
+uses KambiUtils, VectorMath, FGL;
 
 type
   { Primitive geometry types. Analogous to OpenGL primitives. }
   TGeometryPrimitive = (gpTriangles);
+
+  TTexCoordDimensions = 2..4;
+
+  { @exclude Internally used by TGeometryArrays }
+  TGeometryTexCoord = class
+    Dimensions: TTexCoordDimensions;
+    Offset: Integer;
+  end;
+  TGeometryTexCoordsList = specialize TFPGObjectList<TGeometryTexCoord>;
 
   { Geometry represented as arrays of indexes, vertex positions,
     texture coordinates and such. Many (eventually, all) geometry nodes
@@ -51,8 +60,13 @@ type
     HasColor: boolean;
     ColorOffset: Integer;
 
+    FTexCoords: TGeometryTexCoordsList;
+
     { TODO: GLSLAttributesOffsets: TStringList; }
     procedure SetCount(const Value: Integer);
+
+    procedure AddTexCoord(const Dimensions: TTexCoordDimensions;
+      const TextureUnit: Cardinal);
   public
     constructor Create;
     destructor Destroy; override;
@@ -119,6 +133,23 @@ type
     procedure AddGLSLAttribute(const Name: string; const Size: Cardinal);
     function GLSLAttribute: Pointer;
     procedure IncGLSLAttribute(var P: Pointer); }
+
+    { Allocated in AttributeArray texture coords.
+      Index is texture unit (counted from renderer first available texture
+      unit). If given item is @nil on this list, then this texture unit
+      is not allocated (just like it would be outside of TexCoords.Count). }
+    property TexCoords: TGeometryTexCoordsList read FTexCoords;
+
+    procedure AddTexCoord2D(const TextureUnit: Cardinal);
+    procedure AddTexCoord3D(const TextureUnit: Cardinal);
+    procedure AddTexCoord4D(const TextureUnit: Cardinal);
+
+    function TexCoord(const Dimensions: TTexCoordDimensions;
+      const TextureUnit, Index: Cardinal): Pointer;
+
+    function TexCoord2D(const TextureUnit, Index: Cardinal): PVector2Single;
+    function TexCoord3D(const TextureUnit, Index: Cardinal): PVector3Single;
+    function TexCoord4D(const TextureUnit, Index: Cardinal): PVector4Single;
   end;
 
 implementation
@@ -132,11 +163,13 @@ begin
   inherited;
   FCoordinateSize := SizeOf(TVector3Single) * 2;
   FAttributeSize := 0;
+  FTexCoords := TGeometryTexCoordsList.Create;
 end;
 
 destructor TGeometryArrays.Destroy;
 begin
   FreeAndNil(FIndexes);
+  FreeAndNil(FTexCoords);
   FreeMemNiling(FCoordinateArray);
   FreeMemNiling(FAttributeArray);
   inherited;
@@ -199,6 +232,78 @@ end;
 procedure TGeometryArrays.IncColor(var P: PVector3Single);
 begin
   PtrUInt(P) += AttributeSize;
+end;
+
+procedure TGeometryArrays.AddTexCoord(const Dimensions: TTexCoordDimensions;
+  const TextureUnit: Cardinal);
+var
+  OldCount, I: Integer;
+begin
+  if TextureUnit >= TexCoords.Count then
+  begin
+    OldCount := TexCoords.Count;
+    TexCoords.Count := TextureUnit + 1;
+    for I := OldCount to TexCoords.Count - 1 do
+      TexCoords[I] := nil; { make sure new items are nil }
+  end;
+
+  Assert(TextureUnit < TexCoords.Count);
+
+  if TexCoords[TextureUnit] = nil then
+  begin
+    TexCoords[TextureUnit] := TGeometryTexCoord.Create;
+    TexCoords[TextureUnit].Dimensions := Dimensions;
+    TexCoords[TextureUnit].Offset := AttributeSize;
+    FAttributeSize += SizeOf(Single) * Dimensions;
+  end else
+  if TexCoords[TextureUnit].Dimensions <> Dimensions then
+  begin
+    raise Exception.CreateFmt('Texture unit %d is already allocated but for %-dimensional tex coords (while %d requested)',
+      [TextureUnit, TexCoords[TextureUnit].Dimensions, Dimensions]);
+  end;
+end;
+
+procedure TGeometryArrays.AddTexCoord2D(const TextureUnit: Cardinal);
+begin
+  AddTexCoord(2, TextureUnit);
+end;
+
+procedure TGeometryArrays.AddTexCoord3D(const TextureUnit: Cardinal);
+begin
+  AddTexCoord(3, TextureUnit);
+end;
+
+procedure TGeometryArrays.AddTexCoord4D(const TextureUnit: Cardinal);
+begin
+  AddTexCoord(4, TextureUnit);
+end;
+
+function TGeometryArrays.TexCoord(const Dimensions: TTexCoordDimensions;
+  const TextureUnit, Index: Cardinal): Pointer;
+begin
+  if (TextureUnit < TexCoords.Count) and
+     (TexCoords[TextureUnit] <> nil) then
+  begin
+    Assert(TexCoords[TextureUnit].Dimensions = Dimensions, 'Texture coord allocated but for different dimensions');
+    Result := Pointer(PtrUInt(PtrUInt(FAttributeArray) +
+      TexCoords[TextureUnit].Offset + Index * AttributeSize));
+  end else
+    Result := nil;
+end;
+
+function TGeometryArrays.TexCoord2D(const TextureUnit, Index: Cardinal): PVector2Single;
+begin
+  Result := PVector2Single(TexCoord(2, TextureUnit, Index));
+end;
+
+function TGeometryArrays.TexCoord3D(const TextureUnit, Index: Cardinal): PVector3Single;
+begin
+  Result := PVector3Single(TexCoord(3, TextureUnit, Index));
+end;
+
+function TGeometryArrays.TexCoord4D(const TextureUnit, Index: Cardinal): PVector4Single;
+begin
+  Result := PVector4Single(TexCoord(4, TextureUnit, Index));
 end;
 
 end.
