96da7375a0b880e4a271b5639896f75dfd32e66d
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-02-10 00:33:29 +0000

    Removed unused GLSL snippets. Shader generator takes care to apply shadow maps regardless of the number of textures.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@8141 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/opengl/glsl/shadow_map_0_show_depth.fs
+++
@@ -1,9 +0,0 @@
-uniform sampler2D shadowMap;
-
-float shadow_depth(sampler2D shadowMap, vec4 shadowMapCoord);
-
-void main(void)
-{
-  float d = shadow_depth(shadowMap, gl_TexCoord[0]);
-  gl_FragColor = vec4(d, d, d, 1.0);
-}
