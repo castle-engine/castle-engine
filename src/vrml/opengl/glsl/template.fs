ac654779a5d34d383cdc0c9ffd2a50d3b06e7f8c
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-03-15 23:58:08 +0000

    Better fragment_color initialization for shader, also fix emissive calculation: sceneColor already contains it.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@8601 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/opengl/glsl/template.fs
+++ src/vrml/opengl/glsl/template.fs
@@ -1,7 +1,7 @@
 /* Generic GLSL fragment shader template. It will be used
    by vrmlshadergenerator.pas to construct final shader.
 
-   This is converted to template.fs.inc, and is them compiled
+   This is converted to template.fs.inc, and is then compiled
    in program's binary.
    When you change this file, rerun `make' and then recompile Pascal sources.
 */
@@ -18,20 +18,15 @@ float shadow_depth(sampler2D shadowMap, vec4 shadowMapCoord);
 
 void main(void)
 {
-  /* NVidia GeForce 450 GTS (kocury) fails to compile a shader when
-     we pass gl_FragColor as inout parameter (testcase even fresnel_and_toon.x3dv).
-     Radeon X1600 (fglrx, chantal) works with it OK.
-     So we just use fragment_color everywhere, and only assign it
-     to gl_FragColor at the end. */
-  vec4 fragment_color = gl_FrontLightModelProduct.sceneColor;
-
   vec3 normal_eye_fragment = normalize(normal_eye);
   /* PLUG: fragment_eye_space (vertex_eye, normal_eye_fragment) */
 
 #ifdef LIT
+  vec4 fragment_color;
+
   if (gl_FrontFacing)
   {
-    fragment_color += gl_FrontMaterial.emission;
+    fragment_color = gl_FrontLightModelProduct.sceneColor;
     /* PLUG: add_light_contribution_front (fragment_color, vertex_eye, normal_eye_fragment, gl_FrontMaterial) */
 
     /* Otherwise, alpha is usually large after previous add_light_contribution,
@@ -41,17 +36,24 @@ void main(void)
     fragment_color.a = gl_FrontMaterial.diffuse.a;
   } else
   {
-    fragment_color += gl_BackMaterial.emission;
+    fragment_color = gl_BackLightModelProduct.sceneColor;
     normal_eye_fragment = -normal_eye_fragment;
     /* PLUG: add_light_contribution_back (fragment_color, vertex_eye, normal_eye_fragment, gl_BackMaterial) */
     fragment_color.a = gl_BackMaterial.diffuse.a;
   }
 #else
-  fragment_color = gl_Color;
+  vec4 fragment_color = gl_Color;
 #endif
 
   /* PLUG: texture_apply (fragment_color, normal_eye_fragment) */
   /* PLUG: fog_apply (fragment_color, normal_eye_fragment) */
+
+  /* NVidia GeForce 450 GTS (kocury) fails to compile a shader when
+     we pass gl_FragColor as inout parameter to functions
+     (testcase even fresnel_and_toon.x3dv).
+     Although on Radeon X1600 (fglrx, chantal) it works OK.
+     So we just use fragment_color everywhere above, and only assign it
+     to gl_FragColor at the end. */
   gl_FragColor = fragment_color;
 
   /* PLUG: fragment_end (gl_FragColor) */
