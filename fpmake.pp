534a82e436d5e8953e09204beb6a838c18d50d0c
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-04-20 03:14:35 +0000

    Engine 2.4.3 version release.
    Also, remove date +%F from generate_versions.sh output -- messes diffs.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@8794 afe8dcc5-9094-451b-8773-5f495042a737

--- fpmake.pp
+++ fpmake.pp
@@ -56,7 +56,7 @@ begin
     P.HomepageURL
     {$endif} := 'http://vrmlengine.sourceforge.net/';
     P.Email := 'michalis.kambi' + '@gmail.com'; { at least protect sources from spammers }
-    P.Version := '2.4.2';
+    P.Version := '2.4.3';
 
     P.SourcePath.Add('src' + PathDelim + '3d');
     P.Targets.AddUnit('base3d.pas');
