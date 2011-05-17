6ce97bb92a0ae8d61e595ed49ec37b527db1f9dd
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-05-17 09:37:54 +0000

    Move plugging fog inside LinkProgram, to avoid doing it before checking CodeHash.
    This speeds rendering for the case when LinkProgram is not needed.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@8937 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/opengl/vrmlshader.pas
+++ src/vrml/opengl/vrmlshader.pas
@@ -184,6 +184,8 @@ type
     FNormalMapTextureUnit: Cardinal;
     FHeightMapInAlpha: boolean;
     FHeightMapScale: Single;
+    FFogEnabled: boolean;
+    FFogType: TFogType;
 
     { We have to optimize the most often case of TVRMLShader usage,
       when the shader is not needed or is already prepared.
@@ -1493,6 +1495,30 @@ var
     end;
   end;
 
+  procedure EnableShaderFog;
+  var
+    FogFactor: string;
+  begin
+    Plug(stVertex,
+      'void PLUG_vertex_eye_space(const in vec4 vertex_eye, const in vec3 normal_eye)' +NL+
+      '{' +NL+
+      '  gl_FogFragCoord = vertex_eye.z;' +NL+
+      '}');
+
+    case FFogType of
+      ftLinear: FogFactor := '(gl_Fog.end - gl_FogFragCoord) * gl_Fog.scale';
+      ftExp   : FogFactor := 'exp(-gl_Fog.density * gl_FogFragCoord)';
+      else raise EInternalError.Create('TVRMLShader.EnableFog:FogType?');
+    end;
+
+    Plug(stFragment,
+      'void PLUG_fog_apply(inout vec4 fragment_color, const vec3 normal_eye_fragment)' +NL+
+      '{' +NL+
+      '  fragment_color.rgb = mix(fragment_color.rgb, gl_Fog.color.rgb,' +NL+
+      '    clamp(1.0 - ' + FogFactor + ', 0.0, 1.0));' +NL+
+      '}');
+  end;
+
   procedure SetupUniformsOnce;
   var
     I: Integer;
@@ -1536,6 +1562,7 @@ begin
   EnableLights;
   EnableShaderMaterialFromColor;
   EnableShaderBumpMapping;
+  EnableShaderFog;
   if AppearanceEffects <> nil then
     EnableEffects(AppearanceEffects);
   if GroupEffects <> nil then
@@ -1884,27 +1911,10 @@ begin
 end;
 
 procedure TVRMLShader.EnableFog(const FogType: TFogType);
-var
-  FogFactor: string;
 begin
-  Plug(stVertex,
-    'void PLUG_vertex_eye_space(const in vec4 vertex_eye, const in vec3 normal_eye)' +NL+
-    '{' +NL+
-    '  gl_FogFragCoord = vertex_eye.z;' +NL+
-    '}');
-
-  case FogType of
-    ftLinear: FogFactor := '(gl_Fog.end - gl_FogFragCoord) * gl_Fog.scale';
-    ftExp   : FogFactor := 'exp(-gl_Fog.density * gl_FogFragCoord)';
-    else raise EInternalError.Create('TVRMLShader.EnableFog:FogType?');
-  end;
-
-  Plug(stFragment,
-    'void PLUG_fog_apply(inout vec4 fragment_color, const vec3 normal_eye_fragment)' +NL+
-    '{' +NL+
-    '  fragment_color.rgb = mix(fragment_color.rgb, gl_Fog.color.rgb,' +NL+
-    '    clamp(1.0 - ' + FogFactor + ', 0.0, 1.0));' +NL+
-    '}');
+  FFogEnabled := true;
+  FFogType := FogType;
+  FCodeHash.AddInteger(67 * (Ord(FFogType) + 1));
 end;
 
 function TVRMLShader.EnableCustomShaderCode(Shaders: TMFNodeShaders;
