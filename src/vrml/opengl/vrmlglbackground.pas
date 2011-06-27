4fb6ec54cd9b8d53a87386dd1be2732e5236ef93
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-06-27 00:10:35 +0000

    Fix formatting (no functional changes, only whitespace moved around).
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/kambi_vrml_game_engine@9300 afe8dcc5-9094-451b-8773-5f495042a737

--- src/vrml/opengl/vrmlglbackground.pas
+++ src/vrml/opengl/vrmlglbackground.pas
@@ -177,7 +177,8 @@ constructor TVRMLGLBackground.Create(
   SkyColor: PArray_Vector3Single; SkyColorCount: Integer;
   SkySphereRadius: Single);
 
-var CubeSize, CubeSize2: Single;
+var
+  CubeSize, CubeSize2: Single;
 
   procedure RenderTextureSide(bs: TBackgroundSide);
   const
@@ -192,41 +193,41 @@ var CubeSize, CubeSize2: Single;
       ((0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1))  {top}
     );
     TexCoords: array [0..3] of TVector2f = ((0, 0), (1, 0), (1, 1), (0, 1));
-  var i: Integer;
+  var
+    i: Integer;
   begin
-   if nieboTex[bs] = 0 then Exit;
+    if nieboTex[bs] = 0 then Exit;
 
-   { If nieboTex[bs] <> 0 to for sure Imgs[bs] <> nil,
-     so I can safely do here checks "Imgs[bs] is ..." }
+    { If nieboTex[bs] <> 0 to for sure Imgs[bs] <> nil,
+      so I can safely do here checks "Imgs[bs] is ..." }
 
-   if Imgs.Images[bs].HasAlpha then
-   begin
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-   end;
+    if Imgs.Images[bs].HasAlpha then
+    begin
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+      glEnable(GL_BLEND);
+    end;
 
-   glBindTexture(GL_TEXTURE_2D, nieboTex[bs]);
-   glBegin(GL_QUADS);
-     for i := 0 to 3 do
-     begin
-      glTexCoordv(TexCoords[i]);
-      glVertex3f( (Coords[bs, i, 0]*2-1)*CubeSize2,
-                  (Coords[bs, i, 1]*2-1)*CubeSize2,
-                  (Coords[bs, i, 2]*2-1)*CubeSize2);
-     end;
-   glEnd;
+    glBindTexture(GL_TEXTURE_2D, nieboTex[bs]);
+    glBegin(GL_QUADS);
+      for i := 0 to 3 do
+      begin
+        glTexCoordv(TexCoords[i]);
+        glVertex3f( (Coords[bs, i, 0]*2-1)*CubeSize2,
+                    (Coords[bs, i, 1]*2-1)*CubeSize2,
+                    (Coords[bs, i, 2]*2-1)*CubeSize2);
+      end;
+    glEnd;
 
-   if Imgs.Images[bs].HasAlpha then
-    glDisable(GL_BLEND);
+    if Imgs.Images[bs].HasAlpha then
+      glDisable(GL_BLEND);
   end;
 
+  { For given Angle (meaning: 0 = zenith, Pi = nadir), calculate the height
+    and radius of given circle of sky sphere. }
   procedure StackCircleCalc(const Angle: Single; var Y, Radius: Single);
-  { dla zadanego Angle, w konwencji 0 = zenith, Pi = nadir, oblicz
-    na jakiej wysokosci powinna znalezc sie obrecz kola dla tego Angle
-    i Radius tego kola. Wez pod uwage SkySphereRadius. }
   begin
-   Radius := sin(Angle)*SkySphereRadius;
-   Y := cos(Angle)*SkySphereRadius;
+    Radius := sin(Angle) * SkySphereRadius;
+    Y := cos(Angle) * SkySphereRadius;
   end;
 
   { Render*Stack: render one stack of sky/ground sphere.
@@ -247,222 +248,236 @@ var CubeSize, CubeSize2: Single;
   procedure RenderStack(
     const UpperColor: TVector3Single; const UpperAngle: Single;
     const LowerColor: TVector3Single; const LowerAngle: Single);
-  var UpperY, UpperRadius, LowerY, LowerRadius: Single;
+  var
+    UpperY, UpperRadius, LowerY, LowerRadius: Single;
 
     procedure Bar(const SliceAngle: Single);
-    var SinSliceAngle, CosSliceAngle: Single;
+    var
+      SinSliceAngle, CosSliceAngle: Single;
     begin
-     SinSliceAngle := Sin(SliceAngle);
-     CosSliceAngle := Cos(SliceAngle);
-     glColorv(LowerColor);
-     glVertex3f(SinSliceAngle*LowerRadius, LowerY, CosSliceAngle*LowerRadius);
-     glColorv(UpperColor);
-     glVertex3f(SinSliceAngle*UpperRadius, UpperY, CosSliceAngle*UpperRadius);
+      SinSliceAngle := Sin(SliceAngle);
+      CosSliceAngle := Cos(SliceAngle);
+      glColorv(LowerColor);
+      glVertex3f(SinSliceAngle*LowerRadius, LowerY, CosSliceAngle*LowerRadius);
+      glColorv(UpperColor);
+      glVertex3f(SinSliceAngle*UpperRadius, UpperY, CosSliceAngle*UpperRadius);
     end;
 
-  var i: Integer;
+  var
+    i: Integer;
   begin
-   StackCircleCalc(UpperAngle, UpperY, UpperRadius);
-   StackCircleCalc(LowerAngle, LowerY, LowerRadius);
-   glBegin(GL_QUAD_STRIP);
-     Bar(0); for i := 1 to Slices-1 do Bar(i* 2*Pi/Slices); Bar(0);
-   glEnd;
+    StackCircleCalc(UpperAngle, UpperY, UpperRadius);
+    StackCircleCalc(LowerAngle, LowerY, LowerRadius);
+    glBegin(GL_QUAD_STRIP);
+      Bar(0);
+      for i := 1 to Slices-1 do Bar(i* 2*Pi/Slices);
+      Bar(0);
+    glEnd;
   end;
 
   procedure RenderUpperStack(
     const UpperColor: TVector3Single;
     const LowerColor: TVector3Single; const LowerAngle: Single);
-  { latwo ale bardzo nieoptymalnie moznaby to zapisac jako
+  { Easy but not optimal implementation of this is
     RenderStack(UpperColor, 0, LowerColor, LowerAngle); }
-  var LowerY, LowerRadius: Single;
+  var
+    LowerY, LowerRadius: Single;
 
     procedure Pt(const SliceAngle: Single);
     begin
-     glVertex3f(Sin(SliceAngle)*LowerRadius, LowerY, Cos(SliceAngle)*LowerRadius);
+      glVertex3f(Sin(SliceAngle)*LowerRadius, LowerY, Cos(SliceAngle)*LowerRadius);
     end;
 
-  var i: Integer;
+  var
+    i: Integer;
   begin
-   StackCircleCalc(LowerAngle, LowerY, LowerRadius);
-   glBegin(GL_TRIANGLE_FAN);
-     glColorv(UpperColor);
-     glVertex3f(0, SkySphereRadius, 0);
-     glColorv(LowerColor);
-     Pt(0); for i := 1 to Slices-1 do Pt(i* 2*Pi/Slices); Pt(0);
-   glEnd;
+    StackCircleCalc(LowerAngle, LowerY, LowerRadius);
+    glBegin(GL_TRIANGLE_FAN);
+      glColorv(UpperColor);
+      glVertex3f(0, SkySphereRadius, 0);
+      glColorv(LowerColor);
+      Pt(0);
+      for i := 1 to Slices-1 do Pt(i* 2*Pi/Slices);
+      Pt(0);
+    glEnd;
   end;
 
   procedure RenderLowerStack(
     const UpperColor: TVector3Single; const UpperAngle: Single;
     const LowerColor: TVector3Single);
-  { latwo ale bardzo nieoptymalnie moznaby to zapisac jako
+  { Easy but not optimal implementation of this is
     RenderStack(UpperColor, UpperAngle, LowerColor, Pi); }
-  var UpperY, UpperRadius: Single;
+  var
+    UpperY, UpperRadius: Single;
 
     procedure Pt(const SliceAngle: Single);
     begin
-     glVertex3f(Sin(SliceAngle)*UpperRadius, UpperY, Cos(SliceAngle)*UpperRadius);
+      glVertex3f(Sin(SliceAngle)*UpperRadius, UpperY, Cos(SliceAngle)*UpperRadius);
     end;
 
-  var i: Integer;
-  begin
-   StackCircleCalc(UpperAngle, UpperY, UpperRadius);
-   glBegin(GL_TRIANGLE_FAN);
-     glColorv(LowerColor);
-     glVertex3f(0, -SkySphereRadius, 0);
-     glColorv(UpperColor);
-     Pt(0); for i := 1 to Slices-1 do Pt(i* 2*Pi/Slices); Pt(0);
-   glEnd;
-  end;
-
-var bs: TBackgroundSide;
-    TexturedSides: TBackgroundSides;
+  var
     i: Integer;
-    GroundHighestAngle: Single;
-    SomeTexturesWithAlpha: boolean;
-begin
- inherited Create;
-
- { caly konstruktor sprowadza sie do skonstruowania display listy
-   szescianNieba_list, no i do zaladowania tekstur nieboTex zeby pozniej
-   ta display lista mogla ich uzyc. }
-
- { calculate nieboTex and SomeTexturesWithAlpha }
- SomeTexturesWithAlpha := false;
- TexturedSides := [];
- for bs := Low(bs) to High(bs) do
- begin
-  nieboTex[bs] := 0;
-  if (Imgs.Images[bs] <> nil) and (not Imgs.Images[bs].IsNull) then
   begin
-   try
-     nieboTex[bs] := LoadGLTexture(Imgs.Images[bs], GL_LINEAR, GL_LINEAR,
-       { poniewaz rozciagamy teksture przy pomocy GL_LINEAR a nie chce nam
-         sie robic teksturze borderow - musimy uzyc GL_CLAMP_TO_EDGE
-         aby uzyskac dobry efekt na krancach }
-       Texture2DClampToEdge);
-   except
-     { Although texture image is already loaded in Imgs[bs],
-       still texture loading may fail, e.g. with ECannotLoadS3TCTexture
-       when OpenGL doesn't have proper extensions. Secure against this by
-       making nice DataWarning. }
-     on E: ETextureLoadError do
-     begin
-       DataWarning('Texture load error: ' + E.Message);
-       Continue;
-     end;
-   end;
-
-   Include(TexturedSides, bs);
-   if Imgs.Images[bs].HasAlpha then SomeTexturesWithAlpha := true;
+    StackCircleCalc(UpperAngle, UpperY, UpperRadius);
+    glBegin(GL_TRIANGLE_FAN);
+      glColorv(LowerColor);
+      glVertex3f(0, -SkySphereRadius, 0);
+      glColorv(UpperColor);
+      Pt(0);
+      for i := 1 to Slices-1 do Pt(i* 2*Pi/Slices);
+      Pt(0);
+    glEnd;
   end;
- end;
 
- CubeSize := SkySphereRadius * SphereRadiusToCubeSize;
- CubeSize2 := CubeSize / 2;
+var
+  bs: TBackgroundSide;
+  TexturedSides: TBackgroundSides;
+  i: Integer;
+  GroundHighestAngle: Single;
+  SomeTexturesWithAlpha: boolean;
+begin
+  inherited Create;
 
- szescianNieba_list := glGenListsCheck(1, 'TVRMLGLBackground.Create');
+  { caly konstruktor sprowadza sie do skonstruowania display listy
+    szescianNieba_list, no i do zaladowania tekstur nieboTex zeby pozniej
+    ta display lista mogla ich uzyc. }
 
- glNewList(szescianNieba_list, GL_COMPILE);
- glPushAttrib(GL_ENABLE_BIT or GL_TEXTURE_BIT or GL_COLOR_BUFFER_BIT);
- try
-   glDisable(GL_DEPTH_TEST);
-   glDisable(GL_LIGHTING);
-   glDisable(GL_FOG);
-   glDisable(GL_BLEND);
-
-   if GL_ARB_multitexture then
-     glActiveTextureARB(GL_TEXTURE0_ARB);
-   glDisable(GL_TEXTURE_2D);
-   if GL_ARB_texture_cube_map then glDisable(GL_TEXTURE_CUBE_MAP_ARB);
-   if GL_EXT_texture3D        then glDisable(GL_TEXTURE_3D_EXT);
-
-   { wykonujemy najbardziej elementarna optymalizacje : jesli mamy 6 tekstur
-     i zadna nie ma kanalu alpha (a w praktyce jest to chyba najczestsza sytuacja)
-     to nie ma sensu sie w ogole przejmowac sky i ground, tekstury je zaslonia. }
-   if (TexturedSides <> BGAllSides) or SomeTexturesWithAlpha then
-   begin
-    { calculate GroundHighestAngle, will be usable to optimize rendering sky.
-      GroundHighestAngle is measured in sky convention (0 = zenith, Pi = nadir).
-      If there is no sky I simply set GroundHighestAngle to sthg > Pi. }
-    if GroundAngleCount <> 0 then
-     GroundHighestAngle := Pi-GroundAngle^[GroundAngleCount-1] else
-     GroundHighestAngle := Pi + 1;
-
-    { render sky }
-    Assert(SkyColorCount >= 1, 'Sky must have at least one color');
-    Assert(SkyAngleCount+1 = SkyColorCount, 'Sky must have exactly one more Color than Angles');
-
-    if SkyColorCount = 1 then
+  { calculate nieboTex and SomeTexturesWithAlpha }
+  SomeTexturesWithAlpha := false;
+  TexturedSides := [];
+  for bs := Low(bs) to High(bs) do
+  begin
+    nieboTex[bs] := 0;
+    if (Imgs.Images[bs] <> nil) and (not Imgs.Images[bs].IsNull) then
     begin
-     { alpha ponizszego koloru nie ma znaczenia dla nas. Uzywamy 0 bo jest
-       standardem (standardowo glClearColor ma alpha = wlasnie 0). }
-     glClearColorv(SkyColor^[0], 0);
-     glClear(GL_COLOR_BUFFER_BIT);
-    end else
+      try
+        nieboTex[bs] := LoadGLTexture(Imgs.Images[bs], GL_LINEAR, GL_LINEAR,
+          { poniewaz rozciagamy teksture przy pomocy GL_LINEAR a nie chce nam
+            sie robic teksturze borderow - musimy uzyc GL_CLAMP_TO_EDGE
+            aby uzyskac dobry efekt na krancach }
+          Texture2DClampToEdge);
+      except
+        { Although texture image is already loaded in Imgs[bs],
+          still texture loading may fail, e.g. with ECannotLoadS3TCTexture
+          when OpenGL doesn't have proper extensions. Secure against this by
+          making nice DataWarning. }
+        on E: ETextureLoadError do
+        begin
+          DataWarning('Texture load error: ' + E.Message);
+          Continue;
+        end;
+      end;
+
+      Include(TexturedSides, bs);
+      if Imgs.Images[bs].HasAlpha then SomeTexturesWithAlpha := true;
+    end;
+  end;
+
+  CubeSize := SkySphereRadius * SphereRadiusToCubeSize;
+  CubeSize2 := CubeSize / 2;
+
+  szescianNieba_list := glGenListsCheck(1, 'TVRMLGLBackground.Create');
+
+  glNewList(szescianNieba_list, GL_COMPILE);
+  glPushAttrib(GL_ENABLE_BIT or GL_TEXTURE_BIT or GL_COLOR_BUFFER_BIT);
+  try
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_FOG);
+    glDisable(GL_BLEND);
+
+    if GL_ARB_multitexture then
+      glActiveTextureARB(GL_TEXTURE0_ARB);
+    glDisable(GL_TEXTURE_2D);
+    if GL_ARB_texture_cube_map then glDisable(GL_TEXTURE_CUBE_MAP_ARB);
+    if GL_EXT_texture3D        then glDisable(GL_TEXTURE_3D_EXT);
+
+    { wykonujemy najbardziej elementarna optymalizacje : jesli mamy 6 tekstur
+      i zadna nie ma kanalu alpha (a w praktyce jest to chyba najczestsza sytuacja)
+      to nie ma sensu sie w ogole przejmowac sky i ground, tekstury je zaslonia. }
+    if (TexturedSides <> BGAllSides) or SomeTexturesWithAlpha then
     begin
-     { wiec SkyColorCount >= 2. W zasadzie rendering przebiega na zasadzie
-         RenderUpperStack
-         RenderStack iles razy
-         RenderLowerStack
-       Probujemy jednak przerwac robote w trakcie ktoregos RenderStack
-       lub RenderLowerStack zeby nie tracic czasu na malowanie obszaru
-       ktory i tak zamalujemy przez ground. Uzywamy do tego GroundHighestAngle.
-     }
-     RenderUpperStack(SkyColor^[0], SkyColor^[1], SkyAngle^[0]);
-     for i := 1 to SkyAngleCount-1 do
-     begin
-      if SkyAngle^[i-1] > GroundHighestAngle then Break;
-      RenderStack(SkyColor^[i]  , SkyAngle^[i-1],
-                  SkyColor^[i+1], SkyAngle^[i]);
-     end;
-     { TODO: jesli ostatni stack ma SkyAngle bliskie Pi to powinnismy renderowac
-       juz ostatni stack przy uzyciu RenderLowerStack. }
-     if SkyAngle^[SkyAngleCount-1] <= GroundHighestAngle then
-      RenderLowerStack(
-        SkyColor^[SkyColorCount-1], SkyAngle^[SkyAngleCount-1],
-        SkyColor^[SkyColorCount-1]);
+      { calculate GroundHighestAngle, will be usable to optimize rendering sky.
+        GroundHighestAngle is measured in sky convention (0 = zenith, Pi = nadir).
+        If there is no sky I simply set GroundHighestAngle to sthg > Pi. }
+      if GroundAngleCount <> 0 then
+        GroundHighestAngle := Pi-GroundAngle^[GroundAngleCount-1] else
+        GroundHighestAngle := Pi + 1;
+
+      { render sky }
+      Assert(SkyColorCount >= 1, 'Sky must have at least one color');
+      Assert(SkyAngleCount+1 = SkyColorCount, 'Sky must have exactly one more Color than Angles');
+
+      if SkyColorCount = 1 then
+      begin
+        { alpha ponizszego koloru nie ma znaczenia dla nas. Uzywamy 0 bo jest
+          standardem (standardowo glClearColor ma alpha = wlasnie 0). }
+        glClearColorv(SkyColor^[0], 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+      end else
+      begin
+        { wiec SkyColorCount >= 2. W zasadzie rendering przebiega na zasadzie
+            RenderUpperStack
+            RenderStack iles razy
+            RenderLowerStack
+          Probujemy jednak przerwac robote w trakcie ktoregos RenderStack
+          lub RenderLowerStack zeby nie tracic czasu na malowanie obszaru
+          ktory i tak zamalujemy przez ground. Uzywamy do tego GroundHighestAngle.
+        }
+        RenderUpperStack(SkyColor^[0], SkyColor^[1], SkyAngle^[0]);
+        for i := 1 to SkyAngleCount-1 do
+        begin
+          if SkyAngle^[i-1] > GroundHighestAngle then Break;
+          RenderStack(SkyColor^[i]  , SkyAngle^[i-1],
+                      SkyColor^[i+1], SkyAngle^[i]);
+        end;
+        { TODO: jesli ostatni stack ma SkyAngle bliskie Pi to powinnismy renderowac
+          juz ostatni stack przy uzyciu RenderLowerStack. }
+        if SkyAngle^[SkyAngleCount-1] <= GroundHighestAngle then
+          RenderLowerStack(
+            SkyColor^[SkyColorCount-1], SkyAngle^[SkyAngleCount-1],
+            SkyColor^[SkyColorCount-1]);
+      end;
+
+      { render ground }
+      if GroundAngleCount <> 0 then
+      begin
+        { jesli GroundAngleCount = 0 to nie ma ground wiec nie wymagamy wtedy
+          zeby GroundColorCount = 1 (a wiec jest to wyjatek od zasady
+          GroundAngleCount + 1 = GroundColorCount) }
+        Assert(GroundAngleCount+1 = GroundColorCount, 'Ground must have exactly one more Color than Angles');
+
+        RenderLowerStack(GroundColor^[1], Pi-GroundAngle^[0],
+                         GroundColor^[0]);
+        for i := 1 to GroundAngleCount-1 do
+         RenderStack(GroundColor^[i+1], Pi-GroundAngle^[i],
+                     GroundColor^[i]  , Pi-GroundAngle^[i-1]);
+      end;
     end;
 
-    { render ground }
-    if GroundAngleCount <> 0 then
-    begin
-     { jesli GroundAngleCount = 0 to nie ma ground wiec nie wymagamy wtedy
-       zeby GroundColorCount = 1 (a wiec jest to wyjatek od zasady
-       GroundAngleCount + 1 = GroundColorCount) }
-     Assert(GroundAngleCount+1 = GroundColorCount, 'Ground must have exactly one more Color than Angles');
+    { render cube with six textured faces }
 
-     RenderLowerStack(GroundColor^[1], Pi-GroundAngle^[0],
-                      GroundColor^[0]);
-     for i := 1 to GroundAngleCount-1 do
-      RenderStack(GroundColor^[i+1], Pi-GroundAngle^[i],
-                  GroundColor^[i]  , Pi-GroundAngle^[i-1]);
-    end;
-   end;
+    glEnable(GL_TEXTURE_2D);
+    { Wybieramy GL_REPLACE bo scianki szescianu beda zawsze cale teksturowane
+      i chcemy olac zupelnie kolor/material jaki bedzie na tych sciankach.
+      Chcemy wziac to z tekstury (dlatego standardowe GL_MODULATE nie jest dobre).
+      Ponadto, kanal alpha tez chcemy wziac z tekstury, tzn. szescian
+      nieba ma byc przeswitujacy gdy tekstura bedzie przeswitujaca
+      (dlatego GL_DECAL nie jest odpowiedni). }
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
-   { render cube with six textured faces }
-
-   glEnable(GL_TEXTURE_2D);
-   { Wybieramy GL_REPLACE bo scianki szescianu beda zawsze cale teksturowane
-     i chcemy olac zupelnie kolor/material jaki bedzie na tych sciankach.
-     Chcemy wziac to z tekstury (dlatego standardowe GL_MODULATE nie jest dobre).
-     Ponadto, kanal alpha tez chcemy wziac z tekstury, tzn. szescian
-     nieba ma byc przeswitujacy gdy tekstura bedzie przeswitujaca
-     (dlatego GL_DECAL nie jest odpowiedni). }
-   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-   for bs := Low(bs) to High(bs) do RenderTextureSide(bs);
- finally
-  glPopAttrib;
-  glEndList;
- end;
+    for bs := Low(bs) to High(bs) do RenderTextureSide(bs);
+  finally
+    glPopAttrib;
+    glEndList;
+  end;
 end;
 
 destructor TVRMLGLBackground.Destroy;
 begin
- glDeleteLists(szescianNieba_list, 1);
- glDeleteTextures(6, @nieboTex);
- inherited;
+  glDeleteLists(szescianNieba_list, 1);
+  glDeleteTextures(6, @nieboTex);
+  inherited;
 end;
 
 end.
