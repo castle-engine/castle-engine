132bcb2fa732d4c65804faaed0b103529ebdff79
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-11-01 21:42:23 +0000

    Fix compile script.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@10288 afe8dcc5-9094-451b-8773-5f495042a737

--- examples/images_videos/fft/fft_tests_compile.sh
+++ examples/images_videos/fft/fft_tests_compile.sh
@@ -6,6 +6,6 @@ set -eu
 # Or just do "make examples" in base castle_game_engine directory.
 
 # Allow calling this script from it's dir.
-if [ -f fft_tests.lpr ]; then cd ../../; fi
+if [ -f fft_tests.lpr ]; then cd ../../../; fi
 
 fpc -dRELEASE @castle-fpc.cfg examples/images_videos/fft/fft_tests.lpr
