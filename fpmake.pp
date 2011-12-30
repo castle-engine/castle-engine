57fafdb42492913504123d95ec5d86a30d68d58b
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-12-30 07:33:10 +0000

    Increased versions of everything, to prepare for new release.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@10572 afe8dcc5-9094-451b-8773-5f495042a737

--- fpmake.pp
+++ fpmake.pp
@@ -56,7 +56,7 @@ begin
     P.HomepageURL
     {$endif} := 'http://castle-engine.sourceforge.net/';
     P.Email := 'michalis.kambi' + '@gmail.com'; { at least protect sources from spammers }
-    P.Version := '2.5.1';
+    P.Version := '3.0.0';
 
     P.SourcePath.Add('src' + PathDelim + '3d');
     P.Targets.AddUnit('base3d.pas');
