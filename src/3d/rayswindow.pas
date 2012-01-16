af5e5813df7f1479c73056b21ab1cd87b951059e
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-01-16 14:55:55 +0000

    Improve RaysWindow comments.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@10829 afe8dcc5-9094-451b-8773-5f495042a737

--- src/3d/rayswindow.pas
+++ src/3d/rayswindow.pas
@@ -43,14 +43,7 @@ function AdjustViewAngleRadToAspectRatio(const FirstViewAngleRad,
 { @groupEnd }
 
 type
-  { Calculate primary rays for given camera settings
-    and screen size.
-
-    This has just two methods: constructor and PrimaryRay that calculates
-    the position and direction of the primary ray. For typical ray-tracer usage,
-    you call constructor only once, and then call PrimaryRay for each pixel.
-    This way constructor can do some slightly more time-consuming work,
-    and in PrimaryRay we can reuse some calculation results. }
+  { Calculate primary rays for given camera settings and screen size. }
   TRaysWindow = class
   private
     FCamPosition, FCamDirection, FCamUp: TVector3Single;
@@ -65,10 +58,14 @@ type
     property CamUp: TVector3Single read FCamUp;
     { @groupEnd }
 
+    { Constructor. Calculates some values based on camera settings,
+      this way many calls to PrimaryRay for the same camera settings
+      are fast (useful for ray-tracers). }
     constructor Create(const ACamPosition, ACamDirection, ACamUp: TVector3Single);
 
-    { Create appropriate non-abstract descendant
-      (TPerspectiveRaysWindow or TOrthographicRaysWindow). }
+    { Create appropriate TRaysWindow instance.
+      Constructs non-abstract descendant (TPerspectiveRaysWindow or
+      TOrthographicRaysWindow, depending on APerspectiveView). }
     class function CreateDescendant(
       const ACamPosition, ACamDirection, ACamUp: TVector3Single;
       const APerspectiveView: boolean;
@@ -144,32 +141,20 @@ uses Math, CastleUtils;
 
 function AdjustViewAngleRadToAspectRatio(const FirstViewAngleRad, SecondToFirstRatio: Single): Single;
 begin
- { naiwny blad jaki kiedys popelnilem chcac to policzyc (chociaz wtedy jeszcze
-   nie bylo to opakowane w ta funkcje) to bylo zastosowanie wzorku
-     result := FirstViewAngle * SecondToFirstRatio;
-   ktory wychodzil z zupelnie blednego zalozenia ze stosunek miar katow x/y
-   jest taki jak stosunek wymiarow rzutni x/y. Elementarny blad !
+  { Ratio on window sizes is the ratio of angle tangeses.
+    For two angles (a, b), and window sizes (x, y) we have
+      Tan(a/2) = (x/2)/d
+      Tan(b/2) = (y/2)/d
+    where D is the distance to projection plane. So
+      Tan(a/2) / Tan(b/2) = x/y }
 
-   Majac dane dwa katy , a i b, i wymiary rzutni x, y mamy
-     tg(a/2) = (x/2)/d
-     tg(b/2) = (y/2)/d
-   gdzie d to odleglosc od rzutni (ktora sie zaraz skroci i nie bedzie potrzebna).
-   Mamy z tego ze
-     tg(a/2) / tg(b/2) = x/y
-   a wiec stosunek TANGENSOW katow jest taki jak stosunek wymiarow rzutni
-   (zreszta widac to w PrimaryRay_Core_Prepare gdzie obliczamy rzutnie z katow)
-   (TANGENSOW, a nie samych KATOW !!).
-
-   Odpowiednio przeksztalcajac powyzsza zaleznosc otrzymujemy poprawny
-   wzor pozwalajacy nam wyliczyc b majac dane a i stosunek y/x -
-   ten wzor jest zapisany ponizej w kodzie. }
- result := ArcTan( Tan(FirstViewAngleRad/2) * SecondToFirstRatio) * 2;
+  Result := ArcTan( Tan(FirstViewAngleRad/2) * SecondToFirstRatio) * 2;
 end;
 
 function AdjustViewAngleDegToAspectRatio(const FirstViewAngleDeg, SecondToFirstRatio: Single): Single;
 begin
- result := RadToDeg( AdjustViewAngleRadToAspectRatio( DegToRad(FirstViewAngleDeg),
-   SecondToFirstRatio));
+  Result := RadToDeg( AdjustViewAngleRadToAspectRatio( DegToRad(FirstViewAngleDeg),
+    SecondToFirstRatio));
 end;
 
 { TRaysWindow ------------------------------------------------------------ }
@@ -206,18 +191,16 @@ constructor TPerspectiveRaysWindow.Create(
   const ACamPosition, ACamDirection, ACamUp: TVector3Single;
   const APerspectiveViewAngles: TVector2Single);
 const
-  WindowDistance = 1;  { dowolna stala > 0, moze kiedys na cos sie przyda }
+  WindowDistance = 1;  { anything > 0 }
 begin
   inherited Create(ACamPosition, ACamDirection, ACamUp);
 
   PerspectiveViewAngles := APerspectiveViewAngles;
 
-  { oblicz rzutnie pomijajac Cam* i przyjmujac ze kamera jest
-    w punkcie (0, 0, 0) skierowana w (0, 0, -1) i ma up w (0, 1, 0) }
+  { calculate window parameters, ignoring camera settings }
   WindowZ := -WindowDistance;
-  { wiemy ze szerokosc rzutni / 2 / Abs(WindowZ) = tg (ViewAngleX / 2).
-    Wiec szerokosc rzutni = tg(ViewAngleX / 2) * Abs(WindowZ) * 2;
-    podobnie wysokosc rzutni = tg(ViewAngleY / 2) * Abs(WindowZ) * 2; }
+  { We know that WindowWidth / 2 / Abs(WindowZ) = Tan(ViewAngleX / 2).
+    From this, equations below follow. }
   WindowWidth  := Tan(DegToRad(PerspectiveViewAngles[0]) / 2) * Abs(WindowZ) * 2;
   WindowHeight := Tan(DegToRad(PerspectiveViewAngles[1]) / 2) * Abs(WindowZ) * 2;
 end;
@@ -226,26 +209,27 @@ procedure TPerspectiveRaysWindow.PrimaryRay(const x, y: Single;
   const ScreenWidth, ScreenHeight: Integer;
   out Ray0, RayDirection: TVector3Single);
 begin
- Ray0 := CamPosition;
+  Ray0 := CamPosition;
 
- { wyznacz kierunek promienia pierwotnego.
-   X z zakresu 0..ScreenWidth-1 ma dawac promienie dokladnie przez srodek
-   pixela na rzutni, analogicznie Y. }
- RayDirection := Vector3Single(
-   MapRange(x+0.5, 0, ScreenWidth , -WindowWidth /2, WindowWidth /2),
-   MapRange(y+0.5, 0, ScreenHeight, -WindowHeight/2, WindowHeight/2),
-   WindowZ);
+  { Direction of ray, ignoring current camera settings
+    (assume camera position = zero, direction = -Z, up = +Y).
+    Integer X, Y values should result in a ray that goes
+    right through the middle of the pixel area. }
+  RayDirection := Vector3Single(
+    MapRange(x+0.5, 0, ScreenWidth , -WindowWidth /2, WindowWidth /2),
+    MapRange(y+0.5, 0, ScreenHeight, -WindowHeight/2, WindowHeight/2),
+    WindowZ);
 
- { uwzglednij teraz Cam* transformujac odpowiednio promien.
-   Zwracam uwage ze nie uwzgledniamy tu przesuniecia CamPosition
-   (wtedy traktowalibysmy RayVector jako punkt na polprostej promienia,
-   a nie jako kierunek. Wiec musielibysmy wtedy od RayVector z powrotem
-   odjac CamPosition, zupelnie bez sensu skoro mozemy je juz teraz pominac). }
- RayDirection := MatrixMultPoint(
-   TransformToCoordsNoScaleMatrix(ZeroVector3Single,
-     CamDirection >< CamUp,
-     CamUp,
-     -CamDirection), RayDirection);
+  { Transform ray, to take camera into acount.
+    Note that we don't pass CamPosition to TransformToCoordsNoScaleMatrix
+    (this would be nonsense, it would treat RayVector as a point
+    on the ray, and later we would have to subtract Ray0 from it to get
+    actual direction). }
+  RayDirection := MatrixMultPoint(
+    TransformToCoordsNoScaleMatrix(ZeroVector3Single,
+      CamDirection >< CamUp,
+      CamUp,
+      -CamDirection), RayDirection);
 end;
 
 { TOrthographicRaysWindow ---------------------------------------------------- }
