891a70ce2517cf9a8fbe1eccd38b16c757a194c2
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-04-27 22:23:12 +0000

    Improve triangulator tests:
     * Change warnings during triangulation into errors, since they mean "Impossible to triangulate..." which should not happen on current testcases.
     * Randomly select color that is not too dark, to not be mistaken with black background of visualization.
     * Add polygon-circle to test.
     * Add polygon from JA, a cave generated by RoomArranger.
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@11362 afe8dcc5-9094-451b-8773-5f495042a737

--- tests/testtriangulator.pas
+++ tests/testtriangulator.pas
@@ -67,7 +67,12 @@ type
 
 implementation
 
-uses CastleStringUtils, CastleUtils, CastleGraphUtil;
+uses CastleStringUtils, CastleUtils, CastleGraphUtil, CastleWarnings;
+
+procedure OnWarningRaiseException(const AType: TWarningType; const Category, S: string);
+begin
+  raise Exception.CreateFmt('Triangulator made warning: %s: %s', [Category, S]);
+end;
 
 procedure TTestTriangulator.TestIndexedConcavePolygonNormal;
 const
@@ -128,6 +133,16 @@ begin
 end;
 {$endif VISUALIZE_TRIANGULATION}
 
+{ Random TFPColor value that isn't too dark. }
+function RandomLightFPColor: TFPColor;
+begin
+  repeat
+    Result := FPColor(Random($FFFF), Random($FFFF), Random($FFFF));
+  until ( (Result.Red / High(Word)) +
+          (Result.Green / High(Word)) +
+          (Result.Blue / High(Word)) ) / 3.0 > 0.2;
+end;
+
 procedure TTestTriangulator.Face(const Tri: TVector3Longint);
 var
   V0, V1, V2, EarNormal, E1, E2, E3: TVector3Single;
@@ -151,7 +166,7 @@ begin
 
   {$ifdef VISUALIZE_TRIANGULATION}
   { draw triangle, each triangle with different (random) color }
-  Canvas.Pen.FPColor := FPColor(Random($FFFF), Random($FFFF), Random($FFFF));
+  Canvas.Pen.FPColor := RandomLightFPColor;
   Canvas.Pen.Width := 5;
   { We would prefer to just use Canvas.Polygon (with brush) to draw triangle,
     but it's not implemented. Also Canvas.FloodFill is not available.
@@ -257,6 +272,22 @@ procedure TTestTriangulator.TestTriangulateFace;
     end;
   end;
 
+type
+  TVector3SingleArray = array of TVector3Single;
+
+  procedure GenerateCircle(const Count: Cardinal; var Vertexes: TVector3SingleArray);
+  var
+    Angle: Float;
+    I: Integer;
+  begin
+    SetLength(Vertexes, Count);
+    for I := 0 to Count - 1 do
+    begin
+      Angle := 2 * Pi * I / Count;
+      Vertexes[I] := Vector3Single(Cos(Angle), 0, Sin(Angle));
+    end;
+  end;
+
 const
   { From polygon_3_5.wrl corrected by JA by removing dup vertexes
     in TriangulateFace. }
@@ -269,6 +300,7 @@ const
     (0.000, 0, 0.630),
     (0.528, 0, 0.413)
   );
+
   Polygon_R3D_cs: array [0..25] of TVector3Single = (
     (8.255, 0, 5.929),
     (8.255, 0, 6.024),
@@ -352,17 +384,257 @@ const
     (5.110, 0, 5.461),
     (6.014, 0, 5.461)
   );
+
+  Polygon_RoomArranger_Cave: array [0..223] of TVector3Single = (
+    (0.099, 0, 0.166),
+    (0.104, 0, 0.183),
+    (0.113, 0, 0.202),
+    (0.139, 0, 0.224),
+    (0.166, 0, 0.253),
+    (0.181, 0, 0.278),
+    (0.187, 0, 0.304),
+    (0.200, 0, 0.311),
+    (0.188, 0, 0.333),
+    (0.188, 0, 0.373),
+    (0.200, 0, 0.403),
+    (0.200, 0, 0.425),
+    (0.188, 0, 0.460),
+    (0.188, 0, 0.477),
+    (0.188, 0, 0.494),
+    (0.203, 0, 0.520),
+    (0.222, 0, 0.538),
+    (0.272, 0, 0.538),
+    (0.292, 0, 0.538),
+    (0.316, 0, 0.538),
+    (0.329, 0, 0.543),
+    (0.339, 0, 0.543),
+    (0.355, 0, 0.543),
+    (0.372, 0, 0.543),
+    (0.380, 0, 0.551),
+    (0.409, 0, 0.532),
+    (0.427, 0, 0.532),
+    (0.435, 0, 0.555),
+    (0.435, 0, 0.566),
+    (0.470, 0, 0.566),
+    (0.482, 0, 0.555),
+    (0.482, 0, 0.544),
+    (0.482, 0, 0.531),
+    (0.482, 0, 0.521),
+    (0.518, 0, 0.495),
+    (0.529, 0, 0.474),
+    (0.529, 0, 0.457),
+    (0.516, 0, 0.426),
+    (0.516, 0, 0.406),
+    (0.516, 0, 0.390),
+    (0.526, 0, 0.371),
+    (0.535, 0, 0.356),
+    (0.545, 0, 0.336),
+    (0.551, 0, 0.325),
+    (0.564, 0, 0.325),
+    (0.570, 0, 0.325),
+    (0.561, 0, 0.340),
+    (0.561, 0, 0.356),
+    (0.554, 0, 0.379),
+    (0.554, 0, 0.397),
+    (0.554, 0, 0.417),
+    (0.554, 0, 0.431),
+    (0.561, 0, 0.456),
+    (0.570, 0, 0.469),
+    (0.570, 0, 0.485),
+    (0.570, 0, 0.502),
+    (0.543, 0, 0.538),
+    (0.530, 0, 0.551),
+    (0.530, 0, 0.557),
+    (0.535, 0, 0.567),
+    (0.548, 0, 0.575),
+    (0.548, 0, 0.589),
+    (0.548, 0, 0.601),
+    (0.537, 0, 0.601),
+    (0.524, 0, 0.615),
+    (0.524, 0, 0.623),
+    (0.524, 0, 0.623),
+    (0.534, 0, 0.646),
+    (0.551, 0, 0.655),
+    (0.564, 0, 0.655),
+    (0.550, 0, 0.693),
+    (0.537, 0, 0.706),
+    (0.506, 0, 0.733),
+    (0.495, 0, 0.745),
+    (0.489, 0, 0.755),
+    (0.482, 0, 0.768),
+    (0.481, 0, 0.779),
+    (0.485, 0, 0.787),
+    (0.494, 0, 0.793),
+    (0.507, 0, 0.798),
+    (0.522, 0, 0.802),
+    (0.540, 0, 0.806),
+    (0.558, 0, 0.809),
+    (0.577, 0, 0.813),
+    (0.596, 0, 0.817),
+    (0.617, 0, 0.824),
+    (0.634, 0, 0.840),
+    (0.634, 0, 0.862),
+    (0.634, 0, 0.888),
+    (0.634, 0, 0.913),
+    (0.634, 0, 0.925),
+    (0.634, 0, 0.942),
+    (0.634, 0, 0.968),
+    (0.634, 0, 1.000),
+    (1.000, 0, 1.000),
+    (1.000, 0, 1.000),
+    (1.000, 0, 0.716),
+    (0.976, 0, 0.710),
+    (0.965, 0, 0.695),
+    (0.941, 0, 0.695),
+    (0.923, 0, 0.695),
+    (0.912, 0, 0.707),
+    (0.904, 0, 0.719),
+    (0.904, 0, 0.735),
+    (0.904, 0, 0.753),
+    (0.904, 0, 0.773),
+    (0.896, 0, 0.779),
+    (0.896, 0, 0.798),
+    (0.914, 0, 0.821),
+    (0.875, 0, 0.850),
+    (0.848, 0, 0.871),
+    (0.810, 0, 0.899),
+    (0.783, 0, 0.917),
+    (0.743, 0, 0.900),
+    (0.732, 0, 0.883),
+    (0.762, 0, 0.848),
+    (0.784, 0, 0.842),
+    (0.812, 0, 0.859),
+    (0.848, 0, 0.778),
+    (0.848, 0, 0.778),
+    (0.834, 0, 0.767),
+    (0.823, 0, 0.750),
+    (0.835, 0, 0.721),
+    (0.835, 0, 0.721),
+    (0.804, 0, 0.699),
+    (0.775, 0, 0.690),
+    (0.767, 0, 0.690),
+    (0.767, 0, 0.678),
+    (0.821, 0, 0.618),
+    (0.813, 0, 0.601),
+    (0.813, 0, 0.584),
+    (0.786, 0, 0.561),
+    (0.770, 0, 0.538),
+    (0.754, 0, 0.531),
+    (0.736, 0, 0.525),
+    (0.711, 0, 0.525),
+    (0.711, 0, 0.514),
+    (0.711, 0, 0.514),
+    (0.725, 0, 0.488),
+    (0.760, 0, 0.443),
+    (0.786, 0, 0.443),
+    (0.786, 0, 0.443),
+    (0.816, 0, 0.434),
+    (0.859, 0, 0.422),
+    (0.903, 0, 0.394),
+    (0.933, 0, 0.330),
+    (0.950, 0, 0.281),
+    (0.950, 0, 0.281),
+    (0.998, 0, 0.255),
+    (0.998, 0, 0.255),
+    (0.998, 0, 0.000),
+    (0.998, 0, 0.000),
+    (0.981, 0, 0.006),
+    (0.958, 0, 0.040),
+    (0.954, 0, 0.058),
+    (0.954, 0, 0.077),
+    (0.968, 0, 0.089),
+    (0.968, 0, 0.113),
+    (0.968, 0, 0.137),
+    (0.950, 0, 0.143),
+    (0.851, 0, 0.190),
+    (0.829, 0, 0.255),
+    (0.759, 0, 0.264),
+    (0.759, 0, 0.247),
+    (0.749, 0, 0.238),
+    (0.688, 0, 0.202),
+    (0.681, 0, 0.192),
+    (0.693, 0, 0.173),
+    (0.679, 0, 0.160),
+    (0.666, 0, 0.143),
+    (0.644, 0, 0.109),
+    (0.637, 0, 0.097),
+    (0.591, 0, 0.089),
+    (0.553, 0, 0.081),
+    (0.553, 0, 0.206),
+    (0.347, 0, 0.206),
+    (0.347, 0, 0.184),
+    (0.517, 0, 0.184),
+    (0.489, 0, 0.064),
+    (0.427, 0, 0.041),
+    (0.411, 0, 0.035),
+    (0.403, 0, 0.048),
+    (0.403, 0, 0.077),
+    (0.411, 0, 0.098),
+    (0.401, 0, 0.109),
+    (0.371, 0, 0.117),
+    (0.334, 0, 0.109),
+    (0.316, 0, 0.101),
+    (0.292, 0, 0.089),
+    (0.283, 0, 0.072),
+    (0.260, 0, 0.057),
+    (0.238, 0, 0.057),
+    (0.203, 0, 0.061),
+    (0.204, 0, 0.067),
+    (0.239, 0, 0.062),
+    (0.296, 0, 0.137),
+    (0.225, 0, 0.182),
+    (0.159, 0, 0.101),
+    (0.165, 0, 0.078),
+    (0.182, 0, 0.071),
+    (0.180, 0, 0.065),
+    (0.155, 0, 0.071),
+    (0.145, 0, 0.071),
+    (0.145, 0, 0.097),
+    (0.145, 0, 0.106),
+    (0.083, 0, 0.098),
+    (0.062, 0, 0.089),
+    (0.058, 0, 0.074),
+    (0.032, 0, 0.066),
+    (0.013, 0, 0.072),
+    (0.000, 0, 0.089),
+    (0.021, 0, 0.110),
+    (0.021, 0, 0.121),
+    (0.030, 0, 0.130),
+    (0.039, 0, 0.136),
+    (0.047, 0, 0.140),
+    (0.056, 0, 0.142),
+    (0.064, 0, 0.143),
+    (0.072, 0, 0.144),
+    (0.079, 0, 0.145),
+    (0.085, 0, 0.147),
+    (0.091, 0, 0.151),
+    (0.095, 0, 0.157),
+    (0.099, 0, 0.166)
+  );
 var
   RevertOrder: boolean;
+  PreviousOnWarning: TWarningProc;
+  Polygon_Circle: TVector3SingleArray;
 begin
+  { Warnings from triangulator mean that polygon cannot be triangulated.
+    They mean errors for tests below, that *must* pass. }
+  PreviousOnWarning := OnWarning;
+  OnWarning := @OnWarningRaiseException;
+
+  GenerateCircle(16, Polygon_Circle);
+
   for RevertOrder := false to true do
   begin
+    DoPolygon(Polygon_Circle, 'Circle', 0, 2, RevertOrder);
     DoPolygon(Polygon_3_5, 'polygon_3_5', 0, 2, RevertOrder);
     DoPolygon(Polygon_R3D_cs, 'R3D_cs', 0, 2, RevertOrder);
     DoPolygon(Polygon_R3D_cs_full_polygon, 'R3D_cs_full_polygon', 0, 2, RevertOrder);
+    DoPolygon(Polygon_RoomArranger_Cave, 'RoomArranger_Cave', 0, 2, RevertOrder);
   end;
 
-  { TODO: test that results are same as hardcoded results }
+  { TODO: test that results are same as hardcoded results? }
+
+  OnWarning := PreviousOnWarning;
 end;
 
 initialization
