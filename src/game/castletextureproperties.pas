36bf7cadb13673c0414019f27ad7a3ffd787abd5
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-09-15 18:01:10 +0000

    Update header, these units are now part of the engine and so - LGPL.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@11794 afe8dcc5-9094-451b-8773-5f495042a737

--- src/game/castletextureproperties.pas
+++ src/game/castletextureproperties.pas
@@ -1,21 +1,14 @@
 {
   Copyright 2007-2012 Michalis Kamburelis.
 
-  This file is part of "castle".
+  This file is part of "Castle Game Engine".
 
-  "castle" is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
 
-  "castle" is distributed in the hope that it will be useful,
+  "Castle Game Engine" is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with "castle"; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
   ----------------------------------------------------------------------------
 }
