5eec70f87ed921f93ec3cfb2fd0961f863f593ff
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-09-22 05:04:26 +0000

    Clearly mark TBasicRenderParams in docs as something not supported.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@11865 afe8dcc5-9094-451b-8773-5f495042a737

--- src/x3d/opengl/castlescene.pas
+++ src/x3d/opengl/castlescene.pas
@@ -400,6 +400,10 @@ type
   );
 
   { Basic non-abstact implementation of render params for calling T3D.Render.
+
+    @exclude
+    @bold(This is a hack, exposed here only to support some really weird
+    OpenGL tricks in engine example programs. Do not use this in your own code.)
     To be used when you have to call T3D.Render, but you don't use scene manager.
     Usually this should not be needed, and this class may be removed at some
     point! You should always try to use TCastleSceneManager to manage and render
