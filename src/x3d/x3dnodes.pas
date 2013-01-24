8ffc6d2d62f446416198e047c3c5e5c902acd632
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-01-24 00:46:22 +0000

    X3DNodes docs fixes.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@12315 afe8dcc5-9094-451b-8773-5f495042a737

--- src/x3d/x3dnodes.pas
+++ src/x3d/x3dnodes.pas
@@ -14,11 +14,11 @@
 }
 
 (*
-  @abstract(All VRML/X3D nodes and other bulding blocks
-  of VRML/X3D (prototypes, routes and such).)
+  @abstract(Nodes and other important bulding blocks
+  of VRML/X3D (prototypes, routes and so on).)
 
   This is the central unit for VRML/X3D processing, as VRML/X3D file
-  is basically just a graph of nodes. We can represent VRML/X3D file
+  is basically just a graph of nodes. We represent whole VRML/X3D file
   by it's root node. This is what we load, save and process in this unit.
 
   The chapter "Reading, writing, processing VRML scene graph"
@@ -29,49 +29,54 @@
   @bold(Various uses of this unit:)
 
   @unorderedList(
-    @item(Nodes can be loaded/saved from the stream in a "classic" encoding.
-      This uses internally the lexer (in X3DLexer unit)
-      and parser of VRML fields (in X3DFields unit).
-
-      We can also load from an XML encoding. (Saving to XML encoding
-      is not implemented for now.)
+    @item(Nodes can be loaded or saved from the stream in a classic or
+      XML encoding.
+      For classic encoding we use a lexer in X3DLexer unit.
+      For XML encoding, we use standard FPC DOM unit.
+      Loading and saving of fields (in both encodings) is inside X3DFields unit.
 
       When reading VRML/X3D files, we generally do not change the VRML/X3D graph.
       So we're able to save exactly the same VRML/X3D graph
       back to another file. See also
       [http://castle-engine.sourceforge.net/vrml_engine_doc/output/xsl/html/section.writing_vrml.html#section.vrml_preserving].
-      This allows writing various VRML
+      This allows writing various VRML/X3D
       processing tools, that can simply read the file, change whatever
       they want, and write the file back --- knowing that the "untouched"
-      parts of VRML graph are preserved perfectly.)
+      parts of graph are preserved perfectly.)
 
     @item(TX3DNode class offers a lot of methods to process VRML/X3D graph.
       See TX3DNode.Traverse, TX3DNode.EnumerateNodes and
-      TX3DNode.FindNode. Traverse is especially important, since it
+      TX3DNode.FindNode. TX3DNode.Traverse is especially important, as it
       walks through VRML/X3D graph just as the specification says
-      (e.g. visiting only one child from a Switch node's children),
+      (accumulating transformation, visiting only active children of
+      nodes like Switch or LOD),
       gathering some state (useful especially for VRML 1.0, but also
       used for various things in later VRML/X3D versions).
 
-      When you want to e.g. render VRML/X3D graph, you can just traverse
-      the graph and render each pair of State with a geometry node
-      (some TAbstractGeometryNode instance).
+      When you want to render VRML/X3D graph, you can just traverse
+      the graph and render each geometry node (TAbstractGeometryNode instance)
+      knowing it's state (that will contain transformation and such).
       Alternatively, simple renderer can also use TAbstractGeometryNode.Triangulate.)
 
     @item(TAbstractGeometryNode is an important descendant of TX3DNode,
       as it defines stuff actually visible in the 3D world.
-      It has useful routinesfor calculating bounding volumes,
-      triangulating and such.)
+      It has useful routines for calculating bounding volumes,
+      triangulating and such.
+
+      But note that usually it's more comfortable
+      to load your scene to TCastleScene or TCastleSceneCore and then
+      query the shapes list in TCastleSceneCore.Shapes --- this is usually
+      more comfortable, also TCastleSceneCore and TShape cache some results
+      for speed.)
 
     @item(This unit doesn't depend on OpenGL, or any other particular rendering
-      method. So it's suitable also for RayTracer, and every other possible
+      method. So it's suitable also for CastleRayTracer, and every other possible
       renderer that will ever get implemented.)
 
     @item(Your own units can define new VRML/X3D nodes, by declaring
       new classes descending from TX3DNode (or other, more specialized,
       descendant). You should register your new classes by calling
-      @link(TNodesManager.RegisterNodeClasses NodesManager.RegisterNodeClasses),
-      usually in the initialization section of your unit.
+      @link(TNodesManager.RegisterNodeClasses NodesManager.RegisterNodeClasses).
 
       Examples of defining your own VRML/X3D node types (without modifying
       sources of this unit, or any other unit) are for example in the
@@ -79,7 +84,7 @@
       and LevelUnit in malfunction.)
   )
 
-  @bold(VRML node classes names, and inheritance:)
+  @bold(Node class names, and inheritance:)
 
   @unorderedList(
     @item(Normal VRML/X3D nodes are defined by classses
@@ -109,24 +114,24 @@
       They all descend from IX3DNode.)
 
     @item(
-      Optional suffix _1 or _2 after the node class name indicates that
-      this node class is used only for given major VRML version.
+      Optional suffix _1 or _2 at the node class name indicates that
+      this is only for a specific VRML/X3D standard version.
       Suffix _1 indicates nodes specific to VRML 1.0.
       Suffix _2 indicates nodes specific to VRML 2.0 (aka 97),
       that are not available in X3D.
-      Latest X3D nodes are simply indicated without any suffix
+      Latest X3D nodes do not have any suffix
       (to not clutter the source code that simply wants to use the latest
       and best version of the standard).
 
       For example, we have TIndexedFaceSetNode_1 for VRML 1.0 and
-      TIndexedFaceSetNode for VRML >= 2.0 (97, X3D).)
+      TIndexedFaceSetNode for VRML 2.0 and X3D.)
   )
 
   @bold(VRML/X3D versions handling:)
 
   @unorderedList(
     @item(
-      We handle VRML 1.0, VRML 2.0 (aka 97) and X3D (aka VRML 3.x).
+      We handle VRML 1.0, VRML 2.0 (aka VRML 97) and X3D (aka VRML 3.x).
 
       Every correct VRML / X3D file in classic and XML encoding should be parsed
       by this unit.
@@ -143,22 +148,22 @@
       Note that structures in this unit are @italic(not) focused
       on either VRML 1.0 or VRML >= 2.0. On the contrary: we try to handle
       the @italic(sum of all VRML and X3D). When reading VRML 1.0,
-      many VRML 2.0 constructs (that not conflict with anything in VRML 1.0)
+      many VRML 2.0 constructs (that do not conflict with anything in VRML 1.0)
       are allowed, and the other way around too.
 
       Internally, we do not convert VRML 1.0-specific constructs
-      to VRML 2.0 constructs (or the other way around).
+      to VRML 2.0/X3D constructs (or the other way around).
       See [http://castle-engine.sourceforge.net/vrml_engine_doc/output/xsl/html/section.vrml_1_2_sum.html]
       for more in-depth explanation of how, and why, we handle both
-      old-style (Inventor, VRML 1.0) and new-style (VRML 2.0, X3D) VRML
+      old-style (Inventor, VRML 1.0) and new-style (VRML 2.0, X3D)
       syntax.)
   )
 
   @bold(Files organization:) X3D nodes are inside x3d_COMPONET_NAME.inc files.
-  That's a nice thing, since X3D components provide a natural way to group
+  This way X3D specification components provide a natural way to group
   the vast number of nodes into files. Some remaining nodes that are not part
   of X3D are in other x3dnodes_xxx.inc files, for example
-  x3dnodes_1.inc contains only VRML-1.0 specific nodes.
+  x3dnodes_1.inc contains only VRML 1.0-specific nodes.
 *)
 
 unit X3DNodes;
