27f0529d338ccc9320db5aa29509073eff97eca8
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-11-18 04:00:01 +0000

    Commit android_lib_for_gl2jni/android_project. This is, for now, a modified version of hello-gl2 from Android NDK (although most of it will disappear when changing to NativeActivity, but I want to keep this first attempt in SVN).
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@13304 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ examples/android/android_lib_for_gl2jni/android_project/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eu
+
+cp -f ../libcge_android_lib.so libs/armeabi/libgl2jni.so
+
+ant debug
+adb install -r bin/GL2JNIActivity-debug.apk
+adb shell am start -a android.intent.action.MAIN -n com.android.gl2jni/.GL2JNIActivity
