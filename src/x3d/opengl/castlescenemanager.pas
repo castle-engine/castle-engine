814a38588b10342e5a72324a8d9dd87b3583472d
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-11-19 22:11:06 +0000

    Fix CastleSceneManager to have 1st mouse down work on a 3D sensor even when no mouse move happened before. Should fix both Android and iOS problem.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@13332 afe8dcc5-9094-451b-8773-5f495042a737

--- src/x3d/opengl/castlescenemanager.pas
+++ src/x3d/opengl/castlescenemanager.pas
@@ -1353,6 +1353,11 @@ begin
   Result := inherited;
   if Result or Paused or (not GetExists) then Exit;
 
+  { Call PointingDeviceMove, set MouseHitRay and such --- otherwise
+    the 1st mouse down event over a 3D object (like a TouchSensor) will be ignored
+    if it happens before any mouse move (which is normal on touch devices). }
+  MouseMove(0, 0, Container.MouseX, Container.MouseY);
+
   Result := GetItems.Press(Event);
   if Result then Exit;
 
