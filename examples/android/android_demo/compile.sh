7ce09cc5ac4c420464ed7edc0df71932092d33bf
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-11-19 22:10:05 +0000

    Fix android_demo/compile.sh compilation path.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@13331 afe8dcc5-9094-451b-8773-5f495042a737

--- examples/android/android_demo/compile.sh
+++ examples/android/android_demo/compile.sh
@@ -2,4 +2,4 @@
 set -eu
 
 cd ../../../
-ppcrossarm -Tandroid -CfVFPV3 -dDEBUG ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg examples/android/android_lib_for_gl2jni/cge_android_lib.lpr
+ppcrossarm -Tandroid -CfVFPV3 -dDEBUG ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg examples/android/android_demo/cge_android_lib.lpr
