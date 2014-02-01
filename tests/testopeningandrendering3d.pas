aa28d4b3ed5eb25eef1e7c4652d8a481ee2e8164
Michalis Kamburelis <michalis.kambi@gmail.com>
2014-02-01 20:50:39 +0000

    Fix test compilation.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@13574 afe8dcc5-9094-451b-8773-5f495042a737

--- tests/testopeningandrendering3d.pas
+++ tests/testopeningandrendering3d.pas
@@ -84,8 +84,8 @@ begin
 
   { Force preparing and using OpenGL resources for the scene.
     This way we also check that next Load frees them Ok. }
-  Window.EventBeforeRender;
-  Window.EventRender;
+  Window.Container.EventBeforeRender;
+  Window.Container.EventRender;
 
   { Check OpenGL errors now, otherwise they could be detected by an unrelated
     CheckGLErrors call later. This way we know what 3D filename caused
