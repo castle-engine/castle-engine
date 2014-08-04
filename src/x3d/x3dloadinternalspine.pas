22923d7e2e4633697082473f03f78a05829b57bc
Michalis Kamburelis <michalis.kambi@gmail.com>
2014-08-04 22:45:28 +0000

    Read spine JSON: skeleton, slots, bones, attachments, skins.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@13961 afe8dcc5-9094-451b-8773-5f495042a737

--- src/x3d/x3dloadinternalspine.pas
+++ src/x3d/x3dloadinternalspine.pas
@@ -26,11 +26,14 @@ implementation
 
 uses SysUtils, Classes, FGL, FpJson, JSONParser,
   CastleVectors, CastleUtils, CastleLog, CastleURIUtils, CastleDownload,
-  CastleStringUtils, CastleClassUtils;
+  CastleStringUtils, CastleClassUtils, CastleColors;
 
 type
   ESpineReadError = class(Exception);
 
+{ Atlas ---------------------------------------------------------------------- }
+
+type
   TAtlasRegion = class
   public
     Name: string;
@@ -237,7 +240,6 @@ begin
             Region := TAtlasRegion.Create;
             Region.Name := Line;
             Page.Regions.Add(Region);
-            WritelnLog('Spine', 'Added region ' + Region.Name);
           end;
         end else
         if Region <> nil then
@@ -257,6 +259,330 @@ begin
   finally FreeAndNil(Reader) end;
 end;
 
+{ JSON skeleton -------------------------------------------------------------- }
+
+type
+  TBoneList = class;
+
+  TBone = class
+    Name: string;
+    Length: Single;
+    XY: TVector2Single;
+    Scale: TVector2Single;
+    Rotation: Single;
+    Parent: TBone;
+    procedure Parse(const Json: TJSONObject;
+      const PossibleParents: TBoneList; const ExpectedParent: boolean);
+  end;
+
+  TBoneList = class(specialize TFPGObjectList<TBone>)
+    { Find bone by name.
+      @raises ESpineReadError If bone does not exist. }
+    function Find(const BoneName: string): TBone;
+  end;
+
+  TSlot = class
+    Name: string;
+    Bone: TBone;
+    Color: TCastleColor;
+    Attachment: string;
+    procedure Parse(const Json: TJSONObject; const Bones: TBoneList);
+  end;
+
+  TSlotList = specialize TFPGObjectList<TSlot>;
+
+  TAttachment = class
+    { SlotName and AttachmentName are map keys, to detect which TAttachment to use. }
+    SlotName, AttachmentName: string;
+    { The real attachment name, unique for a skeleton, for image attachment
+      this refers to atlas region name. }
+    Name: string;
+    procedure Parse(const Json: TJSONObject); virtual;
+    { Create and parse correct TAttachment descendant. }
+    class function CreateAndParse(const Json: TJSONObject;
+      const ASlotName, AnAttachmentName: string): TAttachment;
+  end;
+
+  TRegionAttachment = class(TAttachment)
+    XY: TVector2Single;
+    Scale: TVector2Single;
+    Rotation: Single;
+    Width, Height: Integer;
+    procedure Parse(const Json: TJSONObject); override;
+  end;
+
+  TAttachmentList = class(specialize TFPGObjectList<TAttachment>)
+    { Find by name.
+      @raises ESpineReadError If does not exist. }
+    function Find(const Name: string): TAttachment;
+  end;
+
+  TSkin = class
+    Attachments: TAttachmentList;
+    Name: string;
+    constructor Create;
+    destructor Destroy; override;
+    procedure Parse(const Json: TJSONObject);
+  end;
+
+  TSkinList = class(specialize TFPGObjectList<TSkin>)
+    { Find by name.
+      @raises ESpineReadError If does not exist. }
+    function Find(const Name: string): TSkin;
+  end;
+
+  TSkeleton = class
+    Root: TBone;
+    Bones: TBoneList;
+    Slots: TSlotList;
+    Skins: TSkinList;
+    DefaultSkin: TSkin;
+    constructor Create;
+    destructor Destroy; override;
+    procedure Parse(const Json: TJSONData);
+  end;
+
+procedure TBone.Parse(const Json: TJSONObject;
+  const PossibleParents: TBoneList; const ExpectedParent: boolean);
+var
+  ParentName: string;
+begin
+  Name := Json.Get('name', '');
+  Length := Json.Get('length', 0.0);
+  XY[0] := Json.Get('x', 0.0);
+  XY[1] := Json.Get('y', 0.0);
+  Scale[0] := Json.Get('scaleX', 0.0);
+  Scale[1] := Json.Get('scaleY', 0.0);
+  Rotation := Json.Get('rotation', 0.0);
+
+  ParentName := Json.Get('parent', '');
+  if ParentName <> '' then
+    Parent := PossibleParents.Find(ParentName);
+
+  if ExpectedParent then
+  begin
+    if Parent = nil then
+      raise ESpineReadError.CreateFmt('Parent for bone "%s" expected, but not specified', [Name]);
+  end else
+  begin
+    if Parent <> nil then
+      raise ESpineReadError.CreateFmt('Parent for bone "%s" not expected, but specified', [Name]);
+  end;
+end;
+
+function TBoneList.Find(const BoneName: string): TBone;
+var
+  I: Integer;
+begin
+  for I := 0 to Count - 1 do
+    if Items[I].Name = BoneName then
+      Exit(Items[I]);
+  raise ESpineReadError.CreateFmt('Bone name "%s" not found', [BoneName]);
+end;
+
+procedure TSlot.Parse(const Json: TJSONObject; const Bones: TBoneList);
+begin
+  Name := Json.Get('name', '');
+  Bone := Bones.Find(Json.Get('bone', ''));
+  Attachment := Json.Get('attachment', '');
+  // TODO: Color :=
+end;
+
+function TAttachmentList.Find(const Name: string): TAttachment;
+var
+  I: Integer;
+begin
+  for I := 0 to Count - 1 do
+    if Items[I].Name = Name then
+      Exit(Items[I]);
+  raise ESpineReadError.CreateFmt('Attachment name "%s" not found', [Name]);
+end;
+
+class function TAttachment.CreateAndParse(const Json: TJSONObject;
+  const ASlotName, AnAttachmentName: string): TAttachment;
+var
+  TypeName: string;
+begin
+  TypeName := Json.Get('type', 'region');
+  if TypeName = 'region' then
+  begin
+    Result := TRegionAttachment.Create;
+    Result.SlotName := ASlotName;
+    Result.AttachmentName := AnAttachmentName;
+    Result.Parse(Json);
+  end else
+    raise ESpineReadError.Create('Only attachment type "region" supported now');
+end;
+
+procedure TAttachment.Parse(const Json: TJSONObject);
+begin
+  Name := Json.Get('name', AttachmentName);
+end;
+
+procedure TRegionAttachment.Parse(const Json: TJSONObject);
+begin
+  inherited;
+  XY[0] := Json.Get('x', 0.0);
+  XY[1] := Json.Get('y', 0.0);
+  Scale[0] := Json.Get('scaleX', 0.0);
+  Scale[1] := Json.Get('scaleY', 0.0);
+  Rotation := Json.Get('rotation', 0.0);
+  Width := Json.Get('width', 0);
+  Height := Json.Get('height', 0);
+end;
+
+constructor TSkin.Create;
+begin
+  inherited;
+  Attachments := TAttachmentList.Create;
+end;
+
+destructor TSkin.Destroy;
+begin
+  FreeAndNil(Attachments);
+  inherited;
+end;
+
+procedure TSkin.Parse(const Json: TJSONObject);
+
+  procedure ParseSlotMap(const Json: TJSONObject; const SlotName: string);
+  var
+    I: Integer;
+    Attachment: TAttachment;
+  begin
+    for I := 0 to Json.Count - 1 do
+      if Json.Items[I] is TJSONObject then
+      begin
+        Attachment := TAttachment.CreateAndParse(
+          TJSONObject(Json.Items[I]), SlotName, Json.Names[I]);
+        Attachments.Add(Attachment);
+      end;
+  end;
+
+var
+  I: Integer;
+begin
+  for I := 0 to Json.Count - 1 do
+    if Json.Items[I] is TJSONObject then
+      ParseSlotMap(TJSONObject(Json.Items[I]), Json.Names[I]);
+end;
+
+function TSkinList.Find(const Name: string): TSkin;
+var
+  I: Integer;
+begin
+  for I := 0 to Count - 1 do
+    if Items[I].Name = Name then
+      Exit(Items[I]);
+  raise ESpineReadError.CreateFmt('Skin name "%s" not found', [Name]);
+end;
+
+constructor TSkeleton.Create;
+begin
+  inherited;
+  Bones := TBoneList.Create;
+  Slots := TSlotList.Create;
+  Skins := TSkinList.Create;
+end;
+
+destructor TSkeleton.Destroy;
+begin
+  DefaultSkin := nil;
+  FreeAndNil(Skins);
+  FreeAndNil(Slots);
+  Root := nil;
+  FreeAndNil(Bones);
+  inherited;
+end;
+
+procedure TSkeleton.Parse(const Json: TJSONData);
+var
+  O: TJSONObject;
+
+  procedure ReadBones;
+  var
+    I: Integer;
+    Bone: TBone;
+    ChildArray: TJSONArray;
+  begin
+    ChildArray := O.Find('bones', jtArray) as TJSONArray;
+    if ChildArray = nil then
+      raise ESpineReadError.Create('Spine JSON skeleton: Missing "bones" array');
+
+    for I := 0 to ChildArray.Count - 1 do
+      if ChildArray[I] is TJSONObject then
+      begin
+        Bone := TBone.Create;
+        Bones.Add(Bone);
+        if Root = nil then
+        begin
+          { reading Root bone. It should have no parent. }
+          Bone.Parse(TJSONObject(ChildArray[I]), Bones, false);
+          Root := Bone;
+        end else
+          { reading child bone. It must have a parent. }
+          Bone.Parse(TJSONObject(ChildArray[I]), Bones, true);
+      end;
+  end;
+
+  procedure ReadSlots;
+  var
+    I: Integer;
+    Slot: TSlot;
+    ChildArray: TJSONArray;
+  begin
+    ChildArray := O.Find('slots', jtArray) as TJSONArray;
+    if ChildArray = nil then
+      raise ESpineReadError.Create('Spine JSON skeleton: Missing "slots" array');
+
+    for I := 0 to ChildArray.Count - 1 do
+      if ChildArray[I] is TJSONObject then
+      begin
+        Slot := TSlot.Create;
+        Slots.Add(Slot);
+        Slot.Parse(TJSONObject(ChildArray[I]), Bones);
+      end;
+  end;
+
+  procedure ReadSkins;
+  var
+    I: Integer;
+    Skin: TSkin;
+    ChildObj: TJSONObject;
+  begin
+    ChildObj := O.Find('skins', jtObject) as TJSONObject;
+    if ChildObj = nil then
+      raise ESpineReadError.Create('Spine JSON skeleton: Missing "skins" object');
+
+    for I := 0 to ChildObj.Count - 1 do
+      if ChildObj.Items[I] is TJSONObject then
+      begin
+        Skin := TSkin.Create;
+        Skins.Add(Skin);
+        Skin.Name := ChildObj.Names[I];
+        Skin.Parse(TJSONObject(ChildObj.Items[I]));
+
+        if Skin.Name = 'default' then
+          DefaultSkin := Skin;
+      end;
+
+    if DefaultSkin = nil then
+      raise ESpineReadError.Create('Spine JSON skeleton: Missing "default" skin definition');
+  end;
+
+begin
+  if not (Json is TJSONObject) then
+    raise ESpineReadError.Create('Spine JSON skeleton: Expected JSONObject at root');
+
+  O := TJSONObject(Json);
+
+  ReadBones;
+  ReadSlots;
+  ReadSkins;
+end;
+
+{ Main loading function ------------------------------------------------------ }
+
 function LoadSpine(const URL: string): TX3DRootNode;
 var
   Json: TJSONData;
@@ -264,7 +590,17 @@ var
   S: TStream;
   AtlasURL: string;
   Atlas: TAtlas;
+  Skeleton: TSkeleton;
 begin
+  AtlasURL := ChangeURIExt(URL, '.atlas');
+  if URIFileExists(AtlasURL) then
+  begin
+    Atlas := ReadAtlas(AtlasURL);
+    try
+      WritelnLog('Spine', Format('Atlas read, pages: %d', [Atlas.Pages.Count]));
+    finally FreeAndNil(Atlas) end;
+  end;
+
   S := Download(URL);
   try
     P := TJSONParser.Create(S);
@@ -274,16 +610,17 @@ begin
         Result := TX3DRootNode.Create('', URL);
         try
           if Assigned(Json) then
+          begin
             WritelnLogMultiline('Spine', 'Returned JSON structure: ' +
               Json.ClassName + NL + Json.AsJSON);
-
-          AtlasURL := ChangeURIExt(URL, '.atlas');
-          if URIFileExists(AtlasURL) then
-          begin
-            Atlas := ReadAtlas(AtlasURL);
+            Skeleton := TSkeleton.Create;
             try
-              WritelnLog('Spine', Format('Atlas read, pages: %d', [Atlas.Pages.Count]));
-            finally FreeAndNil(Atlas) end;
+              Skeleton.Parse(Json);
+              WritelnLog('Spine', Format('Skeleton read, bones: %d, slots: %d, skins: %d',
+                [Skeleton.Bones.Count,
+                 Skeleton.Slots.Count,
+                 Skeleton.Skins.Count]));
+            finally FreeAndNil(Skeleton) end;
           end;
         except FreeAndNil(Result); raise end;
       finally FreeAndNil(Json) end;
@@ -292,4 +629,3 @@ begin
 end;
 
 end.
-
