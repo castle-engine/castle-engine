6c119bd57168a318e15874912bfcf2a5489aa53d
Michalis Kamburelis <michalis.kambi@gmail.com>
2014-12-22 04:25:36 +0000

    Fix compilation: remove references to non-existing outline fonts.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@14242 afe8dcc5-9094-451b-8773-5f495042a737

--- src/fonts/castlefont2pascal.pas
+++ src/fonts/castlefont2pascal.pas
@@ -13,22 +13,12 @@
   ----------------------------------------------------------------------------
 }
 
-{ Converting fonts (TOutlineFontData or TTextureFontData) to Pascal source code. }
+{ Converting fonts (TTextureFontData) to Pascal source code. }
 unit CastleFont2Pascal;
 
 interface
 
-uses CastleOutlineFontData, CastleTextureFontData, Classes;
-
-{ @noAutoLinkHere }
-procedure Font2Pascal(const Font: TOutlineFontData;
-  const UnitName, PrecedingComment, FontFunctionName: string; Stream: TStream);
-  overload;
-
-{ @noAutoLinkHere }
-procedure Font2Pascal(const Font: TOutlineFontData;
-  const UnitName, PrecedingComment, FontFunctionName: string;
-  const OutURL: string); overload;
+uses CastleTextureFontData, Classes;
 
 { @noAutoLinkHere }
 procedure Font2Pascal(const Font: TTextureFontData;
@@ -83,123 +73,6 @@ begin
 end;
 
 { Font2Pascal ----------------------------------------------------- }
-
-procedure Font2Pascal(const Font: TOutlineFontData;
-  const UnitName, PrecedingComment, FontFunctionName: string; Stream: TStream);
-
-  procedure WriteOutlineChar(Stream: TStream; c: char; TTChar: POutlineChar);
-
-    function PolygonKindToString(PolygonKind: TPolygonKind): string;
-    begin
-      case PolygonKind of
-        pkNewPolygon : Result := 'pkNewPolygon';
-        pkLines : Result := 'pkLines';
-        pkBezier : Result := 'pkBezier';
-        pkPoint : Result := 'pkPoint';
-        else raise EInternalError.Create(
-          'PolygonKindToString: Undefined value of TPolygonKind');
-      end;
-    end;
-
-  var
-    CharName: string;
-    I, WrittenInLine, WriteItemsCount: Cardinal;
-  begin
-    case c of
-      ' ':CharName := 'space = ';
-      '}':CharName := 'right curly brace = ';
-      else
-        (* Avoid C = '{' or '}', to not activate accidentaly
-           ObjFpc nested comments feature. *)
-        if c in ([' '..#255] - ['{', '}']) then
-          CharName := ''''+c+''' = ' else
-          CharName := '';
-    end;
-    WriteStr(Stream, Format('  Char%d : packed record { %s#%d }' +nl,
-      [Ord(c), CharName, Ord(c)]));
-
-    if TTChar^.Info.ItemsCount = 0 then
-      WriteItemsCount := 1 else
-      WriteItemsCount := TTChar^.Info.ItemsCount;
-
-    WriteStr(Stream, Format(
-      '    Info : TOutlineCharInfo;'+NL+
-      '    Items : array[0..%d] of TOutlineCharItem;'+NL+
-      '  end ='+NL+
-      '  ( Info : ( MoveX: %g; MoveY: %g; Height: %g;'+NL+
-      '             PolygonsCount: %d;'+NL+
-      '             ItemsCount: %d );' +NL+
-      '    Items :'+NL+
-      '    ( ',
-      [ WriteItemsCount-1,
-        TTChar^.Info.MoveX, TTChar^.Info.MoveY, TTChar^.Info.Height,
-        TTChar^.Info.PolygonsCount,
-        TTChar^.Info.ItemsCount ]));
-
-    if TTChar^.Info.ItemsCount = 0 then
-      WriteStr(Stream, '(Kind: pkPoint; x:0; y:0) { dummy item }') else
-    begin
-      WrittenInLine := 0;
-      { TTChar^.Info.ItemsCount > 0 so TTChar^.Info.ItemsCount-1 >= 0.
-        So i may be Cardinal. }
-      for i := 0 to TTChar^.Info.ItemsCount-1 do
-      begin
-        WriteStr(Stream, '(Kind: ' + PolygonKindToString(TTChar^.Items[i].Kind) + '; ');
-        if TTChar^.Items[i].Kind = pkPoint then
-         WriteStr(Stream, Format('x: %g; y: %g)', [TTChar^.Items[i].x, TTChar^.Items[i].y])) else
-         WriteStr(Stream, Format('Count: %d)', [TTChar^.Items[i].Count]));
-
-        Inc(WrittenInLine);
-        if i < TTChar^.Info.ItemsCount-1 then
-        begin
-          WriteStr(Stream, ', ');
-          if TTChar^.Items[i+1].Kind <> pkPoint then
-            begin WriteStr(Stream, NL+'      '); WrittenInLine := 0 end else
-          if WrittenInLine mod 30 = 0 then
-            begin WriteStr(Stream, NL+'          '); WrittenInLine := 0 end;
-        end;
-      end;
-    end;
-    WriteStr(Stream, NL+'    )'+NL+'  );'+NL+nl);
-  end;
-
-  procedure WriteUnitEnd(Stream: TStream; const FontFunctionName, FontTypeName: string);
-  var
-    i: Integer;
-  begin
-    WriteStr(Stream, '  Data : ' + FontTypeName + 'Array = (' +NL+
-      '    ');
-    for i := 0 to 255 do
-    begin
-      WriteStr(Stream, '@Char' + IntToStr(i));
-      if i < 255 then WriteStr(Stream, ', ');
-      if (i+1) mod 20 = 0 then WriteStr(Stream, NL+'    ');
-    end;
-
-    WriteStr(Stream, ');' +NL+
-       '' +NL+
-       'initialization' +NL+
-       '  FFont := ' + FontTypeName + '.Create;' +NL+
-       '  FFont.Data := Data;' +NL+
-       'finalization' +NL+
-       '  FreeAndNil(FFont);' +NL+
-       'end.' +nl);
-  end;
-
-var
-  c: char;
-begin
-  WriteUnitBegin(Stream, UnitName, PrecedingComment,
-    'CastleOutlineFontData', FontFunctionName, 'TOutlineFontData');
-
-  WriteStr(Stream, 'const' +nl);
-  for c := #0 to #255 do
-  begin
-    WriteOutlineChar(Stream, c, Font.Data[c]);
-  end;
-
-  WriteUnitEnd(Stream, FontFunctionName, 'TOutlineFontData');
-end;
 
 procedure Font2Pascal(const Font: TTextureFontData;
   const UnitName, PrecedingComment, FontFunctionName: string; Stream: TStream);
@@ -270,18 +143,6 @@ begin
 end;
 
 { OutURL versions ---------------------------------------------------- }
-
-procedure Font2Pascal(const Font: TOutlineFontData;
-  const UnitName, PrecedingComment, FontFunctionName: string;
-  const OutURL: string); overload;
-var
-  Stream: TStream;
-begin
-  Stream := URLSaveStream(OutURL);
-  try
-    Font2Pascal(Font, UnitName, PrecedingComment, FontFunctionName, Stream);
-  finally Stream.Free end;
-end;
 
 procedure Font2Pascal(const Font: TTextureFontData;
   const UnitName, PrecedingComment, FontFunctionName: string;
