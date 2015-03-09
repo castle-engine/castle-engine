cb55d41d32fcb8246d88d0fe53bc58c459c70a0a
Michalis Kamburelis <michalis.kambi@gmail.com>
2015-03-09 22:13:07 +0000

    UI state helper class.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle_game_engine@14447 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ src/window/castleuistate.pas
@@ -0,0 +1,216 @@
+{
+  Copyright 2015-2015 Michalis Kamburelis.
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
+{ UI state (TUIState). }
+unit CastleUIState;
+
+interface
+
+uses Classes, FGL,
+  CastleConfig, CastleKeysMouse, CastleImages, CastleUIControls,
+  CastleGLImages, CastleVectors;
+
+type
+  { UI state, a useful singleton to manage the state of your game UI.
+    Only one such state is @italic(current) at a given time.
+    The current state is present on the list of container controls
+    (along with other state-specific controls, that should be added/removed
+    to the controls list in @link(Start) / @link(Finish) methods.)
+
+    Expressed as UI control, that will be placed under
+    state-specific UI controls (that should be added in overridden @link(Start)
+    method by @code(Container.Controls.InsertFront)).
+    This way it
+
+    @unorderedList(
+      @item(catches press/release and similar events, when no other
+        state-specific control handled them,)
+      @item(catches update, GL context open/close and other useful events,)
+      @item(can have it's own draw function, to directly draw UI.)
+    ) }
+  TUIState = class(TUIControl)
+  private
+  type
+    TDataImage = class
+      Image: TCastleImage;
+      GLImage: TGLImage;
+      destructor Destroy; override;
+    end;
+    TDataImageList = specialize TFPGObjectList<TDataImage>;
+  var
+    FDataImages: TDataImageList;
+    class var FCurrent: TUIState;
+    class function GetCurrent: TUIState; static;
+    class procedure SetCurrent(const Value: TUIState); static;
+  protected
+    { Adds image to the list of automatically loaded images for this state.
+      Path is automatically wrapped in ApplicationData(Path) to get URL.
+      The basic image (TCastleImage) is loaded immediately,
+      and always available, under DataImage(Index).
+      The OpenGL image resource (TGLImage) is loaded when GL context
+      is active, available under DataGLImage(Index).
+      Where Index is the return value of this method. }
+    function AddDataImage(const Path: string): Integer;
+    function DataImage(const Index: Integer): TCastleImage;
+    function DataGLImage(const Index: Integer): TGLImage;
+    { Container on which we work. By default, this is Application.MainWindow. }
+    function Container: TUIContainer; virtual;
+  public
+    class property Current: TUIState read GetCurrent write SetCurrent;
+
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure Start; virtual;
+    procedure Finish; virtual;
+
+    function PositionInside(const Position: TVector2Single): boolean; override;
+    procedure GLContextOpen; override;
+    procedure GLContextClose; override;
+  end;
+
+implementation
+
+uses SysUtils,
+  CastleWindow, CastleWarnings, CastleFilesUtils;
+
+{ TUIState.TDataImage ---------------------------------------------------------- }
+
+destructor TUIState.TDataImage.Destroy;
+begin
+  FreeAndNil(Image);
+  FreeAndNil(GLImage);
+  inherited;
+end;
+
+{ TUIState --------------------------------------------------------------------- }
+
+class function TUIState.GetCurrent: TUIState;
+begin
+  Result := FCurrent;
+end;
+
+class procedure TUIState.SetCurrent(const Value: TUIState);
+var
+  ControlsCount, PositionInControls: Integer;
+  NewControls: TUIControlList;
+begin
+  if FCurrent <> Value then
+  begin
+    if FCurrent <> nil then
+    begin
+      FCurrent.Container.Controls.Remove(FCurrent);
+      FCurrent.Finish;
+    end;
+    FCurrent := Value;
+    if FCurrent <> nil then
+    begin
+      NewControls := FCurrent.Container.Controls;
+      ControlsCount := NewControls.Count;
+      FCurrent.Start;
+      { actually insert FCurrent, this will also call GLContextOpen
+        and ContainerResize.
+        However, check first that we're still within the same state,
+        to safeguard from the fact that FCurrent.Start changed state
+        (like the loading state, that changes to play state immediately in start). }
+      if FCurrent = Value then
+      begin
+        PositionInControls := NewControls.Count - ControlsCount;
+        if PositionInControls < 0 then
+        begin
+          OnWarning(wtMinor, 'State', 'TUIState.Start removed some controls from container');
+          PositionInControls := 0;
+        end;
+        NewControls.Insert(PositionInControls, FCurrent);
+      end;
+    end;
+  end;
+end;
+
+function TUIState.Container: TUIContainer;
+begin
+  Result := Application.MainWindow.Container;
+end;
+
+constructor TUIState.Create(AOwner: TComponent);
+begin
+  inherited;
+  FDataImages := TDataImageList.Create;
+end;
+
+destructor TUIState.Destroy;
+begin
+  FreeAndNil(FDataImages);
+  inherited;
+end;
+
+procedure TUIState.Start;
+begin
+end;
+
+procedure TUIState.Finish;
+begin
+end;
+
+function TUIState.AddDataImage(const Path: string): Integer;
+var
+  DI: TDataImage;
+begin
+  DI := TDataImage.Create;
+  DI.Image := LoadImage(ApplicationData(Path), []);
+  if GLInitialized then
+    DI.GLImage := TGLImage.Create(DI.Image, true);
+  Result := FDataImages.Add(DI);
+end;
+
+function TUIState.DataImage(const Index: Integer): TCastleImage;
+begin
+  Result := FDataImages[Index].Image;
+end;
+
+function TUIState.DataGLImage(const Index: Integer): TGLImage;
+begin
+  Result := FDataImages[Index].GLImage;
+end;
+
+function TUIState.PositionInside(const Position: TVector2Single): boolean;
+begin
+  Result := true;
+end;
+
+procedure TUIState.GLContextOpen;
+var
+  I: Integer;
+  DI: TDataImage;
+begin
+  inherited;
+  for I := 0 to FDataImages.Count - 1 do
+  begin
+    DI := FDataImages[I];
+    if DI.GLImage = nil then
+      DI.GLImage := TGLImage.Create(DI.Image, true);
+  end;
+end;
+
+procedure TUIState.GLContextClose;
+var
+  I: Integer;
+begin
+  if FDataImages <> nil then
+    for I := 0 to FDataImages.Count - 1 do
+      FreeAndNil(FDataImages[I].GLImage);
+  inherited;
+end;
+
+end.
