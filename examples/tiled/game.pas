8839d9b318cc67202273f5a83bd687610137322c
Tomasz W <tomekwcastle@nela.pw>
2016-07-09 20:48:52 +0200

    Tiled demo application stumb

---
+++ examples/tiled/game.pas
@@ -0,0 +1,66 @@
+{$mode objfpc}{$H+}
+
+{ Implements the game logic, independent from mobile / standalone. }
+unit Game;
+
+interface
+
+uses CastleWindowTouch;
+
+var
+  Window: TCastleWindowTouch;
+
+implementation
+
+uses SysUtils, CastleWindow, CastleScene, CastleControls,
+  CastleFilesUtils, CastleSceneCore, CastleKeysMouse, CastleColors;
+
+var
+  ExampleImage: TCastleImageControl;
+  ExampleScene: TCastleScene;
+
+{ routines ------------------------------------------------------------------- }
+
+{ One-time initialization of resources. }
+procedure ApplicationInitialize;
+begin
+  
+end;
+
+procedure WindowRender(Container: TUIContainer);
+begin
+  // ... custom rendering code
+  UIFont.Print(10, 10, Yellow, Format('FPS: %f', [Container.Fps.RealTime]));
+end;
+
+procedure WindowUpdate(Container: TUIContainer);
+begin
+  // ... do something every frame
+end;
+
+procedure WindowPress(Container: TUIContainer; const Event: TInputPressRelease);
+begin
+  // ... react to press of key, mouse, touch
+end;
+
+function MyGetApplicationName: string;
+begin
+  Result := 'Tiled demo';
+end;
+
+initialization
+  { This sets SysUtils.ApplicationName.
+    It is useful to make sure it is correct (as early as possible)
+    as our log routines use it. }
+  OnGetApplicationName := @MyGetApplicationName;
+
+  { initialize Application callbacks }
+  Application.OnInitialize := @ApplicationInitialize;
+
+  { create Window and initialize Window callbacks }
+  Window := TCastleWindowTouch.Create(Application);
+  Application.MainWindow := Window;
+  Window.OnRender := @WindowRender;
+  Window.OnUpdate := @WindowUpdate;
+  Window.OnPress := @WindowPress;
+end.
---
+++ examples/tiled/tiled_demo_android.lpr
@@ -0,0 +1,5 @@
+{$mode objfpc}{$H+}
+library tiled_demo_android;
+uses CastleAndroidNativeAppGlue, Game;
+exports ANativeActivity_onCreate;
+end.
