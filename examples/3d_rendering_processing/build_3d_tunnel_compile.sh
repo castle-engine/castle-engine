33d2459b198d5ae54058c511e33cf976a4f44571
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-11-02 05:42:56 +0100

    Fix shell script

--- examples/3d_rendering_processing/build_3d_tunnel_compile.sh
+++ examples/3d_rendering_processing/build_3d_tunnel_compile.sh
@@ -6,6 +6,6 @@ set -eu
 # Or just do "make examples" in base castle_game_engine directory.
 
 # Allow calling this script from it's dir.
-if [ -f build_3d_tunnel_compile.lpr ]; then cd ../../; fi
+if [ -f build_3d_tunnel.lpr ]; then cd ../../; fi
 
-fpc -dRELEASE @castle-fpc.cfg examples/3d_rendering_processing/build_3d_tunnel_compile.lpr
+fpc -dRELEASE @castle-fpc.cfg examples/3d_rendering_processing/build_3d_tunnel.lpr
