5fb17f55a6050ac98d6ead5963196dd62e43f5cb
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-04-11 22:02:14 +0200

    compile-iOS improvements more
    - use -dDEBUG/-dRELEASE
    - output dir inside ios-output
    - use run_build function
    - specify simulator/iphone SDK (other docs say we should - FPC getting
      started, fpcupdeluxe defaults...)

--- src/library/compile-iOS.sh
+++ src/library/compile-iOS.sh
@@ -20,6 +20,10 @@ FPC_SIM64_COMPILER="fpc -Px86_64 -V3.0.3"
 FPC_ARM_COMPILER="fpc -Parm"
 FPC_ARM64_COMPILER="fpc -Paarch64"
 
+# debug or release (for the exact meaning, see ../../castle-fpc.cfg)
+FPC_CONFIG="-dDEBUG"
+#FPC_CONFIG="-dRELEASE"
+
 # Functions ------------------------------------------------------------------
 
 run_logging ()
@@ -30,15 +34,16 @@ run_logging ()
 }
 
 # Run libtool to create library from .o files referenced in $1/link.res.
-# Output library is $2.
+# Output library is $1/$EXECUTABLE_NAME
 # Also update global OUTPUT_LIBRARIES_COUNT and OUTPUT_LIBRARIES vars.
 run_libtool ()
 {
-  local LINK_RES_DIR="$1"
-  local OUTPUT_LIB="$2"
-  shift 2
+  local DIR="$1"
+  shift 1
 
-  LINK_RES="${LINK_RES_DIR}"/link.res
+  local LINK_RES="${DIR}/link.res"
+  local OUTPUT_LIB="${DIR}/${EXECUTABLE_NAME}"
+
   grep '\.o$' "${LINK_RES}" > compile_ios_filelist.tmp
   run_logging libtool -static -o "${OUTPUT_LIB}" -filelist compile_ios_filelist.tmp
   if [ ! -e "${OUTPUT_LIB}" ]; then
@@ -49,6 +54,22 @@ run_libtool ()
 
   OUTPUT_LIBRARIES[$OUTPUT_LIBRARIES_COUNT]="${OUTPUT_LIB}"
   OUTPUT_LIBRARIES_COUNT=$(( $OUTPUT_LIBRARIES_COUNT + 1 ))
+}
+
+# Run fpc and libtool to build library inside dir $1.
+# The $2 and following parameters should specify OS/CPU specific
+# FPC command and switches.
+run_build ()
+{
+  local OUTPUT_DIR="$1"
+
+  mkdir -p "${OUTPUT_DIR}"
+  # Note: $FPC_CONFIG must be specified before $FPC_COMMON,
+  # as $FPC_CONFIG contains -dDEBUG / -dRELEASE that determines
+  # switches set by $FPC_COMMON inside @castle-fpc.cfg.
+  run_logging "$@" $FPC_CONFIG $FPC_COMMON \
+    "-FU${OUTPUT_DIR}" "-o${OUTPUT_DIR}/${EXECUTABLE_NAME}" "${FPC_MAIN_FILE}"
+  run_libtool "${OUTPUT_DIR}"
 }
 
 # Main code ------------------------------------------------------------------
@@ -62,51 +83,27 @@ EXECUTABLE_NAME="cge_library.a"
 FPC_MAIN_FILE="src/library/castleengine.lpr"
 FPC_COMMON="-Cn -WP5.1 ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg -dCASTLE_WINDOW_LIBRARY"
 LIBRARY_PATH="src/library"
-PROJECT_DIR="."
-OUTPUT_SIM="${LIBRARY_PATH}/out-iphonesim"
-OUTPUT_ARM="${LIBRARY_PATH}/out-armv7"
-OUTPUT_SIM64="${LIBRARY_PATH}/out-iphonesim64"
-OUTPUT_ARM64="${LIBRARY_PATH}/out-arm64"
-#debug
-FPC_CONFIG="-CirotR -gltw -Sa"
-#release
-#FPC_CONFIG="-O2"
-#FPC_CONFIG="-gw"
-
-SIM_LIB=$OUTPUT_SIM/$EXECUTABLE_NAME
-ARM_LIB=$OUTPUT_ARM/$EXECUTABLE_NAME
-SIM64_LIB=$OUTPUT_SIM64/$EXECUTABLE_NAME
-ARM64_LIB=$OUTPUT_ARM64/$EXECUTABLE_NAME
+OUTPUT_SIM="${LIBRARY_PATH}/ios-output/i386-iphonesim"
+OUTPUT_SIM64="${LIBRARY_PATH}/ios-output/x86_64-iphonesim"
+OUTPUT_ARM="${LIBRARY_PATH}/ios-output/arm-darwin"
+OUTPUT_ARM64="${LIBRARY_PATH}/ios-output/aarch64-darwin"
+SIMULATOR_SDK='/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk'
+DEVICE_SDK='/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk'
 
 declare -a OUTPUT_LIBRARIES
 OUTPUT_LIBRARIES_COUNT=0
 
 # compile for iPhone simulator
 if [ $COMPILE_SIM -eq 1 ]; then
-  mkdir -p $OUTPUT_SIM
-  OUT_FILES="-FU$OUTPUT_SIM -o$SIM_LIB"
-  run_logging $FPC_SIM_COMPILER -Tiphonesim $FPC_COMMON $FPC_CONFIG $OUT_FILES "${FPC_MAIN_FILE}"
-  run_libtool "$OUTPUT_SIM" "$SIM_LIB"
-
-  # 64 bit
-  mkdir -p $OUTPUT_SIM64
-  OUT_FILES="-FU$OUTPUT_SIM64 -o$SIM64_LIB"
-  run_logging $FPC_SIM64_COMPILER -Tiphonesim $FPC_COMMON $FPC_CONFIG $OUT_FILES "${FPC_MAIN_FILE}"
-  run_libtool "$OUTPUT_SIM64" "$SIM64_LIB"
+  run_build "${OUTPUT_SIM}"   $FPC_SIM_COMPILER   -Tiphonesim "-XR${SIMULATOR_SDK}"
+  run_build "${OUTPUT_SIM64}" $FPC_SIM64_COMPILER -Tiphonesim "-XR${SIMULATOR_SDK}"
 fi
 
 # compile for a physical device
 if [ $COMPILE_ARM -eq 1 ]; then
-  mkdir -p $OUTPUT_ARM
-  OUT_FILES="-FU$OUTPUT_ARM -o$ARM_LIB"
-  run_logging $FPC_ARM_COMPILER -Cparmv7 -Cfvfpv3 $FPC_COMMON $FPC_CONFIG $OUT_FILES "${FPC_MAIN_FILE}"
-  run_libtool "$OUTPUT_ARM" "$ARM_LIB"
-
-  # 64 bit
-  mkdir -p $OUTPUT_ARM64
-  OUT_FILES="-FU$OUTPUT_ARM64 -o$ARM64_LIB"
-  run_logging $FPC_ARM64_COMPILER -dCPUARM64 $FPC_COMMON $FPC_CONFIG $OUT_FILES "${FPC_MAIN_FILE}"
-  run_libtool "$OUTPUT_ARM64" "$ARM64_LIB"
+  run_build "${OUTPUT_ARM}"   $FPC_ARM_COMPILER   -Tdarwin "-XR${DEVICE_SDK}" -Cparmv7 -Cfvfpv3
+  # -dCPUARM64 is checked by castleconf.inc
+  run_build "${OUTPUT_ARM64}" $FPC_ARM64_COMPILER -Tdarwin "-XR${DEVICE_SDK}" -dCPUARM64
 fi
 
 if [ $OUTPUT_LIBRARIES_COUNT -eq 0 ]; then
@@ -115,8 +112,8 @@ if [ $OUTPUT_LIBRARIES_COUNT -eq 0 ]; then
 else
   echo "----------------------------------------------------------------------"
   echo "Combining together into a single library:"
-  run_logging libtool -static "${OUTPUT_LIBRARIES[@]}" -o "$PROJECT_DIR/$EXECUTABLE_NAME"
+  run_logging libtool -static "${OUTPUT_LIBRARIES[@]}" -o "./$EXECUTABLE_NAME"
   mv $EXECUTABLE_NAME "${LIBRARY_PATH}"
   echo "----------------------------------------------------------------------"
-  echo "Done, output inside ${LIBRARY_PATH}${EXECUTABLE_NAME}"
+  echo "Done, output inside ${LIBRARY_PATH}/${EXECUTABLE_NAME}"
 fi
