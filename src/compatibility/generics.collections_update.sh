430fe123a5b6c2b283b8c7731519d783ab4f9ee7
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-06-22 19:56:49 +0200

    Download generics.collections_update.sh

---
+++ src/compatibility/generics.collections_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+# Get the Generics.Collections units, for use in FPC < 3.1.1
+# (later FPC contains these already).
+#
+# Note: we don't use git submodules for this
+# (see https://git-scm.com/book/en/v2/Git-Tools-Submodules )
+# because git doesn't download the submodules automatically.
+# It would need "git submodule init", "git submodule update",
+# and the primary purpose of this directory
+# is to make CGE "work out-of-the-box" for people with older FPC.
+
+rm -Rf generics.collections
+git clone https://github.com/dathox/generics.collections
