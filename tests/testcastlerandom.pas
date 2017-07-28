3b38f248b89ff822cd5cfb5bf6fa743f56468234
eugeneloza <Loza@i.ua>
2017-07-29 02:45:47 +0300

    Change AssertTrue to AssertEquals when possible in TestCastleRandom fixes https://github.com/castle-engine/castle-engine/pull/71#issuecomment-318784888

--- tests/testcastlerandom.pas
+++ tests/testcastlerandom.pas
@@ -34,20 +34,21 @@ uses CastleRandom;
 
 procedure TTestCastleRandom.TestHash;
 begin
-  //zero string Hash
-  AssertTrue(StringToHash('') = 0);
-  AssertTrue(StringToHash('1') = %01001001001101000010111110101111);
-  //zero seed Hash
-  AssertTrue(StringToHash('String to hash test') = %11100000010001101011001110011100);
-  //seeded hash
-  AssertTrue(StringToHash('String to hash test',$9747b28c) = %01111101100111100000101011011100);
+  // Zero string hash
+  AssertEquals(%00000000000000000000000000000000, StringToHash(''));
+  // Short string hash
+  AssertEquals(%01001001001101000010111110101111, StringToHash('1'));
+  // Zero seed hash
+  AssertEquals(%11100000010001101011001110011100, StringToHash('String to hash test'));
+  // Seeded hash
+  AssertEquals(%01111101100111100000101011011100, StringToHash('String to hash test',$9747b28c));
 end;
 
 procedure TTestCastleRandom.TestRandom;
 const NTests = 10000000;
 var Rnd: TCastleRandom;
-    i: integer;
-    Sum: double;
+    i: Integer;
+    Sum: Double;
 begin
   Rnd := TCastleRandom.Create;
 
@@ -62,9 +63,9 @@ begin
    is random, and therefore there's always a tiny chance that it'll fail the test}
   Sum := 0;
   for i := 0 to NTests*10 do
-    Sum += Rnd.random;
-  //checking random against shot noise
-  AssertTrue(abs(Sum/(NTests*10)-0.5) <= 2/sqrt(NTests*10));
+    Sum += Rnd.Random;
+  //checking random against shot noise (with extended margin, just in case)
+  AssertTrue(Abs(Sum/(NTests*10)-0.5) <= 2/Sqrt(NTests*10));
 
   FreeAndNil(Rnd);
 end;
