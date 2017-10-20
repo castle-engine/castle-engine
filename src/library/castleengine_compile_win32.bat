1f70ece6188794593b48f90b528c9fb8c177fef7
Jan Adamec <jadamec@gmail.com>
2017-10-20 17:21:26 +0200

    Improve fpc.exe folder auto-detection for library compile scripts on Windows

--- src/library/castleengine_compile_win32.bat
+++ src/library/castleengine_compile_win32.bat
@@ -1,19 +1,16 @@
 :: Call this script from this directory
 @echo off
+setlocal
 
 :: First auto-detect fpc.exe location
-
-:: Find FPC version folder under Lazarus installation, e.g. c:\lazarus\fpc\3.0.2 
-for /d %%D in (c:\lazarus\fpc\*.*) do set FPC_VER_DIR=%%D
-echo fpc version folder:  %FPC_VER_DIR%
-
-:: Find installed FPC platform (win32 or win64), e.g. c:\lazarus\fpc\3.0.2\bin\x86_64-win64 
-for /d %%D in (%FPC_VER_DIR%\bin\*.*) do set FPC_EXE_DIR=%%D
-echo fpc platform folder: %FPC_EXE_DIR%
+for /f "tokens=*" %%i in ('..\..\tools\fpc_path_detect.bat') do set FPC_EXE_DIR=%%i
+echo fpc folder: %FPC_EXE_DIR%fpc.exe
 
 :: Supposing you have cross-compilers for win32 or win64 in that folder, let's compile the library
+set OUT_DIR=build-win32
+if not exist %OUT_DIR% mkdir %OUT_DIR%
 
 :: Change to the directory where castle-fpc.cfg is.
 cd ..\..
 
-%FPC_EXE_DIR%\fpc.exe -fPIC -dRELEASE @castle-fpc.cfg -dCASTLE_WINDOW_LIBRARY src/library/castleengine.lpr -Pi386 -ocastleengine32.dll
+%FPC_EXE_DIR%fpc.exe -fPIC -dRELEASE @castle-fpc.cfg -dCASTLE_WINDOW_LIBRARY src/library/castleengine.lpr -Pi386 -FEsrc/library/%OUT_DIR%
