b56a8ea645d04b5387c0debc3578b350361b962d
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-12-02 02:16:04 +0100

    Move binaries up in castle-engine_compile.sh

--- tools/build-tool/castle-engine_compile.sh
+++ tools/build-tool/castle-engine_compile.sh
@@ -11,3 +11,11 @@ if [ -f code/castle-engine.lpr ]; then cd ../../; fi
 fpc -dRELEASE @castle-fpc.cfg \
   -Futools/build-tool/embedded_images/ \
   tools/build-tool/code/castle-engine.lpr
+
+# move binaries up
+if [ -f tools/build-tool/code/castle-engine.exe ]; then
+  mv -f tools/build-tool/code/castle-engine.exe tools/build-tool/
+fi
+if [ -f tools/build-tool/code/castle-engine ]; then
+  mv -f tools/build-tool/code/castle-engine tools/build-tool/
+fi
