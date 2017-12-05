10dea5ac06a4f4a0580e205e3f21257059605916
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-12-05 03:35:25 +0100

    Better portable_game_skeleton code

--- examples/portable_game_skeleton/game.pas
+++ examples/portable_game_skeleton/game.pas
@@ -1,21 +1,36 @@
-{$mode objfpc}{$H+}
+{ Game initialization and logic.
+  You will probably split larger games into more units,
+  and then this main unit will just initialize everything else.
 
-{ Implements the game logic, independent from mobile / standalone. }
+  This code is independent from mobile / standalone platforms.
+  It will be used by the appropriate .lpr file for desktop, Android or iOS.
+
+  Feel free to use this code as a starting point for your own projects.
+  (This code is in public domain, unlike most other CGE code which
+  is covered by the LGPL license variant, see the COPYING.txt file.) }
 unit Game;
+
+{ Compilation options adviced by the Castle Game Engine.
+  But actually you can use any options suitable for you (although you will
+  most likely want to use ObjFpc or Delphi mode, to have classes support).
+
+  Feel free to remove this line if you know that you configure proper
+  compilation options elsewhere (e.g. in the Lazarus project options;
+  the CGE build tool automatically defines these options). }
+{$mode objfpc}{$H+}{$J-}
 
 interface
 
-uses CastleWindowTouch;
+implementation
+
+uses SysUtils,
+  CastleWindowTouch, CastleWindow, CastleScene, CastleControls, CastleLog,
+  CastleFilesUtils, CastleSceneCore, CastleKeysMouse, CastleColors,
+  CastleUIControls;
 
 var
   Window: TCastleWindowTouch;
-
-implementation
-
-uses SysUtils, CastleWindow, CastleScene, CastleControls, CastleLog,
-  CastleFilesUtils, CastleSceneCore, CastleKeysMouse, CastleColors;
-
-var
+  Status: TCastleLabel;
   ExampleImage: TCastleImageControl;
   ExampleScene: TCastleScene;
 
@@ -24,15 +39,27 @@ var
 { One-time initialization of resources. }
 procedure ApplicationInitialize;
 begin
-  { This is just an example of creating a simple 2D control
-    (TCastleImageControl) and 3D object (TCastleScene). }
+  { For a scalable UI (adjusts to any window size in a smart way), use UIScaling }
+  Window.Container.UIReferenceWidth := 1024;
+  Window.Container.UIReferenceHeight := 768;
+  Window.Container.UIScaling := usEncloseReferenceSize;
 
-  ExampleImage := TCastleImageControl.Create(Window);
+  { Show a label with some text }
+  Status := TCastleLabel.Create(Application);
+  Status.Anchor(vpTop, -10);
+  Status.Anchor(hpRight, -10);
+  Status.Color := Yellow; // you could use "Vector4(1, 1, 0, 1)" instead of Yellow
+  Window.Controls.InsertFront(Status);
+
+  { Show 2D image }
+  ExampleImage := TCastleImageControl.Create(Application);
   ExampleImage.URL := ApplicationData('example_image.png');
   ExampleImage.Bottom := 100;
   ExampleImage.Left := 100;
   Window.Controls.InsertFront(ExampleImage);
 
+  { Show a 3D object (TCastleScene) inside a Window.SceneManager
+    (which acts as a full-screen viewport by default). }
   ExampleScene := TCastleScene.Create(Application);
   ExampleScene.Load(ApplicationData('example_scene.x3dv'));
   ExampleScene.Spatial := [ssRendering, ssDynamicCollisions];
@@ -44,12 +71,12 @@ end;
 procedure WindowRender(Container: TUIContainer);
 begin
   // ... custom rendering code
-  UIFont.Print(10, 10, Yellow, Format('FPS: %f', [Container.Fps.RealTime]));
 end;
 
 procedure WindowUpdate(Container: TUIContainer);
 begin
   // ... do something every frame
+  Status.Caption := Format('FPS: %f', [Container.Fps.RealTime]);
 end;
 
 procedure WindowPress(Container: TUIContainer; const Event: TInputPressRelease);
