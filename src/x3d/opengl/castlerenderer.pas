c7f8f8c0535b17fb18f44a277a97f0588bc7d02a
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-12-10 03:28:39 +0100

    Remove USE_VRML_TRIANGULATION option, was using fixed-function

--- src/x3d/opengl/castlerenderer.pas
+++ src/x3d/opengl/castlerenderer.pas
@@ -104,54 +104,16 @@
     )
   )
 
-  @bold(Rendered TrianglesCount and VerticesCount:)
+  The renderer uses arrays in GPU-friendly format defined by TGeometryArrays.
 
-  This renderer uses the same triangles and vertices counts as
+  They have the same number of triangles and vertices as
   calculated by TAbstractGeometryNode.Triangulate,
   TAbstractGeometryNode.LocalTriangulate,
   TAbstractGeometryNode.TrianglesCount,
-  TAbstractGeometryNode.VerticesCount, with OverTriangulate = @true.
-
-  Note that it doesn't mean that we actually call TAbstractGeometryNode.Triangulate
-  for VRML rendering. In fact, currently we don't, and it allows us to be
-  much faster (for starters, rendering by indexes, or quad strips,
-  that would not be possible by generic implementation calling
-  TAbstractGeometryNode.Triangulate).
-  But our rendering methods generate the same triangles
-  as TAbstractGeometryNode.Triangulate.
-
-  Although for debug purposes, we have a renderer using
-  TShape.LocalTriangulate, see notes about
-  USE_VRML_TRIANGULATION in the source code.
+  TAbstractGeometryNode.VerticesCount (with OverTriangulate = @true).
 }
 
 unit CastleRenderer;
-
-{ When you define USE_VRML_TRIANGULATION, an alternative
-  rendering method will be used. Each node will be triangulated
-  using TShape.LocalTriangulate, and each generated triangle
-  will be passed to OpenGL.
-
-  This is usable only for TShape.LocalTriangulate testing.
-  - It's slower than the normal rendering method,
-    as triangles are passed to the OpenGL in the most naive immediate way,
-    without any vertex arrays or VBOs. In fact, it will not work with
-    OpenGL >= 3.
-  - Things that are not expressed as triangles
-    (IndexedLineSet, PointSet) will not be rendered at all.
-  - It lacks some features, because the triangulating routines
-    do not return enough information. For example, multi-texturing
-    does not work (correctly), as TTriangleEvent currently only passes
-    the coordinates for first texture unit.
-}
-{ $define USE_VRML_TRIANGULATION}
-
-{$ifdef USE_VRML_TRIANGULATION}
-  {$ifdef RELEASE}
-    {$fatal Undefine USE_VRML_TRIANGULATION for CastleRenderer,
-      you don't want to use this in RELEASE version. }
-  {$endif}
-{$endif}
 
 {$I castleconf.inc}
 
@@ -912,13 +874,6 @@ type
       out Enabled, Volumetric: boolean;
       out VolumetricDirection: TVector3;
       out VolumetricVisibilityStart: Single);
-
-    {$ifdef USE_VRML_TRIANGULATION}
-    procedure DrawTriangle(Shape: TObject;
-      const Position: TTriangle3;
-      const Normal: TTriangle3; const TexCoord: TTriangle4;
-      const Face: TFaceIndex);
-    {$endif}
 
     { If multitexturing available, this sets currently active texture unit.
       TextureUnit is newly active unit, this is added to GL_TEXTURE0.
@@ -2611,23 +2566,6 @@ begin
   TGLSLProgram.Current := nil;
 end;
 
-{$ifdef USE_VRML_TRIANGULATION}
-procedure TGLRenderer.DrawTriangle(Shape: TObject;
-  const Position: TTriangle3;
-  const Normal: TTriangle3; const TexCoord: TTriangle4;
-  const Face: TFaceIndex);
-var
-  I: Integer;
-begin
-  for I := 0 to 2 do
-  begin
-    glNormalv(Normal[I]);
-    glTexCoordv(TexCoord[I]);
-    glVertexv(Position[I]);
-  end;
-end;
-{$endif USE_VRML_TRIANGULATION}
-
 procedure TGLRenderer.RenderShape(Shape: TX3DRendererShape;
   Fog: IAbstractFogObject);
 
@@ -3157,7 +3095,6 @@ begin
   ShapeBumpMappingAllowed := false;
   ShapeBumpMappingUsed := false;
 
-  {$ifndef USE_VRML_TRIANGULATION}
   { Initalize MeshRenderer to something non-nil. }
   if not InitMeshRenderer then
   begin
@@ -3167,9 +3104,6 @@ begin
   end;
 
   Assert(MeshRenderer <> nil);
-  {$else}
-  MeshRenderer := nil;
-  {$endif}
 
   try
     RenderShapeShaders(Shape, Fog, Shader, MaterialOpacity, Lighting,
@@ -3404,13 +3338,6 @@ var
   CoordinateRenderer: TBaseCoordinateRenderer;
   VBO: boolean;
 begin
-  {$ifdef USE_VRML_TRIANGULATION}
-  { Simple rendering using LocalTriangulate. }
-  glBegin(GL_TRIANGLES);
-  Shape.LocalTriangulate(true, @DrawTriangle);
-  glEnd;
-  {$else}
-
   { initialize TBaseCoordinateRenderer.Arrays now }
   if GeneratorClass <> nil then
   begin
@@ -3476,8 +3403,6 @@ begin
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
   end;
-
-  {$endif USE_VRML_TRIANGULATION}
 end;
 
 {$ifndef OpenGLES}
