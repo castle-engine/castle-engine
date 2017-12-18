f44b0e77f61e51d9cf4e432e9a393b89c910be79
Jan Adamec on Mac <jadamec@gmail.com>
2017-12-18 09:27:25 +0100

    Replace accidental tabs with spaces in java code

--- tools/build-tool/data/android/integrated-services/open_associated_urls/app/src/main/java/net/sourceforge/castleengine/ServiceOpenAssociatedUrls.java
+++ tools/build-tool/data/android/integrated-services/open_associated_urls/app/src/main/java/net/sourceforge/castleengine/ServiceOpenAssociatedUrls.java
@@ -36,141 +36,141 @@ public class ServiceOpenAssociatedUrls extends ServiceAbstract
     @Override
     public void onCreate()
     {
-    	Intent intent = getActivity().getIntent();
-		openIntent(intent);
-	}
+        Intent intent = getActivity().getIntent();
+        openIntent(intent);
+    }
 
-	@Override
-	public void onNewIntent(Intent intent)
-	{
-		openIntent(intent);
-	}
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        openIntent(intent);
+    }
 
-	private void openIntent(Intent intent)
-	{
-		// https://stackoverflow.com/a/26034600/2870459
-		String action = intent.getAction();
-		if (action != null && action.compareTo(Intent.ACTION_VIEW) == 0)
-		{
-			String scheme = intent.getScheme();
-			Uri uri = intent.getData();
-			if (scheme == null || uri == null)
-				return;
+    private void openIntent(Intent intent)
+    {
+        // https://stackoverflow.com/a/26034600/2870459
+        String action = intent.getAction();
+        if (action != null && action.compareTo(Intent.ACTION_VIEW) == 0)
+        {
+            String scheme = intent.getScheme();
+            Uri uri = intent.getData();
+            if (scheme == null || uri == null)
+                return;
 
-			ContentResolver resolver = getActivity().getContentResolver();
+            ContentResolver resolver = getActivity().getContentResolver();
 
-			File urlDocumentsDir = getActivity().getDir("inbox", Context.MODE_PRIVATE);
+            File urlDocumentsDir = getActivity().getDir("inbox", Context.MODE_PRIVATE);
 
-			if (scheme.compareTo(ContentResolver.SCHEME_CONTENT) == 0)
-			{
-				String name = getContentName(resolver, uri);
+            if (scheme.compareTo(ContentResolver.SCHEME_CONTENT) == 0)
+            {
+                String name = getContentName(resolver, uri);
 
-				Log.i(TAG, "Content intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
-				try
-				{
-					InputStream input = resolver.openInputStream(uri);
-					String importfilepath = urlDocumentsDir.getAbsolutePath() + "/" + name;
-					InputStreamToFile(input, importfilepath);
-					messageSend(new String[]{"open_associated_url", "file://" + importfilepath});
-				}
-				catch (Exception e)
-				{
-					Log.e(TAG, "resolver.openInputStream exception: " + e.getMessage());
-				}
-			}
-			else if (scheme.compareTo(ContentResolver.SCHEME_FILE) == 0)
-			{
-				String name = uri.getLastPathSegment();
+                Log.i(TAG, "Content intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
+                try
+                {
+                    InputStream input = resolver.openInputStream(uri);
+                    String importfilepath = urlDocumentsDir.getAbsolutePath() + "/" + name;
+                    InputStreamToFile(input, importfilepath);
+                    messageSend(new String[]{"open_associated_url", "file://" + importfilepath});
+                }
+                catch (Exception e)
+                {
+                    Log.e(TAG, "resolver.openInputStream exception: " + e.getMessage());
+                }
+            }
+            else if (scheme.compareTo(ContentResolver.SCHEME_FILE) == 0)
+            {
+                String name = uri.getLastPathSegment();
 
-				Log.i(TAG, "File intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
-				messageSend(new String[]{"open_associated_url", uri.toString()});
-			}
-			else if (scheme.compareTo("http") == 0 || scheme.compareTo("https") == 0 || scheme.compareTo("ftp") == 0)
-			{
-				String name = uri.getLastPathSegment();
+                Log.i(TAG, "File intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
+                messageSend(new String[]{"open_associated_url", uri.toString()});
+            }
+            else if (scheme.compareTo("http") == 0 || scheme.compareTo("https") == 0 || scheme.compareTo("ftp") == 0)
+            {
+                String name = uri.getLastPathSegment();
 
-				Log.i(TAG, "Http intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
-				/*
-				// open directly from http:
-				messageSend(new String[]{"open_associated_url", uri.toString()});
-				/*/
-				try {
-					String importfilepath = urlDocumentsDir.getAbsolutePath() + "/" + name;
-					DownloadDataFromUrl(new URL(uri.toString()), importfilepath);
-				}
-				catch (Exception e)
-				{
-					Log.e(TAG, "URL exception: " + e.getMessage());
-				}
-				//*/
-			}
-		}
-	}
+                Log.i(TAG, "Http intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
+                /*
+                // open directly from http:
+                messageSend(new String[]{"open_associated_url", uri.toString()});
+                /*/
+                try {
+                    String importfilepath = urlDocumentsDir.getAbsolutePath() + "/" + name;
+                    DownloadDataFromUrl(new URL(uri.toString()), importfilepath);
+                }
+                catch (Exception e)
+                {
+                    Log.e(TAG, "URL exception: " + e.getMessage());
+                }
+                //*/
+            }
+        }
+    }
 
-	private String getContentName(ContentResolver resolver, Uri uri)
-	{
-		String sName = "untitled";
-		Cursor cursor = resolver.query(uri, null, null, null, null);
-		if (cursor != null)
-		{
-			cursor.moveToFirst();
-			int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
-			if (nameIndex >= 0)
-				sName = cursor.getString(nameIndex);
-			cursor.close();
-		}
-		return sName;
-	}
+    private String getContentName(ContentResolver resolver, Uri uri)
+    {
+        String sName = "untitled";
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        if (cursor != null)
+        {
+            cursor.moveToFirst();
+            int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
+            if (nameIndex >= 0)
+                sName = cursor.getString(nameIndex);
+            cursor.close();
+        }
+        return sName;
+    }
 
-	private void InputStreamToFile(InputStream in, String file)
-	{
-		try
-		{
-			OutputStream out = new FileOutputStream(new File(file));
+    private void InputStreamToFile(InputStream in, String file)
+    {
+        try
+        {
+            OutputStream out = new FileOutputStream(new File(file));
 
-			int size = 0;
-			byte[] buffer = new byte[1024];
+            int size = 0;
+            byte[] buffer = new byte[1024];
 
-			while ((size = in.read(buffer)) != -1)
-			{
-				out.write(buffer, 0, size);
-			}
+            while ((size = in.read(buffer)) != -1)
+            {
+                out.write(buffer, 0, size);
+            }
 
-			out.close();
-		}
-		catch (Exception e)
-		{
-			Log.e(TAG, "InputStreamToFile exception: " + e.getMessage());
-		}
-	}
+            out.close();
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, "InputStreamToFile exception: " + e.getMessage());
+        }
+    }
 
-	private void DownloadDataFromUrl(final URL url, final String file)
-	{
-		Thread thread = new Thread(new Runnable(){
-			@Override
-			public void run(){
-				try {
-					InputStream inStream = url.openStream();
+    private void DownloadDataFromUrl(final URL url, final String file)
+    {
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    InputStream inStream = url.openStream();
 
-					DataInputStream stream = new DataInputStream(inStream);
-					BufferedInputStream bufferedReader = new BufferedInputStream(stream);
+                    DataInputStream stream = new DataInputStream(inStream);
+                    BufferedInputStream bufferedReader = new BufferedInputStream(stream);
 
-					InputStreamToFile(bufferedReader, file);
+                    InputStreamToFile(bufferedReader, file);
 
-					stream.close();
+                    stream.close();
 
-					new Handler(Looper.getMainLooper()).post(new Runnable() {   // run in main thread
-						@Override
-						public void run() {
-							messageSend(new String[]{"open_associated_url", "file://" + file});
-						}
-					});
-				}
-				catch (Exception e) {
-					Log.e(TAG, "DownloadDataFromUrl exception: " + e.getMessage());
-				}
-			}
-		});
-		thread.start();
-	}
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {   // run in main thread
+                        @Override
+                        public void run() {
+                            messageSend(new String[]{"open_associated_url", "file://" + file});
+                        }
+                    });
+                }
+                catch (Exception e) {
+                    Log.e(TAG, "DownloadDataFromUrl exception: " + e.getMessage());
+                }
+            }
+        });
+        thread.start();
+    }
 }
