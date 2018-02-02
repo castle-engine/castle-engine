99f0d6f801cc82ee75a1569269d8f43b4336c6e1
michalis <michalis>
2018-02-02 20:03:07 +0100

    Automatic tests that measuring font with customized size is always OK

--- tests/testcastlefonts.pas
+++ tests/testcastlefonts.pas
@@ -23,6 +23,7 @@ type
   TTestCastleFonts = class(TTestCase)
   published
     procedure TestMaxTextWidthHtml;
+    procedure TestSizeFontFamily;
   end;
 
 implementation
@@ -67,6 +68,81 @@ begin
     TestMeasuring;
     Window.Close;
   finally FreeAndNil(Window) end;
+end;
+
+procedure TTestCastleFonts.TestSizeFontFamily;
+var
+  Font: TTextureFont;
+  Family: TFontFamily;
+  Customized: TCustomizedFont;
+begin
+  Font := TTextureFont.Create(TextureFont_DejaVuSansMonoBold_15);
+  try
+    AssertEquals(15, Font.Size);
+    AssertEquals(15, Font.RealSize);
+    AssertEquals(14, Font.RowHeight);
+
+    Family := TFontFamily.Create(nil);
+    try
+      Family.RegularFont := Font;
+
+      AssertEquals(0, Family.Size);
+      AssertEquals(15, Family.RealSize);
+      AssertEquals(14, Family.RowHeight);
+
+      Family.Size := 30;
+      AssertEquals(30, Family.Size);
+      AssertEquals(30, Family.RealSize);
+      AssertEquals(28, Family.RowHeight);
+    finally FreeAndNil(Family) end;
+
+    Customized := TCustomizedFont.Create(nil);
+    try
+      Customized.SourceFont := Font;
+
+      AssertEquals(0, Customized.Size);
+      AssertEquals(15, Customized.RealSize);
+      AssertEquals(14, Customized.RowHeight);
+
+      Customized.Size := 30;
+      AssertEquals(30, Customized.Size);
+      AssertEquals(30, Customized.RealSize);
+      AssertEquals(28, Customized.RowHeight);
+    finally FreeAndNil(Customized) end;
+
+    Font.Size := 60;
+    AssertEquals(60, Font.Size);
+    AssertEquals(60, Font.RealSize);
+    AssertEquals(56, Font.RowHeight);
+
+    Family := TFontFamily.Create(nil);
+    try
+      Family.RegularFont := Font;
+
+      AssertEquals(0, Family.Size);
+      AssertEquals(60, Family.RealSize);
+      AssertEquals(56, Family.RowHeight);
+
+      Family.Size := 30;
+      AssertEquals(30, Family.Size);
+      AssertEquals(30, Family.RealSize);
+      AssertEquals(28, Family.RowHeight);
+    finally FreeAndNil(Family) end;
+
+    Customized := TCustomizedFont.Create(nil);
+    try
+      Customized.SourceFont := Font;
+
+      AssertEquals(0, Customized.Size);
+      AssertEquals(60, Customized.RealSize);
+      AssertEquals(56, Customized.RowHeight);
+
+      Customized.Size := 30;
+      AssertEquals(30, Customized.Size);
+      AssertEquals(30, Customized.RealSize);
+      AssertEquals(28, Customized.RowHeight);
+    finally FreeAndNil(Customized) end;
+  finally FreeAndNil(Font) end;
 end;
 
 initialization
