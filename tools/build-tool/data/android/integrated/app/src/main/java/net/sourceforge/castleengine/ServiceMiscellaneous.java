ea6941b3035cf00c425bfac222e4390a5bb8f92b
Jan Adamec on Mac <jadamec@gmail.com>
2018-03-01 17:37:22 +0100

    Move downloading urls from ServiceMiscellaneous.java to its own service

--- tools/build-tool/data/android/integrated/app/src/main/java/net/sourceforge/castleengine/ServiceMiscellaneous.java
+++ tools/build-tool/data/android/integrated/app/src/main/java/net/sourceforge/castleengine/ServiceMiscellaneous.java
@@ -1,24 +1,12 @@
 /* -*- tab-width: 4 -*- */
 package net.sourceforge.castleengine;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.os.Build;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.net.Uri;
 import android.widget.Toast;
-
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
 
 /**
  * Integration of various Android small stuff with
@@ -88,59 +76,6 @@ public class ServiceMiscellaneous extends ServiceAbstract
         getActivity().startActivity(intent);
     }
 
-    private void downloadDataFromUrl(String urlToDownload)
-    {
-        final URL url;
-        try {
-            url = new URL(urlToDownload);
-        }
-        catch (Exception e) {
-            messageSend(new String[]{"download-error", e.getMessage()});
-            return;
-        }
-
-        File urlDocumentsDir = getActivity().getDir("inbox", Context.MODE_PRIVATE);
-        String tempFileName = url.getPath().replaceAll("[/|><]", "_");
-        final String tempDownloadFile = urlDocumentsDir.getAbsolutePath() + "/" + tempFileName;
-
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run(){
-                try {
-                    InputStream inStream = url.openStream();
-
-                    DataInputStream stream = new DataInputStream(inStream);
-                    BufferedInputStream bufferedReader = new BufferedInputStream(stream);
-
-                    OutputStream streamOut = new FileOutputStream(new File(tempDownloadFile));
-
-                    int size = 0;
-                    byte[] buffer = new byte[1024];
-
-                    while ((size = bufferedReader.read(buffer)) != -1)
-                    {
-                        streamOut.write(buffer, 0, size);
-                    }
-
-                    streamOut.close();
-                    stream.close();
-
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {   // run in main thread
-                        @Override
-                        public void run() {
-                            messageSend(new String[]{"download-finished", tempDownloadFile});
-                        }
-                    });
-                }
-                catch (Exception e) {
-                    Log.e("DL_URL", "DownloadDataFromUrl exception: " + e.getMessage());
-                    messageSend(new String[]{"download-error", e.getMessage()});
-                }
-            }
-        });
-        thread.start();
-    }
-
     @Override
     public boolean messageReceived(String[] parts)
     {
@@ -154,10 +89,6 @@ public class ServiceMiscellaneous extends ServiceAbstract
         } else
         if (parts.length == 2 && parts[0].equals("on-screen-notification")) {
             Toast.makeText(getActivity().getApplicationContext(), parts[1], Toast.LENGTH_SHORT).show();
-            return true;
-        } else
-        if (parts.length == 2 && parts[0].equals("download-url")) {
-            downloadDataFromUrl(parts[1]);
             return true;
         } else
         {
