6953ca9724a67b19bb65e3ca78da2352ff1f9cc2
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-04-10 21:16:17 +0200

    Update more links to https://castle-engine.io/

--- src/services/castleinapppurchases.pas
+++ src/services/castleinapppurchases.pas
@@ -187,7 +187,7 @@ type
       when user buys a "chest of gold", and "chest of gold"
       is non-consumable (you can only own it once, and then you just own it forever),
       then store the fact that you "already increased gold because of the chest ownership"
-      in the user persistent data (see https://castle-engine.sourceforge.io/manual_user_prefs.php).
+      in the user persistent data (see https://castle-engine.io/manual_user_prefs.php).
       Do not just increase the "gold owned" at every call of this method.
 
       If the product is a @bold(consumable), which means it has a one-time use
