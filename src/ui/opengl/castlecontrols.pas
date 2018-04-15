c8348f37b954f798d904273001f5e1df2c4f27b0
Benedikt Magnus <magnus@magnuscraft.de>
2018-04-15 02:30:15 +0200

    Removed changes in Castle core.

--- src/ui/opengl/castlecontrols.pas
+++ src/ui/opengl/castlecontrols.pas
@@ -24,7 +24,7 @@ uses Classes, Generics.Collections,
   CastleVectors, CastleUIControls, CastleFonts, CastleTextureFontData,
   CastleKeysMouse, CastleImages, CastleUtils, CastleGLImages, CastleRectangles,
   CastleColors, CastleProgress, CastleTimeUtils, CastleFontFamily, CastleGLUtils,
-  CastleURIUtils, CastleLog, CastleStringUtils, CastleLocalisation;
+  CastleURIUtils, CastleLog, CastleStringUtils;
 
 type
   TCastleLabel = class;
@@ -74,7 +74,6 @@ type
 
   {$define read_interface}
   {$I castlecontrols_uicontrolfont.inc}
-  {$I castlecontrols_uicontrollocalised.inc}
   {$I castlecontrols_button.inc}
   {$I castlecontrols_panel.inc}
   {$I castlecontrols_imagecontrol.inc}
@@ -110,7 +109,6 @@ uses SysUtils, Math, CastleControlsImages, CastleTextureFont_DjvSans_20,
 {$define read_implementation}
 {$I castlecontrols_uifont.inc} //< Keep this on top, to allow uicontrolfont.inc to access internals
 {$I castlecontrols_uicontrolfont.inc}
-{$I castlecontrols_uicontrollocalised.inc}
 {$I castlecontrols_button.inc}
 {$I castlecontrols_panel.inc}
 {$I castlecontrols_imagecontrol.inc}
