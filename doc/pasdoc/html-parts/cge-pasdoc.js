5cd80793d11705541ea5f24e5b9aa6c2ed273cf8
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-05-27 05:39:46 +0200

    Preserve URL suffix when switching between docs versions stable/unstable

--- doc/pasdoc/html-parts/cge-pasdoc.js
+++ doc/pasdoc/html-parts/cge-pasdoc.js
@@ -1,5 +1,23 @@
 'use strict';
 
+/* Change browser location to url, unless it doesn't exist,
+   then jump to fallbackUrl.
+   Detecting of url uses AJAX and will only work on the same domain. */
+function cgeChangeLocation(url, fallbackUrl)
+{
+    var ajax = jQuery.ajax({
+        type: "get",
+        url: url,
+        cache: false
+    });
+    ajax.done(function() {
+        window.location = url;
+    });
+    ajax.fail(function() {
+        window.location = fallbackUrl;
+    });
+}
+
 /* Append a form to switch API docs between stable/unstable engine versions. */
 jQuery(document).ready(function() {
     var versionsForm = jQuery(`<form class="cge-version-form form-inline">
@@ -28,11 +46,29 @@ jQuery(document).ready(function() {
         if (newVersion == cgeCurrentVersion) {
             return; // don't redirect to main page of current version
         }
+
+        var newVersionUrl = '', oldVersionUrl = '';
         cgeVersions.forEach(function (version) {
-            if (newVersion == version.id) {
-                window.location = version.url;
+            if (version.id == newVersion) {
+                newVersionUrl = version.url;
+            }
+            if (version.id == cgeCurrentVersion) {
+                oldVersionUrl = version.url;
             }
         });
+
+        /* We want to jump to newVersionUrl now.
+           Try to preserve a suffix in window.location (ignoring oldVersionUrl). */
+        if (newVersionUrl !== '') {
+            var completeNewVersionUrl = newVersionUrl;
+            if (oldVersionUrl !== '' && window.location.href.startsWith(oldVersionUrl)) {
+                completeNewVersionUrl =
+                    window.location.href.replace(oldVersionUrl, newVersionUrl);
+            }
+
+            /* try to jump to completeNewVersionUrl, fallback on newVersionUrl */
+            cgeChangeLocation(completeNewVersionUrl, newVersionUrl);
+        }
     });
 
     jQuery('td.navigation').append(versionsForm);
