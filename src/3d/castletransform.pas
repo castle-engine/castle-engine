0e6e5b341fe1d9383d4f51020141d1cb584fe067
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-07-27 17:28:51 +0200

    Comment to clarify adding/removing to TCastleTransform hierarchy is fast

--- src/3d/castletransform.pas
+++ src/3d/castletransform.pas
@@ -779,6 +779,12 @@ type
 
     { Operate on 3D objects contained in the list.
       You can also operate directly on @link(List) instance.
+
+      Note that adding and removing from this hierarchy is guaranteed to be fast,
+      so you can do it even in the middle of the game.
+      In particular calling @link(Remove) doesn't free OpenGL reasources
+      of the removed scene,
+      so removing scene only to add it later to another scene manager is blazingly fast.
       @groupBegin }
     procedure Add(const Item: TCastleTransform);
     procedure Insert(const Index: Integer; const Item: TCastleTransform);
