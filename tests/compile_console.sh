904ad5b43f77fee15e5edb0215f0e8c5fd5b58d8
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-08-09 23:55:25 +0200

    Enable compiling test_castle_game_engine using the build tool

--- tests/compile_console.sh
+++ tests/compile_console.sh
@@ -1,4 +1,10 @@
-cd ../
+#!/bin/bash
+set -euo pipefail
 
-fpc -gh -dDEBUG -dTEXT_RUNNER "$@" \
-  @castle-fpc.cfg tests/test_castle_game_engine.lpr
+if which castle-engine  > /dev/null; then
+  castle-engine --mode=debug compile
+else
+  cd ../
+  fpc -gh -dDEBUG -dTEXT_RUNNER "$@" \
+    @castle-fpc.cfg tests/test_castle_game_engine.lpr
+fi
--- tests/compile_console_release.sh
+++ tests/compile_console_release.sh
@@ -1,7 +1,13 @@
-cd ../
+#!/bin/bash
+set -euo pipefail
 
 # Tests must pass both in -dDEBUG and -dRELEASE modes.
 # This script recompiles engine (and fpcunit tests) in -dRELEASE mode.
 
-fpc -dRELEASE -dTEXT_RUNNER "$@" \
-  @castle-fpc.cfg tests/test_castle_game_engine.lpr
+if which castle-engine  > /dev/null; then
+  castle-engine --mode=release compile
+else
+  cd ../
+  fpc -gh -dRELEASE -dTEXT_RUNNER "$@" \
+    @castle-fpc.cfg tests/test_castle_game_engine.lpr
+fi
