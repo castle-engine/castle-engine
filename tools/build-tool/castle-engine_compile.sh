91376285f66706a8ea636f6d51363e9231a1d4ab
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-08-15 03:04:19 +0200

    Place build tool compilation output in tools/build-tool/castle-engine-output/...
    This avoids polluting the directory with .ppu/.o files that
    subsequently cause Lazarus compilation warnings/errors.

--- tools/build-tool/castle-engine_compile.sh
+++ tools/build-tool/castle-engine_compile.sh
@@ -17,15 +17,20 @@ set -eu
 # Allow calling this script from it's dir.
 if [ -f code/castle-engine.lpr ]; then cd ../../; fi
 
+mkdir -p tools/build-tool/castle-engine-output/build-tool-compilation
+
 fpc -dRELEASE @castle-fpc.cfg \
+  -FEtools/build-tool/ \
+  -FUtools/build-tool/castle-engine-output/build-tool-compilation \
   -Futools/build-tool/embedded_images/ \
   tools/build-tool/code/castle-engine.lpr
 
+# Not needed anymore, we use -FE
 # move binaries up (writing tools/build-tool/castle-engine.exe explicitly
 # is needed for MinGW "mv" to work OK)
-if [ -f tools/build-tool/code/castle-engine.exe ]; then
-  mv -f tools/build-tool/code/castle-engine.exe tools/build-tool/castle-engine.exe
-fi
-if [ -f tools/build-tool/code/castle-engine ]; then
-  mv -f tools/build-tool/code/castle-engine tools/build-tool/castle-engine
-fi
+# if [ -f tools/build-tool/code/castle-engine.exe ]; then
+#   mv -f tools/build-tool/code/castle-engine.exe tools/build-tool/castle-engine.exe
+# fi
+# if [ -f tools/build-tool/code/castle-engine ]; then
+#   mv -f tools/build-tool/code/castle-engine tools/build-tool/castle-engine
+# fi
