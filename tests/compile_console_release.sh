240a2c56cbd9c1d2a8c824a3a31f5b6aecb440ab
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-11-03 20:03:19 +0100

    Pass additional options to compile_console.sh correctly

--- tests/compile_console_release.sh
+++ tests/compile_console_release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-# Tests must pass both in -dDEBUG and -dRELEASE modes.
-# This script recompiles engine (and fpcunit tests) in -dRELEASE mode.
+# Compile the auto-tests.
+# In release mode (tests must pass in both debug and release), with text runner.
+# One optional additional parameter possible, like -dXXX.
 
 if which castle-engine  > /dev/null; then
-  castle-engine --mode=release compile
+  castle-engine --mode=release --compiler-option="$@" compile
 else
   cd ../
   fpc -gh -dRELEASE -dTEXT_RUNNER "$@" \
