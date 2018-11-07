ac7b318da411aeaa6feef4a218e1335e18faf595
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-11-07 05:15:00 +0100

    Fix auto-tests
    TCastleUserInterface.Rect is only maintained for backward compat.

--- tests/testcastleuicontrols.pas
+++ tests/testcastleuicontrols.pas
@@ -37,18 +37,9 @@ type
   TNothingOverrriden = class(TCastleUserInterface)
   end;
 
-  // TIntOverrriden = class(TCastleUserInterface)
-  //   function Rect: TRectangle; override;
-  // end;
-
   TFloatOverrriden = class(TCastleUserInterface)
     function Rect: TFloatRectangle; override;
   end;
-
-// function TIntOverrriden.Rect: TRectangle;
-// begin
-//   Result := Rectangle(10, 20, 30, 40);
-// end;
 
 function TFloatOverrriden.Rect: TFloatRectangle;
 begin
@@ -58,30 +49,21 @@ end;
 procedure TTestCastleUIControls.TestRectOverrides;
 var
   N: TNothingOverrriden;
-  // I: TIntOverrriden;
   F: TFloatOverrriden;
 begin
   N := TNothingOverrriden.Create(nil);
   try
-    AssertRectsEqual(FloatRectangle(0, 0, 100, 100), N.Rect);
     AssertEquals(100, N.EffectiveWidth);
     AssertEquals(100, N.EffectiveHeight);
 
     N.Width := 0;
     N.Height := 0;
-    AssertRectsEqual(TFloatRectangle.Empty, N.Rect);
     AssertEquals(0, N.EffectiveWidth);
     AssertEquals(0, N.EffectiveHeight);
   finally FreeAndNil(N); end;
 
-  // I := TIntOverrriden.Create(nil);
-  // try
-  //   AssertRectsEqual(FloatRectangle(10, 20, 30, 40), I.Rect);
-  // finally FreeAndNil(I); end;
-
   F := TFloatOverrriden.Create(nil);
   try
-    AssertRectsEqual(FloatRectangle(100, 200, 300, 400), F.Rect);
     AssertEquals(300, F.EffectiveWidth);
     AssertEquals(400, F.EffectiveHeight);
   finally FreeAndNil(F); end;
