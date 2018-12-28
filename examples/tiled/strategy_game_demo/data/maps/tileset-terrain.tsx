a93d9687ea18c28418d2979cac8c21bf58ccfd78
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-12-28 21:16:28 +0100

    Initial data for strategy_game_demo (WIP)

---
+++ examples/tiled/strategy_game_demo/data/maps/tileset-terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tileset-terrain" tilewidth="56" tileheight="56" spacing="8" margin="4" tilecount="16" columns="4">
+ <image source="tileset-terrain.png" width="256" height="256"/>
+</tileset>
