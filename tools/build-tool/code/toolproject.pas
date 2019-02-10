9b71af553060bc564b3c7e19f5ad46dbf1c2a18a
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-02-10 17:10:29 +0100

    Default target SDK version 27 too

--- tools/build-tool/code/toolproject.pas
+++ tools/build-tool/code/toolproject.pas
@@ -323,7 +323,7 @@ constructor TCastleProject.Create(const APath: string);
       We want also NativeActivity and EGL, which require API level 9 or higher. }
     ReallyMinSdkVersion = 9;
     DefaultAndroidMinSdkVersion = 9;
-    DefaultAndroidTargetSdkVersion = 23;
+    DefaultAndroidTargetSdkVersion = DefaultAndroidCompileSdkVersion;
     DefaultUsesNonExemptEncryption = true;
 
     { character sets }
