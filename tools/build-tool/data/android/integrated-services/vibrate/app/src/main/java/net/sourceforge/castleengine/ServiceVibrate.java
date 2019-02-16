582467a7079b02ee615542f20d3e344db7850f40
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-02-16 15:53:56 +0100

    Fix vibrations to work on all API versions

--- tools/build-tool/data/android/integrated-services/vibrate/app/src/main/java/net/sourceforge/castleengine/ServiceVibrate.java
+++ tools/build-tool/data/android/integrated-services/vibrate/app/src/main/java/net/sourceforge/castleengine/ServiceVibrate.java
@@ -4,7 +4,6 @@ package net.sourceforge.castleengine;
 import android.view.View;
 import android.os.Build;
 import android.os.Vibrator;
-import android.os.VibrationEffect;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -31,13 +30,21 @@ public class ServiceVibrate extends ServiceAbstract
        http://developer.android.com/reference/android/os/Vibrator.html
     */
 
+    @SuppressWarnings("deprecation")
     private void vibrate(long milliseconds)
     {
         Vibrator vibs = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-        // vibs.vibrate(milliseconds);
-        // Equivalent, not deprecated:
-        VibrationEffect effect = VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE);
-        vibs.vibrate(effect);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // not deprecated:
+            android.os.VibrationEffect effect =
+                android.os.VibrationEffect.createOneShot(milliseconds,
+                    android.os.VibrationEffect.DEFAULT_AMPLITUDE);
+            vibs.vibrate(effect);
+        } else {
+            // deprecated in new API versions,
+            // but necessary to work on old devices that don't have VibrationEffect
+            vibs.vibrate(milliseconds);
+        }
     }
 
     @Override
