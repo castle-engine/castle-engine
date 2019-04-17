7f0e5dc644414e2d1349fd371584f5b4352d5050
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-04-17 14:11:19 +0200

    Signed/unsigned comments at TSoundDataFormat

--- src/audio/castleinternalsoundfile.pas
+++ src/audio/castleinternalsoundfile.pas
@@ -23,6 +23,18 @@ interface
 uses SysUtils, CastleUtils, Classes, CastleTimeUtils;
 
 type
+  { Sound sample format.
+
+    8-bit data is unsigned.
+    Just like in case of 8-bit WAV files, and OpenAL AL_FORMAT_MONO8 / AL_FORMAT_STEREO8:
+    It is expressed as an unsigned value over the range 0 to 255, 128 being an audio output level of zero.
+
+    16-bit data is signed.
+    Just like in case of 16-bit WAV files, and OpenAL AL_FORMAT_MONO16 / AL_FORMAT_STEREO16:
+    It is expressed as a signed value over the range -32768 to 32767, 0 being an audio output level of zero.
+
+    Stereo data is expressed in an interleaved format, left channel sample followed by theright channel sample.
+  }
   TSoundDataFormat = (
     sfMono8,
     sfMono16,
