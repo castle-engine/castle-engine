611a15ab73df11222b3942d2ede706bd8b46cc50
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-05-08 01:08:29 +0100

    Proper activity recognition on iOS

--- tools/build-tool/data/ios/services/activity_recognition/cge_project_name/activity_recognition/ActivityRecognitionService.m
+++ tools/build-tool/data/ios/services/activity_recognition/cge_project_name/activity_recognition/ActivityRecognitionService.m
@@ -21,26 +21,93 @@
 
 @implementation ActivityRecognitionService
 
-/*
-- (void)application:(UIApplication *) application
-    didFinishLaunchingWithOptions:(NSDictionary *) launchOptions
+- (void) startListening
 {
-    ActivityRecognitionInitialize();
-}
-*/
+    if (![CMMotionActivityManager isActivityAvailable]) {
+        NSLog(@"Motion activity not available on this device.");
+        return;
+    }
 
-/*
+    if (@available(iOS 11.0, *)) {
+        if ([CMMotionActivityManager authorizationStatus] != CMAuthorizationStatusAuthorized) {
+            NSLog(@"Not authorized to reaad motion activity.");
+            return;
+        }
+    }
+
+    if (motionActivityManager == NULL) {
+        motionActivityManager = [[CMMotionActivityManager alloc] init];
+    }
+    [motionActivityManager startActivityUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMMotionActivity * _Nullable activity) {
+        [self processUpdate:activity];
+    }];
+}
+
+- (void) processUpdate: (CMMotionActivity*) activity
+{
+    NSMutableArray* possibleActivitiesNames = [[NSMutableArray alloc] init];
+    if (activity.stationary) {
+        [possibleActivitiesNames addObject:@"stationary"];
+    }
+    if (activity.walking) {
+        [possibleActivitiesNames addObject:@"walking"];
+    }
+    if (activity.running) {
+        [possibleActivitiesNames addObject:@"running"];
+    }
+    if (activity.automotive) {
+        [possibleActivitiesNames addObject:@"automotive"];
+    }
+    if (activity.cycling) {
+        [possibleActivitiesNames addObject:@"cycling"];
+    }
+    if (activity.unknown) {
+        [possibleActivitiesNames addObject:@"unknown"];
+    }
+
+    NSString* possibleActivitiesStr = [possibleActivitiesNames componentsJoinedByString:@"\2"];
+
+    NSString* confidenceName;
+    switch (activity.confidence) {
+        case CMMotionActivityConfidenceLow: confidenceName = @"low"; break;
+        case CMMotionActivityConfidenceMedium: confidenceName = @"medium"; break;
+        case CMMotionActivityConfidenceHigh: confidenceName = @"high"; break;
+        default: NSLog(@"Unrecognized activity confidence"); break;
+    }
+
+    [self messageSend:@[
+        @"activity-recognition-change",
+        possibleActivitiesNames,
+        confidenceName
+    ]];
+}
+
+- (void) stopListening
+{
+    if (motionActivityManager != NULL) {
+        [motionActivityManager stopActivityUpdates];
+    }
+
+}
+
 - (bool)messageReceived:(NSArray* )message
 {
     if (message.count == 2 &&
-        [[message objectAtIndex: 0] isEqualToString:@"test-fairy-log"])
+        [[message objectAtIndex: 0] isEqualToString:@"activity-recognition"] &&
+        [[message objectAtIndex: 1] isEqualToString:@"start"])
     {
-        [ActivityRecognition log: [message objectAtIndex: 1]];
+        [self startListening];
+        return TRUE;
+    } else
+    if (message.count == 2 &&
+        [[message objectAtIndex: 0] isEqualToString:@"activity-recognition"] &&
+        [[message objectAtIndex: 1] isEqualToString:@"stop"])
+    {
+        [self stopListening];
         return TRUE;
     }
 
     return FALSE;
 }
-*/
 
 @end
