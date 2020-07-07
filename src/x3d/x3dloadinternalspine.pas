4cbd160524390607127b1b348d9ae9f92cd4fa7b
kagamma <kagamma.km@gmail.com>
2020-07-07 21:12:34 +0700

    Added basic support for Spine skinned mesh

--- src/x3d/x3dloadinternalspine.pas
+++ src/x3d/x3dloadinternalspine.pas
@@ -67,6 +67,7 @@ type
   {$I x3dloadinternalspine_slottimelines.inc}
   {$I x3dloadinternalspine_drawordertimelines.inc}
   {$I x3dloadinternalspine_deformtimelines.inc}
+  {$I x3dloadinternalspine_weightedmeshtimelines.inc}
   {$I x3dloadinternalspine_animations.inc}
   {$I x3dloadinternalspine_skeleton.inc}
   {$undef read_interface}
@@ -81,6 +82,7 @@ type
   {$I x3dloadinternalspine_slottimelines.inc}
   {$I x3dloadinternalspine_drawordertimelines.inc}
   {$I x3dloadinternalspine_deformtimelines.inc}
+  {$I x3dloadinternalspine_weightedmeshtimelines.inc}
   {$I x3dloadinternalspine_animations.inc}
   {$I x3dloadinternalspine_skeleton.inc}
   {$undef read_implementation}
