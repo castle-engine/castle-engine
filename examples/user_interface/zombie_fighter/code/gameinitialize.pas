263fd946bec5f1e5f25da994563bf4acd95ebadf
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-01-05 07:45:02 +0100

    zombie_fighter gameinitialize.pas more standard

--- examples/user_interface/zombie_fighter/code/gameinitialize.pas
+++ examples/user_interface/zombie_fighter/code/gameinitialize.pas
@@ -22,7 +22,7 @@ implementation
 
 uses SysUtils, Classes, CastleControls, CastleUtils, CastleFilesUtils,
   CastleColors, CastleUIControls, CastleUIState, CastleWindow,
-  CastleApplicationProperties,
+  CastleApplicationProperties, CastleLog,
   GameStateMainMenu, GameStateLoading, GameStatePlay, GameStateAskDialog;
 
 var
@@ -47,8 +47,17 @@ initialization
   { Set ApplicationName early, as our log uses it. }
   ApplicationProperties.ApplicationName := 'zombie_fighter';
 
-  Window := TCastleWindowBase.Create(Application);
+  { Start logging. Do this as early as possible,
+    to log information and eventual warnings during initialization.
 
-  Application.MainWindow := Window;
+    For programs, InitializeLog is not called here.
+    Instead InitializeLog is done by the program main file,
+    after command-line parameters are parsed. }
+  if IsLibrary then
+    InitializeLog;
+
   Application.OnInitialize := @ApplicationInitialize;
+
+  Window := TCastleWindowBase.Create(Application);
+  Application.MainWindow := Window;
 end.
