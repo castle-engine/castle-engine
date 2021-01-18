a55f881dacd473d193993092536297daf764b6eb
and3md <info@digitalkarabela.com>
2021-01-18 14:22:34 +0100

    Added license.

--- tools/castle-editor/code/formspritesheeteditor.pas
+++ tools/castle-editor/code/formspritesheeteditor.pas
@@ -1,3 +1,18 @@
+{
+  Copyright 2020-2021 Andrzej Kilijanski.
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
 unit FormSpriteSheetEditor;
 
 {$mode objfpc}{$H+}
