ec22c09f72b8a71a6a6a9940387872f37480e95f
and3md <and3md@gmail.com>
2021-04-19 15:39:46 +0200

    Make bullet really disappear (need set RigidBody.Exists = false)

--- code/gameenemy.pas
+++ code/gameenemy.pas
@@ -137,6 +137,10 @@ end;
 procedure TEnemy.TakeDamageFromBullet(const Bullet: TCastleTransform);
 begin
   Bullet.Exists := false;
+  { TODO: When we only change Bullet.Exists = false, rigid body
+    still exists, this is only temporary hack to fix that. }
+  Bullet.RigidBody.Exists := false;
+
   Dead := true;
   Parent.RigidBody.Exists := false;
 end;
