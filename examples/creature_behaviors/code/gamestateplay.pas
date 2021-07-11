dbfc029b8b96d874c6045bc5efb349c811c37779
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-07-11 18:39:47 +0200

    Upgrade SoundEngine.Sound -> SoundEngine.Play usage

--- examples/creature_behaviors/code/gamestateplay.pas
+++ examples/creature_behaviors/code/gamestateplay.pas
@@ -275,7 +275,7 @@ begin
 
   if Event.IsMouseButton(buttonLeft) then
   begin
-    SoundEngine.Sound(SoundEngine.SoundFromName('shoot_sound'));
+    SoundEngine.Play(SoundEngine.SoundFromName('shoot_sound'));
 
     { We clicked on enemy if
       - TransformUnderMouse indicates we hit something
