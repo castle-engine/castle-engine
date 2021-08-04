a7f0ebf1af8585ca1646704d3ee5c952e7af0738
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-08-04 17:05:21 +0200

    Fix after merge - remove duplicate DoMakeExecutable implementatuib

--- tools/build-tool/code/toolutils.pas
+++ tools/build-tool/code/toolutils.pas
@@ -234,24 +234,4 @@ begin
   {$endif}
 end;
 
-procedure DoMakeExecutable(const PathAndName: String);
-{$ifdef UNIX}
-var
-  ChmodResult: CInt;
-begin
-  ChmodResult := FpChmod(PathAndName,
-    S_IRUSR or S_IWUSR or S_IXUSR or
-    S_IRGRP or            S_IXGRP or
-    S_IROTH or            S_IXOTH);
-  if ChmodResult <> 0 then
-    WritelnWarning('Package', Format('Error setting executable bit on "%s": %s', [
-      PathAndName,
-      SysErrorMessage(ChmodResult)
-    ]));
-{$else}
-begin
-  WritelnWarning('Package', 'Packaging for a platform where UNIX permissions matter, but we cannot set "chmod" on this platform. This usually means that you package for Unix from Windows, and means that "executable" bit inside binary in tar.gz archive may not be set --- archive may not be 100% comfortable for Unix users');
-  {$endif}
-end;
-
 end.
