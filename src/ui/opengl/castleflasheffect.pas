a62b4cb0608897dbb4435dc7bff467cfd617e005
and3md <info@digitalkarabela.com>
2021-10-05 07:46:49 +0200

    Port CastleFlashEffect to Delphi.

--- src/ui/opengl/castleflasheffect.pas
+++ src/ui/opengl/castleflasheffect.pas
@@ -54,7 +54,7 @@ type
     function Active: boolean;
   published
     property Duration: Single read FDuration write FDuration
-      default DefaultDuration;
+      {$ifdef FPC}default DefaultDuration{$endif};
 
     { Set this image to modulate the color with an image.
       The image is always stretched to cover our whole size. }
@@ -74,7 +74,7 @@ begin
   FDuration := DefaultDuration;
   FullSize := true;
   FImage := TCastleImagePersistent.Create;
-  FImage.OnChange := @ImageChanged;
+  FImage.OnChange := {$ifdef CASTLE_OBJFPC}@{$endif}ImageChanged;
 end;
 
 destructor TCastleFlashEffect.Destroy;
