a52d2bd383d4f148513be2d8afae1109fae6061e
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-11-16 11:29:03 +0100

    Fix CastleNoise with FPC again :)
    Our commits (me and Andrzej) crossed. Now it should be OK.

--- src/castlescript/castlenoise.pas
+++ src/castlescript/castlenoise.pas
@@ -190,7 +190,7 @@ type
   PUInt32Array = ^TUInt32Array;
 var
   N: LongWord;
-  PPrimes: {$ifdef FPC}PUint32{$else}PUInt32Array{$endif};
+  PPrimes: PUInt32Array;
 {$I norqcheckbegin.inc}
 begin
   { Choose our primes row from Primes table. }
