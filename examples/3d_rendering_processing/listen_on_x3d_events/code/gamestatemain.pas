5b4dec0b6a11ee13c822117f24dc99d560f0e158
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-12-09 15:38:19 +0100

    listen_on_x3d_events ported to editor, with new data from Blender

--- examples/3d_rendering_processing/listen_on_x3d_events/code/gamestatemain.pas
+++ examples/3d_rendering_processing/listen_on_x3d_events/code/gamestatemain.pas
@@ -20,7 +20,8 @@ interface
 
 uses Classes,
   CastleVectors, CastleUIState, CastleComponentSerialize,
-  CastleUIControls, CastleControls, CastleKeysMouse;
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleScene,
+  CastleNotifications, X3DNodes, X3DFields, X3DTime;
 
 type
   { Main state, where most of the application logic takes place. }
@@ -28,11 +29,17 @@ type
   private
     { Components designed using CGE editor, loaded from gamestatemain.castle-user-interface. }
     LabelFps: TCastleLabel;
+    MainScene: TCastleScene;
+    Notifications: TCastleNotifications;
+
+    TouchSensors: array [1..4] of TTouchSensorNode;
+    procedure TouchSensorClick(Sender: TObject);
+    procedure TouchSensorTouchTime(const Event: TX3DEvent; const Value: TX3DField; const Time: TX3DTime);
+    procedure TouchSensorIsActive(const Event: TX3DEvent; const Value: TX3DField; const Time: TX3DTime);
   public
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
     procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
-    function Press(const Event: TInputPressRelease): Boolean; override;
   end;
 
 var
@@ -51,11 +58,25 @@ begin
 end;
 
 procedure TStateMain.Start;
+var
+  I: Integer;
 begin
   inherited;
 
   { Find components, by name, that we need to access from code }
   LabelFps := DesignedComponent('LabelFps') as TCastleLabel;
+  Notifications := DesignedComponent('Notifications') as TCastleNotifications;
+  MainScene := DesignedComponent('MainScene') as TCastleScene;
+
+  for I := Low(TouchSensors) to High(TouchSensors) do
+  begin
+    TouchSensors[I] := MainScene.Node('TouchSensorPiece' + IntToStr(I)) as TTouchSensorNode;
+    { It is easiest to just handle OnClick.
+      It corresponds to EventTouchTime, though OnClick is more standard TNotifyEvent. }
+    TouchSensors[I].OnClick := {$ifdef FPC}@{$endif} TouchSensorClick;
+    TouchSensors[I].EventTouchTime.AddNotification({$ifdef FPC}@{$endif} TouchSensorTouchTime);
+    TouchSensors[I].EventIsActive.AddNotification({$ifdef FPC}@{$endif} TouchSensorIsActive);
+  end;
 end;
 
 procedure TStateMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
@@ -65,29 +86,40 @@ begin
   LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
 end;
 
-function TStateMain.Press(const Event: TInputPressRelease): Boolean;
+procedure TStateMain.TouchSensorClick(Sender: TObject);
+var
+  TouchSensor: TTouchSensorNode;
 begin
-  Result := inherited;
-  if Result then Exit; // allow the ancestor to handle keys
+  TouchSensor := Sender as TTouchSensorNode;
+  Notifications.Show(Format('TouchSensor OnClick event: piece %s', [
+    TouchSensor.X3DName
+  ]));
+end;
 
-  { This virtual method is executed when user presses
-    a key, a mouse button, or touches a touch-screen.
+procedure TStateMain.TouchSensorTouchTime(const Event: TX3DEvent; const Value: TX3DField; const Time: TX3DTime);
+var
+  Val: Double;
+  TouchSensor: TTouchSensorNode;
+begin
+  Val := (Value as TSFTime).Value;
+  TouchSensor := Event.ParentNode as TTouchSensorNode;
+  Notifications.Show(Format('TouchSensor.touchTime event: piece %s, touch time %f', [
+    TouchSensor.X3DName,
+    Val
+  ]));
+end;
 
-    Note that each UI control has also events like OnPress and OnClick.
-    These events can be used to handle the "press", if it should do something
-    specific when used in that UI control.
-    The TStateMain.Press method should be used to handle keys
-    not handled in children controls.
-  }
-
-  // Use this to handle keys:
-  {
-  if Event.IsKey(keyXxx) then
-  begin
-    // DoSomething;
-    Exit(true); // key was handled
-  end;
-  }
+procedure TStateMain.TouchSensorIsActive(const Event: TX3DEvent; const Value: TX3DField; const Time: TX3DTime);
+var
+  Val: Boolean;
+  TouchSensor: TTouchSensorNode;
+begin
+  Val := (Value as TSFBool).Value;
+  TouchSensor := Event.ParentNode as TTouchSensorNode;
+  Notifications.Show(Format('TouchSensor.isActive event: piece %s, is active: %s', [
+    TouchSensor.X3DName,
+    BoolToStr(Val, true)
+  ]));
 end;
 
 end.
