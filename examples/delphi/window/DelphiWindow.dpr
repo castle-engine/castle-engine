0d93068787ce1a23e2c1c8fed932a07118d2da02
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-12-12 00:40:04 +0100

    Simpler examples/delphi/window

--- examples/delphi/window/DelphiWindow.dpr
+++ examples/delphi/window/DelphiWindow.dpr
@@ -19,14 +19,7 @@ uses
   CastleComponentSerialize,
   CastleClassUtils,
   CastleApplicationProperties,
-  fpjson,
-  jsonparser,
-  CastleUtils,
-  fpjsonrtti,
-  TestFpJsonRTTI in 'TestFpJsonRTTI.pas',
-  TestFpJson in 'TestFpJson.pas',
-  TestCastleConfig in 'testcastleconfig.pas',
-  TestCastleSerialization in 'testcastleserialization.pas';
+  CastleUtils;
 
 var
   Window: TCastleWindowBase;
@@ -34,59 +27,50 @@ var
   DrawableImageCGE: TDrawableImage;
   DrawableImageDelphi: TDrawableImage;
 
-  procedure Init(Sender: TCastleContainer);
-  begin
-    DrawableImage := TDrawableImage.Create('castle-data:/patreonlogoorange_45px.png');
-    DrawableImageCGE := TDrawableImage.Create('castle-data:/cge_384px.png');
-    DrawableImageDelphi := TDrawableImage.Create('castle-data:/delphi_11_384px.png');
-  end;
+procedure Init(Sender: TCastleContainer);
+begin
+  DrawableImage := TDrawableImage.Create('castle-data:/patreonlogoorange_45px.png');
+  DrawableImageCGE := TDrawableImage.Create('castle-data:/cge_384px.png');
+  DrawableImageDelphi := TDrawableImage.Create('castle-data:/delphi_11_384px.png');
+end;
 
-  procedure ReleaseImg(Sender: TCastleContainer);
-  begin
-    FreeAndNil(DrawableImage);
-    FreeAndNil(DrawableImageCGE);
-    FreeAndNil(DrawableImageDelphi);
-  end;
+procedure ReleaseImg(Sender: TCastleContainer);
+begin
+  FreeAndNil(DrawableImage);
+  FreeAndNil(DrawableImageCGE);
+  FreeAndNil(DrawableImageDelphi);
+end;
 
-  procedure Render(Sender: TCastleContainer);
-  var
-    Rect: TRectangle;
-  begin
-    Rect.Left := 10;
-    Rect.Bottom := 100;
-    Rect.Width := 1000;
-    Rect.Height := 500;
-    DrawRectangle(Rect, Vector4(0.15, 0.15, 0.15, 1.0));
-    GetUIFont.Size := 30;
-    GetUIFont.Print(283, 650, Orange, 'Castle Game Engine in Delphi');
-    DrawableImageDelphi.Draw(100, 150);
-    DrawableImageCGE.Draw(520,150);
-    GetUIFont.Print(215, 40, Silver, 'Support us on Patreon:');
-    DrawableImage.Draw(600,30);
-  end;
+procedure Render(Sender: TCastleContainer);
+var
+  Rect: TRectangle;
+begin
+  Rect.Left := 10;
+  Rect.Bottom := 100;
+  Rect.Width := 1000;
+  Rect.Height := 500;
+  DrawRectangle(Rect, Vector4(0.15, 0.15, 0.15, 1.0));
+  GetUIFont.Size := 30;
+  GetUIFont.Print(283, 650, Orange, 'Castle Game Engine in Delphi');
+  DrawableImageDelphi.Draw(100, 150);
+  DrawableImageCGE.Draw(520,150);
+  GetUIFont.Print(215, 40, Silver, 'Support us on Patreon:');
+  DrawableImage.Draw(600,30);
+end;
 
 
-  begin
-    ApplicationProperties.ApplicationName := 'delphi_window';
-    InitializeLog;
-    //LogShaders := true;
+begin
+  ApplicationProperties.ApplicationName := 'delphi_window';
+  InitializeLog;
+  //LogShaders := true;
 
-    TestFpJson1;
-    // simpledemo form fcl-json examples
-    TestFpJson2;
+  WritelnLog('Simplest CastleWindow in Delphi');
 
-    TestRTTI;
+  Window := TCastleWindowBase.Create(Application);
+  Window.Caption := 'Simplest CastleWindow in Delphi';
+  Window.OnRender := Render;
+  Window.OnOpen := Init;
+  Window.OnClose := ReleaseImg;
+  Window.OpenAndRun;
+end.
 
-    TestRTTICastle;
-
-    TestCastleConfig1;
-
-    WritelnLog('Simplest CastleWindow in Delphi');
-
-    Window := TCastleWindowBase.Create(Application);
-    Window.Caption := 'Simplest CastleWindow in Delphi';
-    Window.OnRender := Render;
-    Window.OnOpen := Init;
-    Window.OnClose := ReleaseImg;
-    Window.OpenAndRun;
-  end.
