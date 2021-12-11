1e286b87db16cffea418dcd1e94da7af026925f3
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-12-11 17:48:41 +0100

    Script to run castle-engine generate-project on all examples

---
+++ examples/regenerate_auto_files_in_all_examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+# ----------------------------------------------------------------------------
+# For most projects, run "castle-engine generate-project" to regenerate
+#
+# - lpi
+# - dproj
+# - dpr (if game_units are specified in manifest)
+#
+# Exceptions are:
+#
+# - LCL projects (leave LPI to be managed using Lazarus and specify LCL packages,
+#   and DPROJ doesn't make sense for these projects)
+# - simplest_manifest_test (we deliberately do not store dpr/lpi/dproj in this demo).
+# ----------------------------------------------------------------------------
+
+FIND='find'
+if which cygpath.exe > /dev/null; then
+  FIND='/bin/find' # On Cygwin, make sure to use Cygwin's find, not the one from Windows
+fi
+
+"${FIND}" -iname CastleEngineManifest.xml \
+  '(' -not -iwholename '*/simplest_manifest_test/*' ')' -or \
+  -execdir bash -c 'if ls *.lfm > /dev/null 2>&1; then echo `pwd`": LCL project"; else echo `pwd`": Regeneratiing"; castle-engine generate-program; fi' ';'
