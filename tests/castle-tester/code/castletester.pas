b3417986a1d616fa9f8cba2219fbe464d56eb339
and3md <info@digitalkarabela.com>
2021-12-27 13:18:55 +0100

    CastleTester: Added caller address, some fpc fixes,

--- tests/castle-tester/code/castletester.pas
+++ tests/castle-tester/code/castletester.pas
@@ -2,13 +2,13 @@ unit CastleTester;
 
 interface
 
-uses SysUtils, Classes, System.Generics.Collections, Rtti;
+uses SysUtils, Classes, Generics.Collections, Rtti;
 
 type
 
   EAssertionFailedError = class(Exception);
 
-  TNotifyAssertFail = procedure (const Msg: String) of object;
+  TNotifyAssertFail = procedure (const TestName, Msg: String) of object;
   TNotifyTestExecuted = procedure (const Name: String) of object;
   TNotifyTestCaseExecuted = procedure (const Name: String) of object;
 
@@ -17,35 +17,39 @@ type
   end;
 
   TCastleTestCase = class
-  private
+  strict private
     FName: String;
+  private
     FNotifyAssertFail: TNotifyAssertFail;
+    FCurrentTestName: String;
   public
     procedure TestTestCase;
     constructor Create;
 
-    procedure Fail(const Msg: String);
+    procedure Fail(const Msg: String; ErrorAddr: Pointer = nil);
     procedure AssertTrue(const ACondition: Boolean); overload;
-    procedure AssertTrue(const Msg: String; const ACondition: Boolean); overload;
+    procedure AssertTrue(const Msg: String; const ACondition: Boolean;
+      ErrorAddr: Pointer = nil); overload;
     procedure AssertFalse(const ACondition: Boolean); overload;
-    procedure AssertFalse(const Msg: String; const ACondition: Boolean); overload;
+    procedure AssertFalse(const Msg: String; const ACondition: Boolean;
+      ErrorAddr: Pointer = nil); overload;
     procedure AssertFilenamesEqual(Expected, Actual: String);
     procedure AssertEquals(Expected, Actual: String);
 
     function CompareFileName(Expected, Actual: String): Boolean;
 
+    property CurrentTestName: String read FCurrentTestName;
   end;
 
 
   TCastleTester = class (TComponent)
-  private
+  strict private
     FRttiContext: TRttiContext;
     FTestCaseList: {$ifdef FPC}specialize{$endif} TObjectList<TCastleTestCase>;
     FNotifyTestExecuted: TNotifyTestExecuted;
     FNotifyTestCaseExecuted: TNotifyTestCaseExecuted;
     FNotifyAssertFail: TNotifyAssertFail;
     procedure SetNotifyAssertFail(const ANotifyAssertFail: TNotifyAssertFail);
-
 
     procedure RunTestCase(TestCase: TCastleTestCase);
 
@@ -63,7 +67,6 @@ type
       read FNotifyTestCaseExecuted write FNotifyTestCaseExecuted;
     property NotifyAssertFail: TNotifyAssertFail read FNotifyAssertFail
       write SetNotifyAssertFail;
-
   end;
 
 implementation
@@ -82,7 +85,7 @@ constructor TCastleTester.Create(AOwner: TComponent);
 begin
   inherited;
 
-  FTestCaseList := TObjectList<TCastleTestCase>.Create;
+  FTestCaseList := {$ifdef FPC}specialize{$endif} TObjectList<TCastleTestCase>.Create;
   FRttiContext := TRttiContext.Create;
 end;
 
@@ -110,6 +113,8 @@ var
   RttiType: TRttiType;
   RttiMethod: TRttiMethod;
 begin
+  // raise Exception.Create('Test Return Adress') at {$ifdef FPC}get_caller_addr(get_frame){$else}System.ReturnAddress{$endif};
+
   RttiType := FRttiContext.GetType(TestCase.ClassInfo);
 
   for RttiMethod in RttiType.GetMethods do
@@ -118,15 +123,15 @@ begin
       (Length(RttiMethod.GetParameters) = 0) and
       (pos('TEST', UpperCase(RttiMethod.Name)) = 1) then
     begin
+      TestCase.FCurrentTestName := RttiMethod.Name;
       RttiMethod.Invoke(TestCase, []);
     end;
 
   end;
-
 end;
 
 procedure TCastleTester.SetNotifyAssertFail(
-  const ANotifyAssertFail: TNotifyAssertFail);
+  const ANotifyAssertFail: TNotifyAssertFail);
 var
   TestCase: TCastleTestCase;
 begin
@@ -144,34 +149,46 @@ end;
 
 procedure TCastleTestCase.AssertEquals(Expected, Actual: String);
 begin
-  AssertTrue('Expected: ' + Expected + ' Actual: ' + Actual, Expected = Actual);
+  AssertTrue('Expected: ' + Expected + ' Actual: ' + Actual, Expected = Actual,
+    {$ifdef FPC}get_caller_addr(get_frame){$else}ReturnAddress{$endif});
 end;
 
 procedure TCastleTestCase.AssertFalse(const Msg: String;
-  const ACondition: Boolean);
+  const ACondition: Boolean; ErrorAddr: Pointer);
 begin
-  AssertTrue(Msg, not ACondition);
+  if not Assigned(ErrorAddr) then
+    ErrorAddr := {$ifdef FPC}get_caller_addr(get_frame){$else}ReturnAddress{$endif});
+
+
+  AssertTrue(Msg, not ACondition, ErrorAddr);
 end;
 
 procedure TCastleTestCase.AssertFalse(const ACondition: Boolean);
 begin
-  AssertFalse('', ACondition);
+  AssertFalse('', ACondition,
+    {$ifdef FPC}get_caller_addr(get_frame){$else}System.ReturnAddress{$endif});
 end;
 
 procedure TCastleTestCase.AssertFilenamesEqual(Expected, Actual: String);
 begin
-  AssertTrue('Expected: ' + Expected + ' Actual: ' + Actual, CompareFileName(Expected, Actual));
+  AssertTrue('Expected: ' + Expected + ' Actual: ' + Actual,
+    CompareFileName(Expected, Actual),
+    {$ifdef FPC}get_caller_addr(get_frame){$else}System.ReturnAddress{$endif});
 end;
 
-procedure TCastleTestCase.AssertTrue(const Msg: String; const ACondition: Boolean);
+procedure TCastleTestCase.AssertTrue(const Msg: String;
+  const ACondition: Boolean;  ErrorAddr: Pointer);
 begin
+  if not Assigned(ErrorAddr) then
+    ErrorAddr := {$ifdef FPC}get_caller_addr(get_frame){$else}System.ReturnAddress{$endif};
+
   if not ACondition then
-    Fail(Msg);
+    Fail(Msg, ErrorAddr);
 end;
 
 procedure TCastleTestCase.AssertTrue(const ACondition: Boolean);
 begin
-  AssertTrue('', ACondition);
+  AssertTrue('', ACondition, {$ifdef FPC}get_caller_addr(get_frame){$else}System.ReturnAddress{$endif});
 end;
 
 function TCastleTestCase.CompareFileName(Expected, Actual: String): Boolean;
@@ -188,17 +205,30 @@ begin
   FName := ClassName;
 end;
 
-procedure TCastleTestCase.Fail(const Msg: String);
+procedure TCastleTestCase.Fail(const Msg: String; ErrorAddr: Pointer);
 begin
   if Assigned(FNotifyAssertFail) then
-    FNotifyAssertFail(Msg);
+    FNotifyAssertFail(CurrentTestName, Msg);
 
-  raise EAssertionFailedError.Create(Msg);
+  if Assigned(ErrorAddr) then
+    raise EAssertionFailedError.Create(Msg) at ErrorAddr
+  else
+  begin
+    raise EAssertionFailedError.Create(Msg) at
+      {$ifdef FPC}
+      // FPC https://www.freepascal.org/docs-html/ref/refse118.html#x244-26800017.1
+      get_caller_addr(get_frame)
+      {$else}
+      // Delphi: https://stackoverflow.com/questions/8950513/what-does-at-returnaddress-mean-in-delphi
+      ReturnAddress
+      {$endif};
+  end;
 end;
 
 procedure TCastleTestCase.TestTestCase;
 begin
   WritelnLog('TestTestCase');
+  //AssertTrue(false);
 end;
 
 
