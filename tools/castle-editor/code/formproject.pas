98b26814ea5c861ebfb5e6ced1553b642d84d7eb
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-01-15 03:32:12 +0100

    Do not make TCastleTransformReference compete for UniqueParent
    Also simplify (remove some checks that were now bad) FWorldReferences
    usage. Values > 1 still enable to have UniqueParent.
    
    Also add methods AddParent, RemoveParent, to make code simpler (previously they were in list notification).

--- tools/castle-editor/code/formproject.pas
+++ tools/castle-editor/code/formproject.pas
@@ -1,5 +1,5 @@
 {
-  Copyright 2018-2021 Michalis Kamburelis.
+  Copyright 2018-2022 Michalis Kamburelis.
 
   This file is part of "Castle Game Engine".
 
--- tools/castle-editor/code/framedesign.pas
+++ tools/castle-editor/code/framedesign.pas
@@ -1,5 +1,5 @@
 {
-  Copyright 2018-2018 Michalis Kamburelis.
+  Copyright 2018-2022 Michalis Kamburelis.
 
   This file is part of "Castle Game Engine".
 
