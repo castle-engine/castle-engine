61c3d73c7780fd7f5dfc04438a07a21b1d82c9d4
eugeneloza <zhloza@gmail.com>
2022-01-15 13:16:13 +0200

    Add quotation marks to XML quote

--- tools/castle-editor/code/projectutils.pas
+++ tools/castle-editor/code/projectutils.pas
@@ -110,8 +110,8 @@ procedure AddMacroXmlQuote(const Macros: TStringStringMap; const MacroName: Stri
   function XmlQuote(const S: String): String;
   begin
     Result := SReplacePatterns(S,
-      ['&', '<', '>'],
-      ['&amp;', '&lt;', '&gt;'],
+      ['&', '<', '>', '"'],
+      ['&amp;', '&lt;', '&gt;', '&quot;'],
       false { IgnoreCase; can be false, it doesn't matter, as our patterns are not letters }
     );
   end;
