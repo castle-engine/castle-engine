99c81e0b44852bca23ecf0a7fedac4e9e1b63390
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-05-04 00:46:37 +0200

    In case of GLVersion.BuggyPureShaderPipeline, we cannot use PhongShading

--- src/scene/castleinternalrenderer.pas
+++ src/scene/castleinternalrenderer.pas
@@ -2384,6 +2384,11 @@ begin
      ShapeUsesEnvironmentLight(Shape) or
      (not Shape.Geometry.Solid) { two-sided lighting required by solid=FALSE } then
     PhongShading := true;
+  { As an exception, in case of buggy shader pipeline, force using Gouraud shading.
+    We cannot do Phong shading in this case, as it implies using "pure shader pipeline".
+    See https://forum.castle-engine.io/t/win64-lcl-simple-shapes-something-got-broken/579/14 }
+  if GLVersion.BuggyPureShaderPipeline then
+    PhongShading := false;
 
   Shader.Initialize(PhongShading);
 
