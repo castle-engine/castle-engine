7eaa4e4ce0b74edacd2d668ee3b3e714ceefd0ce
and3md <and3md@gmail.com>
2022-06-07 13:22:20 +0200

    Added physics 2D transformation sync both side example/test.

---
+++ examples/physics/physics_2d_transformation_sync_test/code/gamestatemain.pas
@@ -0,0 +1,95 @@
+{ Main state, where most of the application logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  (This code is in public domain, unlike most other CGE code which
+  is covered by the LGPL license variant, see the COPYING.txt file.) }
+unit GameStateMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleUIState, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleTransform;
+
+type
+  { Main state, where most of the application logic takes place. }
+  TStateMain = class(TUIState)
+  private
+    { Components designed using CGE editor, loaded from gamestatemain.castle-user-interface. }
+    LabelFps: TCastleLabel;
+    ButtonMoveSphere: TCastleButton;
+    Sphere: TCastleTransform;
+
+    procedure ButtonMoveSphereClick(Sender: TObject);
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+    function Press(const Event: TInputPressRelease): Boolean; override;
+  end;
+
+var
+  StateMain: TStateMain;
+
+implementation
+
+uses SysUtils;
+
+{ TStateMain ----------------------------------------------------------------- }
+
+procedure TStateMain.ButtonMoveSphereClick(Sender: TObject);
+begin
+  Sphere.Translation := Vector3(300, 500, 0);
+end;
+
+constructor TStateMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gamestatemain.castle-user-interface';
+end;
+
+procedure TStateMain.Start;
+begin
+  inherited;
+
+  { Find components, by name, that we need to access from code }
+  LabelFps := DesignedComponent('LabelFps') as TCastleLabel;
+  ButtonMoveSphere := DesignedComponent('ButtonMoveSphere') as TCastleButton;
+  Sphere := DesignedComponent('Sphere') as TCastleTransform;
+
+  ButtonMoveSphere.OnClick := @ButtonMoveSphereClick;
+end;
+
+procedure TStateMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame.}
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+function TStateMain.Press(const Event: TInputPressRelease): Boolean;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  { This virtual method is executed when user presses
+    a key, a mouse button, or touches a touch-screen.
+
+    Note that each UI control has also events like OnPress and OnClick.
+    These events can be used to handle the "press", if it should do something
+    specific when used in that UI control.
+    The TStateMain.Press method should be used to handle keys
+    not handled in children controls.
+  }
+
+  // Use this to handle keys:
+  {
+  if Event.IsKey(keyXxx) then
+  begin
+    // DoSomething;
+    Exit(true); // key was handled
+  end;
+  }
+end;
+
+end.
