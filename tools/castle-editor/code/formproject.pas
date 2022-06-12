f60cf4c9aa3c737586a93d8827c54a050de04bee
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-06-12 04:42:31 +0200

    Apply the hack from https://github.com/michaliskambi/lcl-test-menu-special-shortcuts to handle menu items with special shortcuts

--- tools/castle-editor/code/formproject.pas
+++ tools/castle-editor/code/formproject.pas
@@ -274,6 +274,7 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormHide(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure FormShow(Sender: TObject);
     procedure ListOutputClick(Sender: TObject);
     procedure ListOutputDblClick(Sender: TObject);
@@ -441,7 +442,8 @@ implementation
 
 {$R *.lfm}
 
-uses TypInfo, LCLType, RegExpr,
+uses TypInfo, LCLType, RegExpr, StrUtils,
+  {$ifdef MSWINDOWS} Windows, {$endif}
   CastleXMLUtils, CastleLCLUtils, CastleOpenDocument, CastleURIUtils,
   CastleFilesUtils, CastleUtils, CastleVectors, CastleColors, CastleConfig,
   CastleScene, CastleViewport, Castle2DSceneManager, CastleCameras,
@@ -1367,6 +1369,141 @@ begin
       UserConfig.SetValue('ProjectForm_Design.PanelLeft.Width', Design.PanelLeft.Width);
     end;
     UserConfig.Save;
+  end;
+end;
+
+procedure TProjectForm.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+
+  { When pressing a letter, should we make it upper?
+    It sucks, but in this workaround we cannot depend on OnKeyPress reaching us
+    (it doesn't reach us when menu item intercepts the key)
+    so we need to calculate key->char here. }
+  function LettersUpCase: Boolean;
+  begin
+    Result := false;
+    // TODO: This is Windows-only.
+    {$ifdef MSWINDOWS}
+    { See https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getkeystate .
+      Read low-order bit of VK_CAPITAL (caps lock) state. }
+    if (GetKeyState(VK_CAPITAL) and 1) <> 0 then
+      Result := true;
+    {$endif}
+    if ssShift in Shift then
+      Result := not Result;
+  end;
+
+begin
+  { This is a hack to
+    - Have some menu items with simple shortcuts, like Home, F, Ctrl+Z
+      (and *visible* in menu with these shortcuts, to be discoverable)
+    - but still allow user to interact with text input (like TEdit)
+      using Home, F, Ctrl+Z etc.
+
+    The intuitive result should be:
+    - using Home, F, Ctrl+Z etc. when TEdit is focused, makes normal text-editing
+      operation
+    - only if no text-editing is focused,
+      it goes to the specific custom action we wanted to assign to it.
+
+    Rejected workarounds:
+    - use only shortcuts that avoid common text-editing.
+      Like Ctrl+Home, Ctrl+F, Ctrl+Alt+Z.
+      Rejected because these are important shortcuts.
+      For ease of use, for consistency with other software --
+      we want to keep them easy.
+    - use simple shortcuts, but not set them as menu item shortcuts.
+      Just dispatch them yourself if e.g. TCastleControl is focused.
+      Rejected: then UI sucks, we cannot show shortcuts in menu in standard way.
+
+    This workaround depends on Lazarus behaviors (tested on WinAPI, GTK, Cocoa
+    backends):
+    - Key being used to activate menu item is still send to TForm1.FormKeyDown
+      when form has KeyPreview
+    - We can resign from "normal" key handling (which means running menu item
+      for these special keys) by assigning Key := 0
+    - Our 2 important text-editing states are TEdit and editing in TTreeView,
+      the latter is luckily just an internal TEdit too.
+
+    Note: This is still ugly and inherently unreliable workaround, because we
+    interpret what some special built-in keys should do. So we need to
+    capture all such keys, and implement same logic...
+    E.g. pressing "f" key, with and without Shift, with and without CapsLock,
+    should be consistent with system-handled "g" key.
+    - What happens if we don't include special handling for some key,
+      but we should? In the worst case it will not work with edit boxes,
+      and always activate menu command.
+    - What happens if we included unnecessarily some key in this treatment?
+      Everything should be OK, if only we simulate default behavior OK.
+
+    Tested: It is necessary, and good workaround,
+    - (FPC 3.2.2, LCL 2.2) on Windows with WinAPI widgetset.
+    - (FPC 3.2.2, LCL 2.2) on Linux with GTK widgetset.
+    - (FPC 3.2.2, LCL 2.2) on macOS with Cocoa widgetset.
+
+    See https://github.com/michaliskambi/lcl-test-menu-special-shortcuts
+    for separated LCL application testing this.
+
+    Note: TTreeView also handles Home / End.
+    Strangely, it behaves like we want out-of-the-box:
+    it intercepts Home / End when focused, regardless of if some menu item
+    has such shortcut.
+    Tested this is true on:
+    - (FPC 3.2.2, LCL 2.2) on Windows with WinAPI widgetset.
+    - (FPC 3.2.2, LCL 2.2) on Linux with GTK widgetset.
+    - (FPC 3.2.2, LCL 2.2) on macOS with Cocoa widgetset.
+  }
+
+  if ActiveControl is TEdit then
+  begin
+    case Key of
+      VK_HOME: TEdit(ActiveControl).SelStart := 0;
+      {$ifdef LCLCocoa}
+      { If user didn't adjust Home/End system-wide, then actually
+        by default Home/End do nothing in TEdit.
+        So we could disable Home...
+        but it seems more useful to make Home/End just work in CGE TEdit. }
+      VK_END: TEdit(ActiveControl).SelStart := Length(TEdit(ActiveControl).Text);
+      {$endif}
+      VK_0..VK_9: TEdit(ActiveControl).SelText := Chr(Ord('0') + Key - VK_0);
+      VK_F: TEdit(ActiveControl).SelText := IfThen(LettersUpCase, 'F', 'f');
+      VK_Z:
+        if Shift = [ssCtrl] then
+          TEdit(ActiveControl).Undo
+        else
+          Exit; // resign from special handling
+        //if Shift = [ssCtrl, ssShift] then
+        //  TEdit(ActiveControl).Redo
+        //else
+      VK_C:
+        if Shift = [ssCtrl] then
+          TEdit(ActiveControl).CopyToClipboard
+        else
+          Exit; // resign from special handling
+      VK_V:
+        if Shift = [ssCtrl] then
+          TEdit(ActiveControl).PasteFromClipboard
+        else
+          Exit; // resign from special handling
+      VK_X:
+        if Shift = [ssCtrl] then
+          TEdit(ActiveControl).CutToClipboard
+        else
+          Exit; // resign from special handling
+      VK_DELETE:
+        begin
+          if TEdit(ActiveControl).SelText <> '' then
+            TEdit(ActiveControl).SelText := ''
+          else
+            TEdit(ActiveControl).Text :=
+              Copy(TEdit(ActiveControl).Text, 1, TEdit(ActiveControl).SelStart) +
+              SEnding(TEdit(ActiveControl).Text, TEdit(ActiveControl).SelStart + 2);
+        end;
+      else
+        Exit; // resign from special handling
+    end;
+
+    Key := 0; // prevent key reaching normal menu/action event
   end;
 end;
 
