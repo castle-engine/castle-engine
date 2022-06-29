d564949b17d0bc671bc0c92111b8303caf42973b
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-06-29 15:14:51 +0200

    Split into separate TestReadingOldDesigns2DSceneManager

--- tests/code/testcases/testcastleviewport.pas
+++ tests/code/testcases/testcastleviewport.pas
@@ -26,6 +26,7 @@ uses
 type
   TTestCastleViewport = class(TCastleTestCase)
     procedure TestReadingOldDesigns;
+    procedure TestReadingOldDesigns2DSceneManager;
   end;
 
 implementation
@@ -42,25 +43,6 @@ procedure TTestCastleViewport.TestReadingOldDesigns;
     AssertTrue(V.Camera.Name = CameraName);
     AssertTrue(V.Items.Count > 0);
     AssertTrue(V.Items[V.Items.Count - 1] = V.Camera);
-  end;
-
-  { Check camera will be inserted into TCastle2DSceneManager items. }
-  procedure AssertCameraUpgraded2DSceneManager(const V: TCastle2DSceneManager);
-  var
-    DummyHandleInput: Boolean;
-  begin
-    { TCastle2DSceneManager may not include camera as a subcomponent,
-      it will be added later during Update. }
-    AssertTrue(V.Camera = nil);
-
-    DummyHandleInput := true;
-    V.Update(1/30, DummyHandleInput);
-
-    AssertTrue(V.Camera <> nil);
-    AssertTrue(V.Items.Count > 0);
-    AssertTrue(V.Items[V.Items.Count - 1] = V.Camera);
-    AssertTrue(V.Camera.Name = ''); // created by SetupCamera, nameless, to not conflict with existing components
-    AssertTrue(V.Camera.ProjectionType = ptOrthographic);
   end;
 
 var
@@ -98,7 +80,33 @@ begin
     AssertTrue((RootOwner.FindRequiredComponent('ExamineNavigation1') as TCastleNavigation).Parent =
       RootOwner.FindRequiredComponent('Viewport'));
   finally FreeAndNil(RootOwner) end;
+end;
 
+procedure TTestCastleViewport.TestReadingOldDesigns2DSceneManager;
+
+  { Check camera will be inserted into TCastle2DSceneManager items. }
+  procedure AssertCameraUpgraded2DSceneManager(const V: TCastle2DSceneManager);
+  var
+    DummyHandleInput: Boolean;
+  begin
+    { TCastle2DSceneManager may not include camera as a subcomponent,
+      it will be added later during Update. }
+    AssertTrue(V.Camera = nil);
+
+    DummyHandleInput := true;
+    V.Update(1/30, DummyHandleInput);
+
+    AssertTrue(V.Camera <> nil);
+    AssertTrue(V.Items.Count > 0);
+    AssertTrue(V.Items[V.Items.Count - 1] = V.Camera);
+    AssertTrue(V.Camera.Name = ''); // created by SetupCamera, nameless, to not conflict with existing components
+    AssertTrue(V.Camera.ProjectionType = ptOrthographic);
+  end;
+
+var
+  RootOwner: TComponent;
+  Root: TCastleUserInterface;
+begin
   RootOwner := TComponent.Create(nil);
   Root := UserInterfaceLoad('castle-data:/designs_before_camera_as_transform/old_scaling_tests_2dscenemanager_without_camera.castle-user-interface', RootOwner);
   try
