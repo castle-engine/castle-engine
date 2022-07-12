ac9a6e3e9c83a3f7ed0a01f5d3f2ecb318645976
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-07-13 00:48:47 +0200

    Comma fix

--- tests/code/testcases/testcastleshapes.pas
+++ tests/code/testcases/testcastleshapes.pas
@@ -20,7 +20,7 @@ unit TestCastleShapes;
 interface
 
 uses
-  Classes, SysUtils {$ifndef CASTLE_TESTER}FpcUnit, TestUtils, TestRegistry,
+  Classes, SysUtils, {$ifndef CASTLE_TESTER}FpcUnit, TestUtils, TestRegistry,
   CastleTestCase {$else}CastleTester{$endif};
 
 type
