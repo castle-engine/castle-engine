6382bb7a6c3eb998c9e9072c941a4656dc9371f9
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-10-04 09:23:39 +0200

    No need to use CastleViewport in CastleTransform now, update check_units_dependencies exceptions

--- src/transform/castletransform.pas
+++ src/transform/castletransform.pas
@@ -47,7 +47,7 @@ implementation
 
 uses CastleLog, CastleApplicationProperties, CastleURIUtils, CastleInternalRays,
   // TODO: this breaks unit dependencies, transform->scene
-  X3DNodes, CastleScene, CastleViewport, CastleInternalPhysicsVisualization;
+  X3DNodes, CastleScene, CastleInternalPhysicsVisualization;
 
 {$define read_implementation}
 {$I castletransform_initial_types.inc}
