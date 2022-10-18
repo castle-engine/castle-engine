3098197decb021a98a169bd5c4acde24a1ae2292
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-10-18 11:42:35 +0200

    Add Assert that "LabelFps <> nil" to have clearer error message in case someone removes LabelFps from design

--- tools/castle-editor/data/project_templates/2d_game/files/code/gamestateplay.pas
+++ tools/castle-editor/data/project_templates/2d_game/files/code/gamestateplay.pas
@@ -64,6 +64,7 @@ begin
   inherited;
   { This virtual method is executed every frame.}
 
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
   LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
 
   if DragonFlying then
