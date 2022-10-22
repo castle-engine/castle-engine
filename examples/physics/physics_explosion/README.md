16ee1cd202f2323e917e33ded53f9cbebb338191
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-10-22 05:03:27 +0200

    Explosion demo - use ApplyImpulse to throw stuff around, and show collisions on terrain

---
+++ examples/physics/physics_explosion/README.md
@@ -0,0 +1,17 @@
+# Explosion!
+
+Demo of using `TCastleRigidBody.ApplyImpulse` to simulate an explosion throwing away a number of rigid bodies around.
+
+Also shows that `TCastleMeshCollider` on a terrain works.
+
+Using [Castle Game Engine](https://castle-engine.io/).
+
+## Building
+
+Compile by:
+
+- [CGE editor](https://castle-engine.io/manual_editor.php). Just use menu item _"Compile"_.
+
+- Or use [CGE command-line build tool](https://castle-engine.io/build_tool). Run `castle-engine compile` in this directory.
+
+- Or use [Lazarus](https://www.lazarus-ide.org/). Open in Lazarus `physics_explosion_standalone.lpi` file and compile / run from Lazarus. Make sure to first register [CGE Lazarus packages](https://castle-engine.io/documentation.php).
---
+++ examples/physics/physics_explosion/code/gamestatemain.pas
@@ -0,0 +1,116 @@
+{
+  Copyright 2022-2022 Michalis Kamburelis.
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
+{ Main state, where most of the application logic takes place. }
+unit GameStateMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleUIState, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleTerrain,
+  CastleCameras, CastleScene, CastleViewport, CastleTransform, CastleSoundEngine;
+
+type
+  { Main state, where most of the application logic takes place. }
+  TStateMain = class(TUIState)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    WalkNavigation1: TCastleWalkNavigation;
+    Viewport1: TCastleViewport;
+    SceneDetonate: TCastleScene;
+    SoundDetonate: TCastleSound;
+    Boxes: TCastleTransform;
+    SceneExplosionCenter: TCastleTransform;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+    function Press(const Event: TInputPressRelease): Boolean; override;
+  end;
+
+var
+  StateMain: TStateMain;
+
+implementation
+
+uses SysUtils;
+
+{ TStateMain ----------------------------------------------------------------- }
+
+constructor TStateMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gamestatemain.castle-user-interface';
+end;
+
+procedure TStateMain.Start;
+begin
+  inherited;
+end;
+
+procedure TStateMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame.}
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+function TStateMain.Press(const Event: TInputPressRelease): Boolean;
+const
+  ExplosionStrength = 10;
+var
+  Box: TCastleTransform;
+  RBody: TCastleRigidBody;
+  ExplosionCenter, ForceDir: TVector3;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  if Event.IsMouseButton(buttonLeft) then
+  begin
+    if Viewport1.TransformUnderMouse = SceneDetonate then
+    begin
+      SceneDetonate.PlayAnimation('detonate', false);
+      SoundEngine.Play(SoundDetonate);
+      ExplosionCenter := SceneExplosionCenter.WorldTranslation;
+      for Box in Boxes do
+      begin
+        RBody := Box.FindBehavior(TCastleRigidBody) as TCastleRigidBody;
+        if RBody <> nil then
+        begin
+          ForceDir := Box.WorldTranslation - ExplosionCenter;
+          ForceDir.Y := 2; // make sure to have some force upward, just looks more impressive
+          RBody.ApplyImpulse(
+            ForceDir.AdjustToLength(ExplosionStrength),
+            ExplosionCenter);
+        end;
+      end;
+
+      Exit(true);
+    end;
+  end;
+
+  if Event.IsMouseButton(buttonRight) then
+  begin
+    WalkNavigation1.MouseLook := not WalkNavigation1.MouseLook;
+    Exit(true);
+  end;
+end;
+
+end.
