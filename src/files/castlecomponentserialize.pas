c21b11a77b2346d4af6cb5be333b9962cdfab2bc
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-12-01 03:43:07 +0100

    Common code for T3DCoord serialization on FPC/Delphi

--- src/files/castlecomponentserialize.pas
+++ src/files/castlecomponentserialize.pas
@@ -1067,11 +1067,8 @@ end;
 procedure TCastleJsonWriter.StreamProperty(Sender: TObject;
   AObject: TObject; Info: PPropInfo; var Res: TJsonData);
 var
-  {$ifdef FPC}
-  ValueOfSet, I: Integer;
-  {$else}
-  ValueOfSet, I: Byte;
-  {$endif}
+  ValueOfSet: {$ifdef FPC} Integer {$else} Byte {$endif};
+  Coord: T3DCoord;
 begin
   if Info^.Name = 'Name' then
   begin
@@ -1123,15 +1120,9 @@ begin
     ValueOfSet := GetOrdProp(AObject, Info);
     FreeAndNil(Res);
     Res := TJSONArray.Create;
-    {$ifdef FPC}
-    for I := Low(T3DCoords) to High(T3DCoords) do
-      if (I in T3DCoords(ValueOfSet)) then
-        TJSONArray(Res).Add(I)
-    {$else}
-    for I := Low(T3DCoord) to High(T3DCoord) do
-      if (I in T3DCoords(ValueOfSet)) then
-        TJSONArray(Res).Add(I)
-    {$endif}
+    for Coord := Low(T3DCoord) to High(T3DCoord) do
+      if (Coord in T3DCoords(ValueOfSet)) then
+        TJSONArray(Res).Add(Coord);
   end;
 end;
 
