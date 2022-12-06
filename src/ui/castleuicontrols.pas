7ea64ef3fbbec2907703141b404760e5fdee37ad
and3md <and3md@gmail.com>
2022-12-06 17:13:01 +0100

    Remove unused edit focus approach in TCastleContainer

--- src/ui/castleuicontrols.pas
+++ src/ui/castleuicontrols.pas
@@ -52,7 +52,7 @@ uses DOM, TypInfo, Math,
   {$ifdef FPC} CastleGL, {$else} OpenGL, OpenGLext, {$endif}
   CastleLog, CastleXMLUtils, CastleStringUtils,
   CastleInternalSettings, CastleFilesUtils, CastleURIUtils, CastleRenderOptions,
-  CastleInternalInspector, CastleInternalControlsImages, CastleControls;
+  CastleInternalInspector, CastleInternalControlsImages;
 
 {$define read_implementation}
 {$I castleuicontrols_initial_types.inc}
