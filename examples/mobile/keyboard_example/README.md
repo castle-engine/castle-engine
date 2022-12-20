48771eaf78275f034a74df9b0d3d0b5026903d43
and3md <and3md@gmail.com>
2022-12-20 10:45:29 +0100

    Completed example/test of the on-screen keyboard.

---
+++ examples/mobile/keyboard_example/README.md
@@ -0,0 +1,8 @@
+# On-screen android keyboard test
+
+This is an example of calling the on-screen keyboard for android:
+
+- tap edit to show keyboard
+- third edit shows password mode
+
+See https://castle-engine.io/manual_cross_platform.php for information how to compile and run it on various devices.
--- examples/mobile/keyboard_example/code/gamestatemain.pas
+++ examples/mobile/keyboard_example/code/gamestatemain.pas
@@ -1,3 +1,18 @@
+{
+  Copyright 2022 Andrzej Kilijanski, Michalis Kamburelis.
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
 { Main state, where most of the application logic takes place.
 
   Feel free to use this code as a starting point for your own projects.
@@ -25,7 +40,6 @@ type
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
     procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
-    function Press(const Event: TInputPressRelease): Boolean; override;
 
     procedure ButtonShowClick(Sender: TObject);
     procedure ButtonHideClick(Sender: TObject);
@@ -59,31 +73,6 @@ begin
   { This virtual method is executed every frame.}
   Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
   LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
-end;
-
-function TStateMain.Press(const Event: TInputPressRelease): Boolean;
-begin
-  Result := inherited;
-  if Result then Exit; // allow the ancestor to handle keys
-
-  { This virtual method is executed when user presses
-    a key, a mouse button, or touches a touch-screen.
-
-    Note that each UI control has also events like OnPress and OnClick.
-    These events can be used to handle the "press", if it should do something
-    specific when used in that UI control.
-    The TStateMain.Press method should be used to handle keys
-    not handled in children controls.
-  }
-
-  // Use this to handle keys:
-  {
-  if Event.IsKey(keyXxx) then
-  begin
-    // DoSomething;
-    Exit(true); // key was handled
-  end;
-  }
 end;
 
 procedure TStateMain.ButtonShowClick(Sender: TObject);
