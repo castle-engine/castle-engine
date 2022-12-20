1e589d2eb82693a14c69a186fa8b1daee6e06a93
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-12-20 05:38:46 +0100

    Update TCastleWindow docs following castleglcontextwgl docs

--- src/window/castlewindow.pas
+++ src/window/castlewindow.pas
@@ -1282,34 +1282,27 @@ type
       Note that we have a fallback mechanism in case @link(DepthBits)
       is too large: we fallback on @link(DepthBitsFallback) then.
 
-      @italic(Design notes:) One may ask why default value is not 0?
+      @italic(Design notes:) Why default value is not 0?
 
       @orderedList(
         @item(
-          Most programs using OpenGL use depth testing, so many programs
-          would have to call something like @code(Window.DepthBits := DefaultDepthBits).)
+          Most programs using OpenGL use 3D and so use depth testing.
+          So many programs
+          would have to call something like @code(Window.DepthBits := DefaultDepthBits).
+        )
 
         @item(
           Often graphic cards / window systems / OSes give you an OpenGL
           context with depth buffer @italic(even if you don't need depth buffer).
-          I don't say that it's bad. But it makes very easy to forget about
-          doing @code(DepthBits := something-non-zero;).
-          If you're writing 3d program and sitting on some
-          system that always gives you depth buffer (even if DepthBits = 0)
-          then it may happen that you forget to write in your program
-          @longCode(#  Window.DepthBits := DefaultDepthBits;#)
-
-          And while on your system everything will work, you will
-          receive errors on other systems because you forgot to request a
-          depth buffer.)
+          This makes it easy to forget about setting DepthBits to something
+          non-zero, because on @italic(your) system you may happen
+          to always get some depth buffer.
+        )
       )
 
       If you are writing a program that does not need depth buffer
-      you should set Window.DepthBits := 0. The only advantage of having
-      default DepthBits = DefaultDepthBits is that if you forget to set
-      Window.DepthBits := 0 your programs will still work (most graphic cards
-      will give you some depth buffer anyway).
-      They will just use more resources than they should.
+      you can set Window.DepthBits := 0, to inform OpenGL it doesn't need
+      to allocate any depth buffer.
     }
     property DepthBits: Cardinal
       read FDepthBits write FDepthBits default DefaultDepthBits;
@@ -1392,10 +1385,7 @@ type
       Just like with other XxxBits property, we may get more
       bits than we requested. But we will never get less --- if window system
       will not be able to provide GL context with requested number of bits,
-      @link(Open) will raise an error.
-
-      It's undefined how I'll treat this variable when indexed color mode
-      will be possible in TCastleWindow. }
+      @link(Open) will raise an error. }
     property AlphaBits: Cardinal
       read FAlphaBits write FAlphaBits default 0;
 
