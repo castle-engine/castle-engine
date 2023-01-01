e814fb3d06cbc97180b7925e4088cbc6acc4a3e8
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-02 00:43:00 +0100

    Use better initialization template for tester, also simplify Parameters.IndexOf('--console') tests

--- tests/code/tester-castle/gameinitialize.pas
+++ tests/code/tester-castle/gameinitialize.pas
@@ -52,22 +52,43 @@ begin
 end;
 
 initialization
-  if Parameters.IndexOf('--console') = -1 then
-  begin
+  { This initialization section configures:
+    - Application.OnInitialize
+    - Application.MainWindow
+    - determines initial window size
 
-    { Initialize Application.OnInitialize. }
-    Application.OnInitialize := @ApplicationInitialize;
+    You should not need to do anything more in this initialization section.
+    Most of your actual application initialization (in particular, any file reading)
+    should happen inside ApplicationInitialize. }
 
-    { Create and assign Application.MainWindow. }
-    Window := TCastleWindow.Create(Application);
-    Window.ParseParameters; // allows to control window size / fullscreen on the command-line
-    Application.MainWindow := Window;
+  Application.OnInitialize := @ApplicationInitialize;
 
-  { You should not need to do *anything* more in the unit "initialization" section.
-    Most of your game initialization should happen inside ApplicationInitialize.
-    In particular, it is not allowed to read files before ApplicationInitialize
-    (because in case of non-desktop platforms,
-    some necessary resources may not be prepared yet). }
-  end else
-    InitializeLog;
+  Window := TCastleWindow.Create(Application);
+  Application.MainWindow := Window;
+
+  { Optionally, adjust window fullscreen state and size at this point.
+    Examples:
+
+    Run fullscreen:
+
+      Window.FullScreen := true;
+
+    Run in a 600x400 window:
+
+      Window.FullScreen := false; // default
+      Window.Width := 600;
+      Window.Height := 400;
+
+    Run in a window taking 2/3 of screen (width and height):
+
+      Window.FullScreen := false; // default
+      Window.Width := Application.ScreenWidth * 2 div 3;
+      Window.Height := Application.ScreenHeight * 2 div 3;
+
+    Note that some platforms (like mobile) ignore these window sizes.
+  }
+
+  { Handle command-line parameters like --fullscreen and --window.
+    By doing this last, you let user to override your fullscreen / mode setup. }
+  Window.ParseParameters;
 end.
