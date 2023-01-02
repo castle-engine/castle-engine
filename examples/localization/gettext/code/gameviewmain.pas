ae8dcd6656c048e3f048c6e7155b99540d183002
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-02 02:27:41 +0100

    Modernize gettext example - TCastleText in 3D translatable just like labels

--- examples/localization/gettext/code/gameviewmain.pas
+++ examples/localization/gettext/code/gameviewmain.pas
@@ -21,27 +21,33 @@ interface
 
 uses Classes, Generics.Collections,
   CastleUIControls, CastleComponentSerialize, CastleControls, CastleTimeUtils,
-  X3DNodes, CastleTextureFontData, CastleScene, CastleWindow;
+  X3DNodes, CastleTextureFontData, CastleScene, CastleWindow, CastleFonts, CastleViewport;
 
 type
   { Main user interface class.
     This implements the majority of this application functionality. }
   TViewMain = class(TCastleView)
+  published
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    Viewport: TCastleViewport;
+    ButtonSwitchEnglish, ButtonSwitchGerman, ButtonSwitchPolish,
+      ButtonSwitchRussian, ButtonSwitchUkrainian: TCastleButton;
+    ButtonMessage: TCastleButton;
+    Text3d: TCastleText;
   strict private
     Language: string;
 
     { For animating rotating text. }
     LifeTime: TFloatTime;
-    TextTransform: TTransformNode;
 
-    TextNode: TTextNode;
+    MyFont: TCastleFont;
 
     { Load GetText MO files for new language,
       recreate user interface to show new language (calling InitializeUserInterface). }
     procedure InitializeLanguage(const NewLanguage: string);
-    procedure InitializeUserInterface;
 
-    procedure GetFont(const FontStyle: TFontStyleNode; var Font: TTextureFontData);
     procedure ClickButtonMessage(Sender: TObject);
     procedure ClickButtonEnglish(Sender: TObject);
     procedure ClickButtonGerman(Sender: TObject);
@@ -62,8 +68,8 @@ var
 implementation
 
 uses SysUtils,
-  CastleLog, CastleURIUtils, CastleFonts,
-  CastleLocalizationGetText, CastleColors, CastleViewport, CastleSystemLanguage,
+  CastleLog, CastleURIUtils,
+  CastleLocalizationGetText, CastleColors, CastleSystemLanguage,
   CastleVectors, CastleMessages,
   Font_DejaVuSans;
 
@@ -71,7 +77,6 @@ uses SysUtils,
   Note that we use English as "translation id".
   But this is optional, you could as well use some internal-only ids. }
 resourcestring
-  Text3D = 'I am 3D Text';
   MessageText = 'I am a message!';
 
 { TViewMain ----------------------------------------------------------------- }
@@ -83,6 +88,20 @@ begin
 end;
 
 procedure TViewMain.InitializeLanguage(const NewLanguage: string);
+
+  procedure InitializeUserInterface;
+  begin
+    { reassign things after DesignUrl changed, changing all component references }
+    ButtonSwitchEnglish.OnClick := {$ifdef FPC}@{$endif} ClickButtonEnglish;
+    ButtonSwitchGerman.OnClick := {$ifdef FPC}@{$endif} ClickButtonGerman;
+    ButtonSwitchPolish.OnClick := {$ifdef FPC}@{$endif} ClickButtonPolish;
+    ButtonSwitchRussian.OnClick := {$ifdef FPC}@{$endif} ClickButtonRussian;
+    ButtonSwitchUkrainian.OnClick := {$ifdef FPC}@{$endif} ClickButtonUkrainian;
+    ButtonMessage.OnClick := {$ifdef FPC}@{$endif} ClickButtonMessage;
+
+    Text3d.CustomFont := MyFont;
+  end;
+
 begin
   Language := NewLanguage;
 
@@ -101,75 +120,28 @@ begin
     You could also preserve the TCastleEdit contents around it. }
   DesignUrl := '';
   DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+
   InitializeUserInterface;
-end;
-
-procedure TViewMain.InitializeUserInterface;
-
-  function BuildScene: TX3DRootNode;
-  var
-    FontStyle: TFontStyleNode;
-    TextShape: TShapeNode;
-    Material: TUnlitMaterialNode;
-  begin
-    FontStyle := TFontStyleNode.Create;
-    FontStyle.Justify := fjMiddle;
-    FontStyle.JustifyMinor := fjMiddle;
-
-    TextNode := TTextNode.CreateWithTransform(TextShape, TextTransform);
-    TextNode.FontStyle := FontStyle;
-    TextNode.Solid := false;
-    TextNode.SetText([Text3D]);
-
-    Material := TUnlitMaterialNode.Create;
-    Material.EmissiveColor := YellowRGB;
-    TextShape.Material := Material;
-
-    Result := TX3DRootNode.Create;
-    Result.AddChildren(TextTransform);
-  end;
-
-var
-  Scene: TCastleScene;
-  Viewport: TCastleViewport;
-  ButtonSwitchEnglish, ButtonSwitchGerman, ButtonSwitchPolish,
-    ButtonSwitchRussian, ButtonSwitchUkrainian: TCastleButton;
-  ButtonMessage: TCastleButton;
-begin
-  Viewport := DesignedComponent('Viewport') as TCastleViewport;
-  ButtonSwitchEnglish := DesignedComponent('ButtonSwitchEnglish') as TCastleButton;
-  ButtonSwitchGerman := DesignedComponent('ButtonSwitchGerman') as TCastleButton;
-  ButtonSwitchPolish := DesignedComponent('ButtonSwitchPolish') as TCastleButton;
-  ButtonSwitchRussian := DesignedComponent('ButtonSwitchRussian') as TCastleButton;
-  ButtonSwitchUkrainian := DesignedComponent('ButtonSwitchUkrainian') as TCastleButton;
-  ButtonMessage := DesignedComponent('ButtonMessage') as TCastleButton;
-
-  { build 3D view }
-  Scene := TCastleScene.Create(Application);
-  Scene.Load(BuildScene, true);
-  Scene.ProcessEvents := true;
-  Viewport.Items.Add(Scene);
-
-  { assign callbacks }
-  ButtonSwitchEnglish.OnClick := {$ifdef FPC}@{$endif} ClickButtonEnglish;
-  ButtonSwitchGerman.OnClick := {$ifdef FPC}@{$endif} ClickButtonGerman;
-  ButtonSwitchPolish.OnClick := {$ifdef FPC}@{$endif} ClickButtonPolish;
-  ButtonSwitchRussian.OnClick := {$ifdef FPC}@{$endif} ClickButtonRussian;
-  ButtonSwitchUkrainian.OnClick := {$ifdef FPC}@{$endif} ClickButtonUkrainian;
-  ButtonMessage.OnClick := {$ifdef FPC}@{$endif} ClickButtonMessage;
 end;
 
 procedure TViewMain.Start;
 begin
   inherited;
 
-  { Make sure the font contains all international characters.
-    TFontStyleNode.OnFont is used by TTextNode.
+  { Load custom font, to make sure font contains all international characters.
+    TFontStyleNode.CustomFont is used by TTextNode.
     Everything else uses UIFont (by default). }
-  TFontStyleNode.OnFont := {$ifdef FPC}@{$endif} GetFont;
-  UIFont := TCastleFont.Create(Self);
-  (UIFont as TCastleFont).Load(TextureFont_DejaVuSans_50, false);
-  UIFont.Size := 20;
+
+  MyFont := TCastleFont.Create(FreeAtStop);
+  MyFont.Load(TextureFont_DejaVuSans_50, false);
+  MyFont.Size := 20; // size for UI
+  WritelnLog('Font loaded, size %f, row height %f (should be similar)', [
+    MyFont.Size,
+    MyFont.RowHeight
+  ]);
+
+  Text3d.CustomFont := MyFont;
+  UIFont := MyFont;
 
   { initialize language, InitializeLanguage will also intialize UI }
   Language := SystemLanguage;
@@ -182,13 +154,9 @@ end;
 procedure TViewMain.Update(const SecondsPassed: Single;
   var HandleInput: boolean);
 begin
+  inherited;
   LifeTime += SecondsPassed;
-  TextTransform.Rotation := Vector4(0, 1, 0, LifeTime);
-end;
-
-procedure TViewMain.GetFont(const FontStyle: TFontStyleNode; var Font: TTextureFontData);
-begin
-  Font := TextureFont_DejaVuSans_50;
+  Text3d.Rotation := Vector4(0, 1, 0, LifeTime);
 end;
 
 procedure TViewMain.ClickButtonMessage(Sender: TObject);
