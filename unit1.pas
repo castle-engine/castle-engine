8f02adce7287a355e6aef1e5b21848219418226c
eugeneloza <zhloza@gmail.com>
2023-01-06 14:27:13 +0200

    Old experiment

---
+++ unit1.pas
@@ -0,0 +1,102 @@
+unit Unit1;
+
+{$apptype console}
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, DynLibs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+type
+  SteamAPIWarningMessageHook = procedure (nSeverity: Integer; pchDebugText: PAnsiChar); Cdecl;
+
+procedure WarningHook(nSeverity: Integer; pchDebugText: PAnsiChar); Cdecl;
+begin
+  WriteLn(NSeverity, pchDebugText^);
+end;
+
+const
+  SteamLib = 'steam_api64';
+
+function SteamAPI_Init(): Boolean; CDecl; external SteamLib;
+procedure SteamAPI_Shutdown(); CDecl; external SteamLib;
+function SteamAPI_ISteamUserStats_SetAchievement(pchName: PAnsiChar): Boolean; CDecl; external SteamLib;
+function SteamAPI_ISteamUtils_GetAppID(): UInt32; CDecl; external SteamLib;
+function SteamAPI_RestartAppIfNecessary(): Boolean; CDecl; external SteamLib;
+function SteamAPI_ISteamUser_BLoggedOn(): Boolean; CDecl; external SteamLib;
+procedure SteamAPI_ISteamClient_SetWarningMessageHook(pFunction: SteamAPIWarningMessageHook); CDecl; external SteamLib;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  if SteamAPI_Init() then
+  begin
+    WriteLn('log: SteamAPI_Init successfull');
+
+    if SteamAPI_RestartAppIfNecessary() then
+    begin
+      WriteLn('The app was run through exe - restarting through Steam. DRM will do this automatically.');
+      Halt(0);
+    end else
+      WriteLn('The Steam client is running and no restart is necessary');
+  end;
+
+  //SteamAPI_ISteamClient_SetWarningMessageHook(@WarningHook);
+
+  //if SteamAPI_ISteamUser_BLoggedOn() then
+    WriteLn('Login successful');
+
+  //https://partner.steamgames.com/doc/features/achievements
+
+  // ISteamUserStats::RequestCurrentStats
+  // ISteamUserStats::GetAchievement
+
+  {If you want to display the achievements in your game you can use ISteamUserStats::GetAchievementDisplayAttribute to retrieve human-readable properties of the achievement, including its name ("name") and description ("desc"). These properties are localizable on the Steamworks Partner Website, and the returned data varies with the language in which the user is running the game. You can also get an achievement's icon using ISteamUserStats::GetAchievementIcon or the time each achievements was unlocked with ISteamUserStats::GetAchievementAndUnlockTime.}
+
+  //ISteamUserStats::SetAchievement
+  //ISteamUserStats::StoreStats
+  //wait for ISteamUserStats::UserAchievementStored_t
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  AchievementString: AnsiString;
+begin
+  //WriteLn('SteamAPI_ISteamUtils_GetAppID = ', SteamAPI_ISteamUtils_GetAppID());
+
+  AchievementString := 'ACH_WIN_ONE_GAME';
+  //SteamAPI_ISteamUserStats_SetAchievement(PAnsiChar(AchievementString));
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  SteamAPI_Shutdown();
+  //Sleep(10000);
+end;
+
+end.
+
