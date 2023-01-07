78e3f369d3301524ea411c78b479e50f96b7e37a
eugeneloza <zhloza@gmail.com>
2023-01-07 14:55:36 +0200

    clean up more

--- unit1.pas
+++ unit1.pas
@@ -9,9 +9,6 @@ uses
   DynLibs;
 
 type
-
-  { TForm1 }
-
   TForm1 = class(TForm)
     Button1: TButton;
     Timer1: TTimer;
@@ -34,8 +31,6 @@ uses
 
 {$R *.lfm}
 
-{ TForm1 }
-
 type
   SteamAPIWarningMessageHook = procedure (nSeverity: Integer; pchDebugText: PAnsiChar); Cdecl;
 
@@ -45,32 +40,12 @@ const
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   InitSteam(AppId);
-
-  //SteamAPI_ISteamClient_SetWarningMessageHook(@WarningHook);
-
-  //if SteamAPI_ISteamUser_BLoggedOn() then
-    //WriteLn('Login successful');
-
-
-
-  //https://partner.steamgames.com/doc/features/achievements
-
-  // ISteamUserStats::RequestCurrentStats
-  // ISteamUserStats::GetAchievement
-
-  {If you want to display the achievements in your game you can use ISteamUserStats::GetAchievementDisplayAttribute to retrieve human-readable properties of the achievement, including its name ("name") and description ("desc"). These properties are localizable on the Steamworks Partner Website, and the returned data varies with the language in which the user is running the game. You can also get an achievement's icon using ISteamUserStats::GetAchievementIcon or the time each achievements was unlocked with ISteamUserStats::GetAchievementAndUnlockTime.}
-
-  //ISteamUserStats::SetAchievement
-  //ISteamUserStats::StoreStats
-  //wait for ISteamUserStats::UserAchievementStored_t
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
   AchievementString: AnsiString;
 begin
-  //WriteLn('SteamAPI_ISteamUtils_GetAppID = ', SteamAPI_ISteamUtils_GetAppID());
-
   AchievementString := 'ACH_WIN_ONE_GAME';
   //SteamAPI_ISteamUserStats_SetAchievement(PAnsiChar(AchievementString));
 end;
