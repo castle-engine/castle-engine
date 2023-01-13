8b0fe9bc6c53f6888ed1e3c6a0bc51d68c24b706
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-13 13:40:25 +0100

    Use build tool (castle-engine simple-compile) to compile library

--- src/deprecated_library/castleengine_compile.sh
+++ src/deprecated_library/castleengine_compile.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -eu
 
-# Call this script from this directory,
-# or from base castle_game_engine directory.
+# ----------------------------------------------------------------------------
+# Compile the library.
+#
+# Executes the build tool, which in turn will execute FPC.
+# The build tool searches for FPC on $PATH.
+#
+# Note that the compilation artifacts will be in ./castle-engine-output/ , and that's good,
+# we want separate compilation artifacts (from other projects) to:
+#
+# 1. make sure that CastleWindow unit is compiled with LIBRARY backend,
+# 2. make sure that every unit is recompiled with -fPIC (necessary for .so on x86_64).
+# ----------------------------------------------------------------------------
 
-# Change to the directory where castle-fpc.cfg is.
-if [ -f castleengine.lpr ]; then cd ../../; fi
+# Never use cache, as it will likely contain CastleWindow with different backend, and units without -fPIC.
+castle-engine cache-clean
 
-# We clean first, to
-# 1. make sure that CastleWindow unit is compiled with LIBRARY backend.
-# 2. make sure that every unit is recompiled with -fPIC (necessary for .so on x86_64)
-make --quiet clean
-
-fpc -fPIC -dRELEASE @castle-fpc.cfg -dCASTLE_WINDOW_LIBRARY src/deprecated_library/castleengine.lpr "$@"
-
-# Clean units to avoid other programs to be accidentally compiled
-# with CastleWindow with LIBRARY backend, or -fPIC.
-# Note: do not just run "make clean", it would remove also
-# the library we just compiled.
-find . -type f '(' -iname '*.o' -or -iname '*.ppu' ')' -exec rm -f '{}' ';'
+castle-engine simple-compile \
+  --compiler-option=-fPIC \
+  --compiler-option=-dCASTLE_WINDOW_LIBRARY \
+  --verbose \
+  castleengine.lpr
