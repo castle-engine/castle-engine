7fdb08339a0cef15a082d9c05e7f86a95456cf91
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-01-22 08:21:13 +0100

    Remove complicated approach to account for flips, that generated few lists and assumed mirroredRepeat mode
    We should just generate one piece of information for each TTile, and later when generating big mesh make flips in the texture coordinates of given quad.
    
    This also means we don't care about mirrored repeat mode of texture.
    
    Whole TilesetShapeNodeListList, TilesetTextureTransformNode, GetResolvedTileShapeNode are now gone.

--- src/scene/load/x3dloadinternaltiledmap.pas
+++ src/scene/load/x3dloadinternaltiledmap.pas
@@ -67,14 +67,11 @@ implementation
 
 uses
   SysUtils, Math, Generics.Collections,
-  CastleVectors, CastleTransform, CastleColors,
+  CastleVectors, CastleTransform, CastleColors, CastleRectangles,
   CastleRenderOptions, CastleControls, CastleStringUtils,
   CastleImages;
 
 type
-  TShapeNodeList = {$ifdef FPC}specialize{$endif} TObjectList<TShapeNode>;
-  TShapeNodeListList = {$ifdef FPC}specialize{$endif} TObjectList<TShapeNodeList>;
-
   { Converter class to convert Tiled map into X3D representations.
 
     TODO:
@@ -85,17 +82,9 @@ type
     FMap: TTiledMap;
     FMapNode: TTransformNode;
     FRootNode: TX3DRootNode;
-    FTilesetShapeNodeListList: TShapeNodeListList;
 
-    { Converts tilesets to shape node lists.
-
-      For every tileset in Map.Tilesets there is one TShapeNodeList created
-      which holds one shape nodes for every tile of the tileset.
-
-      @bold(IMPORTANT:) This procedure must ensure that the number of shape node lists
-      is always the same as the actual tilesets in Map.Tilesets.
-      This is crucial for retrieving the correct tileset shape node list from
-      TilesetShapeNodeListList later! }
+    { Load tiles to nodes.
+      Fills TTile.RendererData with TShapeNode to render this. }
     procedure ConvertTilesets;
     { Tries to construct X3D nodes for each layer. }
     procedure ConvertLayers;
@@ -111,10 +100,6 @@ type
     function ConvY(const TiledCoord: TVector2): TVector2; overload;
     function ConvY(const X, Y: Single): TVector2; overload;
     { @groupEnd }
-
-    { The elements of this list are themselves lists which contain
-      the shape node of a tileset each. }
-    property TilesetShapeNodeListList: TShapeNodeListList read FTilesetShapeNodeListList write FTilesetShapeNodeListList;
   public
     constructor Create(ATiledMap: TTiledMap);
     destructor Destroy; override;
@@ -137,24 +122,23 @@ begin
 end;
 
 procedure TTiledMapConverter.ConvertTilesets;
-type
-  TTileFlip = (tfNoFlip, tfHFlip, tfVFlip, tfDFlip);
 var
   Tile: TTiledMap.TTile;
   Tileset: TTiledMap.TTileset;
   TilesetTextureNode: TImageTextureNode;
   TileGeometryNode: TQuadSetNode;
   TileShapeNode: TShapeNode;
-  TilesetTextureTransformNode: TTextureTransformNode;
-  TilesetShapeNodeList: TShapeNodeList;
 
   { The ghost node holds all shape nodes of all tilesets and is added
     to the map node.
     If the map node is free'd, all tiles of the tilesets are free'd via
     the ghost node, even if they were not used as actual map tiles. }
-  GhostNode: TX3DRootNode;
-  TilesetTexCoordOrigin: TVector2;
-  TileFlip: TTileFlip;
+  GhostNode: TGroupNode;
+
+  TexProperties: TTexturePropertiesNode;
+  Coord: TCoordinateNode;
+  TexCoord: TTextureCoordinateNode;
+  TexRect: TFloatRectangle;
 
   { Calculate the number of rows (of tiles) of a tileset. }
   function RowsInTileset: Cardinal;
@@ -162,38 +146,15 @@ var
     Result := Tileset.TileCount div Tileset.Columns;
   end;
 
-var
-  TexProperties: TTexturePropertiesNode;
-  Coord: TCoordinateNode;
-  TexCoord: TTextureCoordinateNode;
 begin
-  TilesetTexCoordOrigin := TVector2.Zero;
-  GhostNode := TX3DRootNode.Create;
+  GhostNode := TGroupNode.Create;
 
   for Tileset in Map.Tilesets do
   begin
-    { Make sure for each tileset there is a shape node list created
-      for consistency and retrieving of correct item-indices later. }
-    { Using false argument below, to not own (free) nodes. }
-    TilesetShapeNodeList := TShapeNodeList.Create(False); // do not own
-    TilesetTextureNode := nil;
-
-    if Assigned(Tileset.Image) then
+    if Tileset.Image = nil then
     begin
-      { Prepare texture node of tileset. }
-      TilesetTextureNode := TImageTextureNode.Create(Tileset.Name, '');
-      TilesetTextureNode.SetUrl([Tileset.Image.URL]);
-
-      TexProperties := TTexturePropertiesNode.Create;
-      TexProperties.MagnificationFilter := magDefault;
-      TexProperties.MinificationFilter := minDefault;
-      TexProperties.BoundaryModeS := bmMirroredRepeat;
-      TexProperties.BoundaryModeT := bmMirroredRepeat;
-      { Do not force "power of 2" size, which may prevent mipmaps.
-        Not resizing is consistent with X3DLoadInternalImage, LoadCastleSpriteSheet,
-        users may use it with pixel-art and don't expect images to be resized. }
-      TexProperties.GuiTexture := true;
-      TilesetTextureNode.TextureProperties := TexProperties;
+      WriteLnWarning('Tileset has no image');
+      Continue;
     end;
 
     if (Tileset.TileCount = 0) or (Tileset.Columns = 0) then
@@ -202,103 +163,66 @@ begin
       Continue;
     end;
 
-    for TileFlip := Low(TTileFlip) to High(TTileFlip) do
+    { Prepare texture node of tileset. }
+    TilesetTextureNode := TImageTextureNode.Create(Tileset.Name, '');
+    TilesetTextureNode.SetUrl([Tileset.Image.URL]);
+
+    TexProperties := TTexturePropertiesNode.Create;
+    TexProperties.MagnificationFilter := magDefault;
+    TexProperties.MinificationFilter := minDefault;
+    { Do not force "power of 2" size, which may prevent mipmaps.
+      Not resizing is consistent with X3DLoadInternalImage, LoadCastleSpriteSheet,
+      users may use it with pixel-art and don't expect images to be resized. }
+    TexProperties.GuiTexture := true;
+    TilesetTextureNode.TextureProperties := TexProperties;
+
+    for Tile in Tileset.Tiles do
     begin
-      { No flip mode }
-      if TileFlip = tfNoFlip then
-        TilesetTexCoordOrigin := Vector2(0, 0);
+      { Make tiles of tileset rectangular and with correct dimensions. }
+      TileGeometryNode := TQuadSetNode.CreateWithShape(TileShapeNode);
 
-      { Hint: The usual coordnate space covers [0, 1], but because of scaling
-              the actual space may cover different dimensions.
+      Coord := TCoordinateNode.Create;
+      Coord.SetPoint([
+        Vector3(0, 0, 0),
+        Vector3(Tileset.TileWidth, 0, 0),
+        Vector3(Tileset.TileWidth, Tileset.TileHeight, 0),
+        Vector3(0, Tileset.TileHeight, 0)
+      ]);
+      TileGeometryNode.Coord := Coord;
 
-        Example for horizontal flipping:
-             If the scale factor is 0.5, the texture is expanded by
-             factor 2, hence the (unflipped) texture is covered by [0, 2].
-             For the flipped texture we do not look at 1 + ...
-             but consequently at tileset width / tile width = 2. }
+      TexRect := FloatRectangle(
+        (Tile.Id mod Tileset.Columns) * (Tileset.TileWidth + Tileset.Spacing)
+        + Tileset.Margin,
+        (Tile.Id div Tileset.Columns) * (Tileset.TileHeight + Tileset.Spacing)
+        + Tileset.Margin,
+        Tileset.TileWidth,
+        Tileset.TileHeight
+      );
 
-      { Horizontal flip }
-      if TileFlip = tfHFlip then
-        TilesetTexCoordOrigin := Vector2(Tileset.Image.Width / Tileset.TileWidth, 0);
-      { Vertical flip }
-      if TileFlip = tfVFlip then
-        TilesetTexCoordOrigin := Vector2(0, Tileset.Image.Height / Tileset.Tileheight);
-      { Diagonal flip }
-      if TileFlip = tfDFlip then
-        TilesetTexCoordOrigin := Vector2(Tileset.Image.Width / Tileset.TileWidth,
-          Tileset.Image.Height / Tileset.Tileheight);
+      { fix TexRect to be in 0..1 range }
+      TexRect.Left := TexRect.Left / Tileset.Image.Width;
+      TexRect.Width := TexRect.Width / Tileset.Image.Width;
+      TexRect.Bottom := TexRect.Bottom / Tileset.Image.Height;
+      TexRect.Height := TexRect.Height / Tileset.Image.Height;
 
-      for Tile in Tileset.Tiles do
-      begin
-        { Make tiles of tileset rectangular and with correct dimensions. }
-        TileGeometryNode := TQuadSetNode.CreateWithShape(TileShapeNode);
+      // account that Tiled logic for texture coords assumes Y goes down
+      TexRect.Bottom := 1 - TexRect.Bottom - TexRect.Height;
 
-        Coord := TCoordinateNode.Create;
-        Coord.SetPoint([
-          Vector3(0, 0, 0),
-          Vector3(Tileset.TileWidth, 0, 0),
-          Vector3(Tileset.TileWidth, Tileset.TileHeight, 0),
-          Vector3(0, Tileset.TileHeight, 0)
-        ]);
-        TileGeometryNode.Coord := Coord;
+      TexCoord := TTextureCoordinateNode.Create;
+      TexCoord.SetPoint([
+        Vector2(TexRect.Left , TexRect.Bottom),
+        Vector2(TexRect.Right, TexRect.Bottom),
+        Vector2(TexRect.Right, TexRect.Top),
+        Vector2(TexRect.Left , TexRect.Top)
+      ]);
+      TileGeometryNode.TexCoord := TexCoord;
 
-        TexCoord := TTextureCoordinateNode.Create;
-        TexCoord.SetPoint([
-          Vector2(0, 0),
-          Vector2(1, 0),
-          Vector2(1, 1),
-          Vector2(0, 1)
-        ]);
-        TileGeometryNode.TexCoord := TexCoord;
+      TileShapeNode.Appearance := TAppearanceNode.Create;
+      TileShapeNode.Appearance.Texture := TilesetTextureNode;
 
-        { Make tiles textured and find correct texture coordinates. }
-        TileShapeNode.Appearance := TAppearanceNode.Create;
-        if Assigned(TilesetTextureNode) then
-        begin
-          TileShapeNode.Appearance.Texture := TilesetTextureNode;
-          TilesetTextureTransformNode := TTextureTransformNode.Create;
-
-          { Scale tileset texture to fit tile size:
-            Scale factor is inverted: E. g. 0.5 means 2x dimension.
-            Divide Tileset Tile width/height by full Tileset width/height.
-            The latter is extracted from the texture node. }
-          TilesetTextureTransformNode.Scale := Vector2(
-            Tileset.TileWidth / Tileset.Image.Width,
-            Tileset.TileHeight / Tileset.Image.Height
-          );
-
-          { Get all tile textures from tileset texture as shape nodes:
-          Important: Origin (0/0) of tex. coordinate is bottom-left! }
-          TilesetTextureTransformNode.Translation :=
-            { Origin: Base coordinate depending on tile flip mode }
-            TilesetTexCoordOrigin
-            + Vector2(
-            { X: Calc. Column (e. g. 0, 1, 2, ...) = No. of full tile widths
-                   in tex. coord. space (0,0 to 1,0)
-                 + Spacing: Col. * No. of spacings in tex. coord. space
-                 + Margin in Tex. coord. space }
-            (Tile.Id mod Tileset.Columns)
-            + (Tile.Id mod Tileset.Columns) * (Tileset.Spacing / Tileset.TileWidth)
-            + (Tileset.Margin / Tileset.TileWidth),
-            { Y: Calc. Row (e. g. 3, 2, 1, 0 if 4 rows exist)
-                 + Spacing: Row * Spacing in tex. coord. space
-                 + Margin in Tex. coord. space }
-            ((RowsInTileset - 1) - Floor(Tile.Id / Tileset.Columns))
-            + ((RowsInTileset - 1) - Floor(Tile.Id / Tileset.Columns)) * (Tileset.Spacing / Tileset.TileWidth)
-            + (Tileset.Margin / Tileset.TileHeight)
-          );
-
-          TileShapeNode.Appearance.TextureTransform := TilesetTextureTransformNode;
-        end;
-
-        TilesetShapeNodeList.Add(TileShapeNode);
-        GhostNode.AddChildren(TileShapeNode);
-      end;
+      Tile.RendererData := TileShapeNode;
+      GhostNode.AddChildren(TileShapeNode);
     end;
-
-    { Add list of shape nodes of this tileset to
-      list of tileset shape node lists. }
-    TilesetShapeNodeListList.Add(TilesetShapeNodeList);
 
     { Add ghost nodes to switch node. }
     (RootNode.FdChildren.Items[0] as TSwitchNode).AddChildren(GhostNode);
@@ -439,150 +363,6 @@ end;
 procedure TTiledMapConverter.BuildTileLayerNode(const LayerNode: TTransformNode;
   const ALayer: TTiledMap.TLayer);
 
-  { Get shape node textured correctly considering
-    all the flipping bits (horizontal, vertical, diagonal bit).
-
-    Tiled manual about flipping:
-    ( https://doc.mapeditor.org/en/stable/reference/tmx-map-format/#layer ) says:
-    "When rendering a tile, the order of operation matters.
-    The diagonal flip (x/y axis swap) is done first, followed
-    by the horizontal and vertical flips." }
-  function GetResolvedTileShapeNode(const ATileset: TTiledMap.TTileset;
-    const ATile: TTiledMap.TTile;
-    const HorizontalFlip, VerticalFlip, DiagonalFlip: Boolean): TShapeNode;
-  var
-    ATilesetShapeNodeList: TShapeNodeList;
-  begin
-    Result := nil;
-
-    { Get tileset's shape node list. }
-    ATilesetShapeNodeList := TilesetShapeNodeListList.Items[
-      Map.Tilesets.IndexOf(ATileset)];
-    if not Assigned(ATilesetShapeNodeList) then
-      Exit;
-
-    { Get shape node from tileset list if no flip flags are set. }
-    if not (HorizontalFlip or VerticalFlip or DiagonalFlip) then
-    begin
-      Result := ATilesetShapeNodeList.Items[ATileset.Tiles.IndexOf(ATile)];
-      Exit;
-    end;
-
-    { Get shape node from tileset list if exclusivly the horizontal flip flag is set. }
-    if HorizontalFlip and not (VerticalFlip or DiagonalFlip) then
-    begin
-      { Calc. of correct horizontally flipped tile:
-
-        Tile indices:
-        orig.       flipped
-        Tex.        Tex.
-        0 1 2       2 1 0
-        3 4 5  -->  5 4 3
-        6 7 8       8 7 6
-
-        In the tileset shape node list, the flipped texture shape nodes follow
-        directly after the original texture shape nodes.
-        List indices: (0 1 2 3 4 5 6 7 8) (2 1 0 5 4 3 8 7 6) ...
-
-        1. Shift index to start of indices of hor. flipped textures
-        2. Shift to row (in which index lies)
-        3. Add a row to be on the right side (correct by -1 bc. the last col.
-           on the right side is adressed by column count - 1)
-        4. Substract index within the correct row to correct index
-
-        Ex. (see diagram above):
-        Index of texture 3.
-        1. TileCount (here 9) => Index 2 (flipped Tex.)
-        2. Floor(3 / 3) = 1; with 3 Cols. => Index 5
-        3. Add a row - 1 => Index 3
-        4. Subract 3 mod 3 = 0 => Index 3
-      }
-
-      Result := ATilesetShapeNodeList.Items[
-                  ATileset.TileCount
-                  + Floor(ATileset.Tiles.IndexOf(ATile) / ATileset.Columns) * ATileset.Columns
-                  + ATileset.Columns - 1
-                  - ATileset.Tiles.IndexOf(ATile) mod ATileset.Columns
-                ];
-      Exit;
-    end;
-
-    { Get shape node from tileset list if exclusivly the vertical flip flag is set. }
-    if VerticalFlip and not (HorizontalFlip or DiagonalFlip) then
-    begin
-      { Calc. of correct vertically flipped tile:
-
-        Tile indices:
-        orig.       flipped
-        Tex.        Tex.
-        0 1 2       6 7 8
-        3 4 5  -->  3 4 5
-        6 7 8       0 1 2
-
-        In the tileset shape node list, the flipped texture shape nodes follow
-        directly after the original texture shape nodes.
-        List indices: (0 1 2 3 4 5 6 7 8) ... (horizontally flipped) ... (6 7 8...
-
-        1. Shift index to start of indices of vert. flipped textures
-        2. Shift into last row
-        3. Substract "row index" of original texture
-        4. Shift to correct texture in row
-      }
-      Result := ATilesetShapeNodeList.Items[
-                  ATileset.TileCount * 2
-                  + ((ATileset.TileCount div ATileset.Columns) - 1) * ATileset.Columns
-                  - Floor(ATileset.Tiles.IndexOf(ATile) / ATileset.Columns) * ATileset.Columns
-                  + ATileset.Tiles.IndexOf(ATile) mod ATileset.Columns
-                ];
-      Exit;
-    end;
-
-    { Ignore all flip flags if DiagonalFlip and additionally
-      HorizontalFlip/VerticalFlip is set (results in rotation).
-      TODO: Implement rotation. }
-    if DiagonalFlip and (HorizontalFlip or VerticalFlip) then
-    begin
-      WritelnWarning('Not supported yet: Combination of diagonal- with other flips and rotations. Flags are ignored.');
-      Result := ATilesetShapeNodeList.Items[ATileset.Tiles.IndexOf(ATile)];
-      Exit;
-    end;
-
-    { Get shape node from tileset list if exclusivly the diagonal flip flag is set
-      or if horizontal and vertical flip flags (= diagonal flip) are set. }
-    if (DiagonalFlip and not (HorizontalFlip or VerticalFlip)) xor
-       ((HorizontalFlip and VerticalFlip) and not DiagonalFlip) then
-    begin
-      { Calc. of correct diagonally flipped tile:
-
-        Tile indices:
-        orig.       flipped
-        Tex.        Tex.
-        0 1 2       8 7 6
-        3 4 5  -->  5 4 3
-        6 7 8       2 1 0
-
-        In the tileset shape node list, the flipped texture shape nodes follow
-        after the horizontally and vertically flipped texture shape nodes.
-        List indices:
-        (0 1 2 3 4 5 6 7 8) (horizon. flipped) (vert. flipped) (8 7 6 5 4 3 2 1 0)
-
-        1. Shift index to start of indices of diag. flipped textures
-        2. Shift into last row
-        3. Substract "row index" of original texture
-        4. Shift to last col.
-        5. Shift back by index
-      }
-      Result := ATilesetShapeNodeList.Items[
-                  ATileset.TileCount * 3
-                  + ((ATileset.TileCount div ATileset.Columns) - 1) * ATileset.Columns
-                  - Floor(ATileset.Tiles.IndexOf(ATile) / ATileset.Columns) * ATileset.Columns
-                  + ATileset.Columns - 1
-                  - ATileset.Tiles.IndexOf(ATile) mod ATileset.Columns
-                ];
-      Exit;
-    end;
-  end;
-
   procedure RenderTile(const X, Y: Integer);
   var
     Tileset: TTiledMap.TTileset;
@@ -597,8 +377,9 @@ procedure TTiledMapConverter.BuildTileLayerNode(const LayerNode: TTransformNode;
       TileNode := TTransformNode.Create;
       TileNode.Translation := Vector3(Map.TileRenderPosition(Vector2Integer(X, Y)), 0);
 
-      TileShapeNode := GetResolvedTileShapeNode(Tileset, Tileset.Tiles[Frame],
-        HorizontalFlip, VerticalFlip, DiagonalFlip);
+      TileShapeNode := Tileset.Tiles[Frame].RendererData as TShapeNode;
+
+      // TODO: apply HorizontalFlip, VerticalFlip, DiagonalFlip for this instance
 
       TileNode.AddChildren(TileShapeNode);
       LayerNode.AddChildren(TileNode);
@@ -628,14 +409,10 @@ begin
   MapNode := TTransformNode.Create;
   SwitchNode.AddChildren(MapNode);
   SwitchNode.WhichChoice := 0;
-
-  TilesetShapeNodeListList := TShapeNodeListList.Create(True);
 end;
 
 destructor TTiledMapConverter.Destroy;
 begin
-  FreeAndNil(FTilesetShapeNodeListList);
-
   inherited Destroy;
 end;
 
