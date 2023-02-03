5b3f2e7fce2bbb7bf22965f3e1b6319d503e036a
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-02-03 05:13:09 +0100

    Debug Jenkins on Windows failure

--- tools/internal/pack_release/pack_release.sh
+++ tools/internal/pack_release/pack_release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
 # ----------------------------------------------------------------------------
 # Pack Castle Game Engine release (source + binaries).
