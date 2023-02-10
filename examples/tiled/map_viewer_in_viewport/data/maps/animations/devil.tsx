92df638db3358a963eed2fbf04b91610c83bdeb8
Freedomax <576916445@qq.com>
2023-02-10 08:58:12 +0800

    Add two animated demo maps.

---
+++ examples/tiled/map_viewer_in_viewport/data/maps/animations/devil.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="devil-animation" tilewidth="50" tileheight="50" tilecount="4" columns="4">
+ <image source="devil-animation.png" width="200" height="50"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
