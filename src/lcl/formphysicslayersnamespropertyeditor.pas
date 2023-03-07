6d24d4d3d6b9cd714759fd85b77c5a8c3dbd0a78
and3md <and3md@gmail.com>
2023-03-07 15:34:10 +0100

    Working layers naming with description + updated platformer.

--- src/lcl/formphysicslayersnamespropertyeditor.pas
+++ src/lcl/formphysicslayersnamespropertyeditor.pas
@@ -6,14 +6,18 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, ComCtrls,
-  StdCtrls, CastleTransform;
+  StdCtrls, ValEdit, CastleTransform;
 
 type
   TPhysicsLayersNamesPropertyEditorForm = class(TForm)
     ButtonOK: TButton;
-    NamesAndDescListView: TListView;
+    NamesAndDescStringGrid: TStringGrid;
+    procedure ButtonOKClick(Sender: TObject);
   strict private
      FLayersNames: TCastleLayersNames;
+
+    procedure Load;
+    procedure Save;
   public
     procedure Init(const LayersNames: TCastleLayersNames);
   end;
@@ -22,12 +26,44 @@ implementation
 
 {$R *.lfm}
 
-{ TPhysicsLayersNamesPropertyEditorForm }
+{ TPhysicsLayersNamesPropertyEditorForm -------------------------------------- }
+
+procedure TPhysicsLayersNamesPropertyEditorForm.ButtonOKClick(Sender: TObject);
+begin
+  Save;
+end;
+
+procedure TPhysicsLayersNamesPropertyEditorForm.Load;
+var
+  I: Integer;
+begin
+  NamesAndDescStringGrid.RowCount := High(TPhysicsLayer) + 2;
+
+  for I := Low(TPhysicsLayer) + 1 to High(TPhysicsLayer) + 1 do
+  begin
+    NamesAndDescStringGrid.Cols[0][I] := IntToStr(I - 1);
+    NamesAndDescStringGrid.Cols[1][I] := FLayersNames.Names[TPhysicsLayer(I-1)];
+    NamesAndDescStringGrid.Cols[2][I] := FLayersNames.Descriptions[TPhysicsLayer(I-1)];
+  end;
+end;
+
+procedure TPhysicsLayersNamesPropertyEditorForm.Save;
+var
+  I: Integer;
+begin
+  for I := Low(TPhysicsLayer) + 1 to High(TPhysicsLayer) + 1 do
+  begin
+    FLayersNames.Names[TPhysicsLayer(I-1)] := NamesAndDescStringGrid.Cols[1][I];
+    FLayersNames.Descriptions[TPhysicsLayer(I-1)] := NamesAndDescStringGrid.Cols[2][I];
+  end;
+end;
 
 procedure TPhysicsLayersNamesPropertyEditorForm.Init(
   const LayersNames: TCastleLayersNames);
 begin
   FLayersNames := LayersNames;
+
+  Load;
 end;
 
 end.
