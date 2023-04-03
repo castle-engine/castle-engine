f4cee95b35f9c9c9ed004e16b78d2f4685e69f46
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-04-03 17:43:32 +0200

    Add missing copyrights

--- tools/castle-editor/components/formlayercollisionspropertyeditor.pas
+++ tools/castle-editor/components/formlayercollisionspropertyeditor.pas
@@ -1,3 +1,19 @@
+{
+  Copyright 2023-2023 Michalis Kamburelis, Andrzej Kilijanski
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
+{ Edit PhysicsProperties.LayerCollisions. }
 unit FormLayerCollisionsPropertyEditor;
 
 {$mode ObjFPC}{$H+}
