b9d1db2b861fff4f1f10e382378e41a56ae21ff7
and3md <and3md@gmail.com>
2023-06-13 12:18:57 +0200

    Better crouch approach with edge cases fixed

--- examples/physics/physics_3d_shooter/code/fpscrouch.pas
+++ examples/physics/physics_3d_shooter/code/fpscrouch.pas
@@ -8,21 +8,33 @@ uses
 
 type
 
-  { Crouch support by scale collider and change player translation }
+  { Crouch support by change collider height and player translation.
+    It supports only capsule collider because of edge cases that are
+    difficult to meet for all types of colliders.
+
+    This functionality assumes that the size of the collider is fixed and is
+    not changed by any other mechanism.  }
   TFpsCrouch = class(TAbstractMovementModifier)
   strict private
     FInput_Crouch: TInputShortcut;
     FCrouchSpeed: Single;
-    FIsCrouching: Boolean;
-    FWasCrouchingInput: Boolean;
+    FIsCrouching: Boolean; { Are player crouching? }
+    FWasCrouchingInput: Boolean; { Was input pressed in previous frame }
+    FOriginalCapsuleHeight: Single; { Capsule height property when not crouching }
+    FScaledOriginalCapusleHeight: Single; { Needed for rigid body translation }
+    FScaledCrouchingCapusleHeight: Single; { Needed for rigid body translation }
+
+    FWasColliderTypeWarning: Boolean; { Show collider type warning only once }
   protected
     procedure StartCrouching(const MovementState: TModularMovementState); virtual;
     procedure StopCrouching(const MovementState: TModularMovementState); virtual;
 
-    function CanPlayerStandUp(const MovementState: TModularMovementState): Boolean;
+    function CanPlayerStandUp(const RBody: TCastleRigidBody;
+      const Collider: TCastleCapsuleCollider): Boolean;
   public
     const
       DefaultCrouchSpeed = 3.0;
+      MinCapsuleHeight = 0.02;
 
     constructor Create(AOwner: TComponent); override;
 
@@ -48,8 +60,12 @@ begin
   inherited Create(AOwner);
   FCrouchSpeed := DefaultCrouchSpeed;
 
-  FInput_Crouch              := TInputShortcut.Create(Self);
-  Input_Crouch              .Assign(keyC);
+  FInput_Crouch := TInputShortcut.Create(Self);
+  Input_Crouch.SetSubComponent(true);
+  Input_Crouch.Assign(keyC);
+  Input_Crouch.Name := 'InputCrouch';
+
+  FWasColliderTypeWarning := false;
 end;
 
 procedure TFpsCrouch.UpdateMovement(const MovementState: TModularMovementState);
@@ -57,8 +73,26 @@ var
   Velocity: TVector3;
   HorizontalSpeed: TVector3;
   VerticalSpeed: Single;
+
+  function CheckColliderType(const Collider: TCastleCollider): Boolean;
+  begin
+    if not (Collider is TCastleCapsuleCollider) then
+    begin
+      if not FWasColliderTypeWarning then
+      begin
+        WritelnWarning('TFpsCrouch supports only player with capsule collider.');
+        FWasColliderTypeWarning := true;
+      end;
+      Exit(false);
+    end;
+    Result := true;
+  end;
+
 begin
   if FocusedContainer = nil then
+    Exit;
+
+  if not CheckColliderType(MovementState.Collider) then
     Exit;
 
   if MovementState.IsJumping or MovementState.IsPlayerOnGround = false then
@@ -100,67 +134,61 @@ begin
 end;
 
 procedure TFpsCrouch.StartCrouching(const MovementState: TModularMovementState);
+var
+  CapsuleCollider: TCastleCapsuleCollider;
+  NewHeight: Single;
 begin
-  MovementState.Collider.SizeScale := 0.5;
-  Parent.Translation := Parent.Translation + Vector3(0, -(MovementState.Collider.ScaledLocalBoundingBox.SizeY / 2 * 0.90), 0); // place player on ground, 0.99 to ensure player will be above ground
+  CapsuleCollider := MovementState.Collider as TCastleCapsuleCollider;
+
+  FScaledOriginalCapusleHeight := CapsuleCollider.CalculateScaledHeight;
+  FOriginalCapsuleHeight := CapsuleCollider.Height;
+  { We try to change player height to 50% }
+  NewHeight := FOriginalCapsuleHeight / 2 - CapsuleCollider.Radius;
+  { When height / 2 is smaller than radius use minimal height value }
+  if NewHeight < MinCapsuleHeight then
+    NewHeight := MinCapsuleHeight;
+
+  CapsuleCollider.Height := NewHeight;
+  FScaledCrouchingCapusleHeight := CapsuleCollider.CalculateScaledHeight;
+
+  { Stand player on ground - 0.90 to ensure player will be above ground }
+  Parent.Translation := Parent.Translation + Vector3(0,
+    -((FScaledOriginalCapusleHeight / 2 - FScaledCrouchingCapusleHeight / 2) * 0.90 ), 0);
   FIsCrouching := true;
 end;
 
 procedure TFpsCrouch.StopCrouching(const MovementState: TModularMovementState);
+var
+  CapsuleCollider: TCastleCapsuleCollider;
 begin
-  if FIsCrouching and CanPlayerStandUp(MovementState) then
+  if FIsCrouching and CanPlayerStandUp(MovementState.RigidBody,
+    MovementState.Collider as TCastleCapsuleCollider) then
   begin
-    Parent.Translation := Parent.Translation + Vector3(0, MovementState.Collider.ScaledLocalBoundingBox.SizeY * 1.01, 0); // place player on ground before scale change 1.01 to ensure player will be above ground
-    MovementState.Collider.SizeScale := 1;
+    CapsuleCollider := MovementState.Collider as TCastleCapsuleCollider;
+
+    { Set the player higher in the air before resizing the collider. }
+    Parent.Translation := Parent.Translation + Vector3(0, (FScaledOriginalCapusleHeight / 2 - FScaledCrouchingCapusleHeight / 2) * 1.10, 0);
+    CapsuleCollider.Height := FOriginalCapsuleHeight;
     FIsCrouching := false;
   end;
 end;
 
-function TFpsCrouch.CanPlayerStandUp(const MovementState: TModularMovementState
-  ): Boolean;
+function TFpsCrouch.CanPlayerStandUp(const RBody: TCastleRigidBody;
+  const Collider: TCastleCapsuleCollider): Boolean;
 var
-  Collider: TCastleCollider;
-  RBody: TCastleRigidBody;
-  ColliderBoundigBox: TBox3D;
-  ColliderRadius: Single;
-  ColliderHeight: Single;
-
   CastDirection: TVector3;
   CastOrigin: TVector3;
-  CastOriginUpAdjustment: Single;
-
   CastResult: TPhysicsRayCastResult;
 begin
-  Collider := MovementState.Collider;
-  RBody := MovementState.RigidBody;
-
   CastDirection := Vector3(0, 1, 0);
   CastOrigin := Collider.Middle;
 
-  ColliderBoundigBox := Collider.ScaledLocalBoundingBox;
-  ColliderRadius := Iff(ColliderBoundigBox.SizeX > ColliderBoundigBox.SizeZ,
-    ColliderBoundigBox.SizeX, ColliderBoundigBox.SizeZ);
-  WritelnLog('ColliderRadius: ' + FloatToStr(ColliderRadius));
-  WritelnLog('ColliderBoundigBox: ' + ColliderBoundigBox.ToString);
-  ColliderHeight := ColliderBoundigBox.SizeY;
-  WritelnLog('ColliderHeight: ' + FloatToStr(ColliderHeight));
-
-  { Adjust sphere cast origin when radius is equal or bigger than ColliderHeight / 2 }
-  if ColliderRadius - ColliderHeight / 2 > -0.1  then
-  begin
-    CastOriginUpAdjustment := ColliderRadius - ColliderHeight / 2 + 0.1;
-    CastOriginUpAdjustment := 0;
-    CastOrigin.Y := CastOrigin.Y + CastOriginUpAdjustment;
-  end;
-
-  WritelnLog('CastOriginUpAdjustment: ' + FloatToStr(CastOriginUpAdjustment));
-  WritelnLog('MaxDistance: ' + FloatToStr(ColliderHeight / 2 + ColliderHeight - CastOriginUpAdjustment));
-
-  CastResult := RBody.PhysicsSphereCast(CastOrigin, ColliderRadius, CastDirection,
-  ColliderHeight / 2 + ColliderHeight - CastOriginUpAdjustment);
+  CastResult := RBody.PhysicsSphereCast(CastOrigin, Collider.CalculateScaledRadius, CastDirection,
+  Collider.CalculateScaledHeight / 2 + (FScaledOriginalCapusleHeight / 2 - FScaledCrouchingCapusleHeight / 2 ) * 1.10);
 
   Result := not CastResult.Hit;
-  WritelnLog(Iff(Result, 'can be moved up', 'no space to move up'));
+  WritelnLog(Iff(Result, 'Stop crouching: can be moved up',
+    'Stop crouching: no space to move up'));
 end;
 
 initialization
