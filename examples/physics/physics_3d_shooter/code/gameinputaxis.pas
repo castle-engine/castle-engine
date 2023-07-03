8e2f0af381a056d5a3106a20eb385e01c15a9261
and3md <and3md@gmail.com>
2023-07-03 14:00:08 +0200

    Docs and cleanup TCastleInputAxis.

--- examples/physics/physics_3d_shooter/code/gameinputaxis.pas
+++ examples/physics/physics_3d_shooter/code/gameinputaxis.pas
@@ -11,6 +11,20 @@ type
     mlaHorizontal,
     mlaVertical);
 
+  { Input for axis, good for specifying values on some axis.
+    In case of keys we have values:
+    - 0 - no key pressed
+    - -1.0 negative key is pressed
+    - 1.0 positive key is pressed
+
+    In case of mouse look:
+    - 0 - no mouse movement
+    - other value - delta from mouse look
+
+    Valuse can be modified by MouseLookMultiplier or Multiplier.
+
+    Setting MouseLook to true do not turn mouse look on it means that
+    CastleInputAxis will read Container.MouseLookLastDelta value }
   TCastleInputAxis = class (TCastleComponent)
   strict private
     FPositiveKey: TKey;
@@ -19,7 +33,6 @@ type
     FMouseLookAxis: TMouseLookAxis;
     FMouseLookMultiplier: Single;
     FMultiplier: Single;
-
   public
     const
       DefaultMultiplier = 1.0;
@@ -27,23 +40,33 @@ type
 
     constructor Create(AOwner: TComponent); override;
 
-    { Mouse look check mouse position between calls }
+    function PropertySections(const PropertyName: String): TPropertySections; override;
+
+    { Return value keys has priority currently (maybe should be changed?) }
     function Value(const Container: TCastleContainer): Single;
   published
+    { Key for positive (1.0) value }
     property PositiveKey: TKey read FPositiveKey write FPositiveKey;
+    { Key for negative (-1.0) value }
     property NegativeKey: TKey read FNegativeKey write FNegativeKey;
+    {Setting MouseLook to true do not turn mouse look on it means that
+    CastleInputAxis will read Container.MouseLookLastDelta value.
+    Use TCastleContainer.StartMouseLook or TCastleContainer.StopMouseLook. }
     property MouseLook: Boolean read FMouseLook write FMouseLook;
+    { Axis for mouse look horizontal or vertical }
     property MouseLookAxis: TMouseLookAxis read FMouseLookAxis
       write FMouseLookAxis;
+    { Good place for change values returned by mouse look - change mouse sensitivity }
     property MouseLookMultiplier: Single read FMouseLookMultiplier
       write FMouseLookMultiplier;
+    { General value multiplier }
     property Multiplier: Single read FMultiplier write FMultiplier;
   end;
 
 
 implementation
 
-uses Math, CastleLog;
+uses Math, CastleUtils;
 
 { TCastleInputAxis  ---------------------------------------------------------- }
 
@@ -53,6 +76,18 @@ begin
 
   FMouseLookMultiplier := DefaultMouseLookMultiplier;
   FMultiplier := DefaultMultiplier;
+end;
+
+function TCastleInputAxis.PropertySections(const PropertyName: String
+  ): TPropertySections;
+begin
+  if ArrayContainsString(PropertyName, [
+     'PositiveKey', 'NegativeKey', 'MouseLook', 'MouseLookAxis',
+     'MouseLookMultiplier', 'Multiplier'
+     ]) then
+    Result := [psBasic]
+  else
+    Result := inherited PropertySections(PropertyName);
 end;
 
 function TCastleInputAxis.Value(const Container: TCastleContainer): Single;
