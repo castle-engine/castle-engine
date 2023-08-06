1bd9cd0ed381a013935f07dfb14ac37bf8920148
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-08-06 16:29:15 +0200

    Remove google_analytics on mobile (reasons on https://castle-engine.io/android_services - it is deprecated and actually crashes on new devices)

--- tools/build-tool/data/ios/services/google_analytics/cge_project_name/AppDelegate.m
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<app_delegate_patch>
-<!--
-  This describes a modification to the AppDelegate.m in the Xcode project
-  to insert GoogleAnalyticsService.
--->
-<import>#import "google_analytics/GoogleAnalyticsService.h"</import>
-<class>GoogleAnalyticsService</class>
-</app_delegate_patch>
