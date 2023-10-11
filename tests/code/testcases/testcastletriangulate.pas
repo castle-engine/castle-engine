7087b4fd00a8ee08bb785b556003c2dc3103c14a
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-10-11 05:41:47 +0200

    Fix TestIndexedConcavePolygonNormal with Delphi/Linux (LongInt->Int32)

--- tests/code/testcases/testcastletriangulate.pas
+++ tests/code/testcases/testcastletriangulate.pas
@@ -48,7 +48,7 @@ const
     (X: 1; Y: -1; Z: 0),
     (X: 0; Y:  0; Z: 0)
   );
-  Indexes: array [0..3] of LongInt = (0, 1, 2, 3);
+  Indexes: array [0..3] of Int32 = (0, 1, 2, 3);
 begin
   AssertVectorEquals(
     IndexedPolygonNormal(@Indexes, High(Indexes) + 1,
