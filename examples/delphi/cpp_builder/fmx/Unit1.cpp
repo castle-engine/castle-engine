0beb9509cdba885d8ecce3a4e9bdf12edd03fc99
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-11-13 02:46:23 +0100

    Working example using FireMonkey and C++ Builder

---
+++ examples/delphi/cpp_builder/fmx/Unit1.cpp
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "Fmx.CastleControl"
+#pragma resource "*.fmx"
+TForm1 *Form1;
+
+// TUiTest --------------------------------------------------------------
+
+/*
+  Define simple TCastleUserInterface descendant, just to capture
+  the moment OpenGL(ES) context is created and fill Memo1 will information.
+*/
+class TUiTest : public TCastleUserInterface
+{
+public:
+	__fastcall TUiTest(TComponent* Owner) : TCastleUserInterface(Owner)
+	{
+	}
+
+	virtual void __fastcall GLContextOpen()
+	{
+		Form1->Memo1->Lines->Add(GLInformationString());
+	}
+};
+
+//---------------------------------------------------------------------------
+
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+	CastleControl1->Container->DesignUrl = "castle-data:/test_3d.castle-user-interface";
+	CastleControl1->Container->Controls()->InsertFront(new TUiTest(this));
+}
+
+//---------------------------------------------------------------------------
