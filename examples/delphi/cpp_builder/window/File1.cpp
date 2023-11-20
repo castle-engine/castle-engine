2c4c9ea158a65665f15cb93ac773c67b62d4186c
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-11-21 00:41:49 +0100

    C++ Builder demo using CastleWindow - links ok

--- examples/delphi/cpp_builder/window/File1.cpp
+++ examples/delphi/cpp_builder/window/File1.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <tchar.h>
 
+#pragma link "CastleWindow"
+
+#include "CastleWindow.hpp"
+#include "CastleControls.hpp"
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	TCastleWindow* window = new TCastleWindow(Application());
 
+	TCastleButton* button = new TCastleButton(Application());
+	button->Caption = "My Button";
+	window->Container->Controls()->InsertFront(button);
+
+	window->Open();
+	Application()->Run();
 }
