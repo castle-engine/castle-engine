b61f7b0350b61086bccbe93c79fa619cff1054db
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-11-22 23:35:57 +0100

    Implement sounds and more in C++ Builder TCastleWindow example

--- examples/delphi/cpp_builder/window/code/GameViewMain.cpp
+++ examples/delphi/cpp_builder/window/code/GameViewMain.cpp
@@ -1,5 +1,4 @@
 //---------------------------------------------------------------------------
-
 #pragma hdrstop
 
 #include "GameViewMain.h"
@@ -9,41 +8,103 @@
 TViewMain *ViewMain;
 //---------------------------------------------------------------------------
 
-
 __fastcall TViewMain::TViewMain(TComponent* Owner) : TCastleView(Owner)
 {
     DesignUrl = "castle-data:/gameviewmain.castle-user-interface";
 }
 
-// TODO: implement rest
-
-__fastcall void TViewMain::ClickButton1(TObject* Sender)
+__fastcall void TViewMain::ClickMakeSound(TObject* Sender)
 {
+    WritelnLog("Button with sound clicked!");
+    SoundEngine()->Play(SoundZap);
 }
 
 __fastcall void TViewMain::ClickMove(TObject* Sender)
 {
+    // TODO: Scene1->Translation = Scene1->Translation + Vector3(0, 1, 0);
 }
 
 __fastcall void TViewMain::ClickBunny(TObject* Sender)
 {
+    // TODO: Scene1->Url = "castle-data:/Bunny.gltf";
 }
 
 __fastcall void TViewMain::Start()
 {
+    TCastleView::Start();
+    ButtonMakeSound->OnClick = ClickMakeSound;
+    ButtonMove->OnClick = ClickMove;
+    ButtonLoadBunny->OnClick = ClickBunny;
 }
 
 __fastcall void TViewMain::Update(const float SecondsPassed, bool &HandleInput)
 {
+    TCastleView::Update(SecondsPassed, HandleInput);
+    // This virtual method is executed every frame (many times per second).
+
+    assert(LabelFps != NULL); // "If you remove LabelFps from the design, remember to remove also the assignment \"LabelFps.Caption := ...\" from code");
+    LabelFps->Caption = "FPS: " + Container()->Fps->ToString();
 }
 
 __fastcall bool TViewMain::Press(const TInputPressRelease Event)
 {
-	bool result = TCastleView::Press(Event);
-	if (result) {
-		return result;
-	}
+    bool result = TCastleView::Press(Event);
+    if (result) {
+        return result;
+    }
 
+// TODO
+/*
+var
+  Body: TCastleRigidBody;
+  NewTransform: TCastleTransform;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  { This virtual method is executed when user presses
+    a key, a mouse button, or touches a touch-screen.
+
+    Note that each UI control has also events like OnPress and OnClick.
+    These events can be used to handle the "press", if it should do something
+    specific when used in that UI control.
+    The TViewMain.Press method should be used to handle keys
+    not handled in children controls.
+  }
+
+  // Push up model on key press
+  if Event.IsKey(keyX) then
+  begin
+    if Viewport1.TransformUnderMouse <> nil then
+    begin
+      Body := Viewport1.TransformUnderMouse.FindBehavior(TCastleRigidBody) as TCastleRigidBody;
+      if Body <> nil then
+        Body.ApplyImpulse(
+          Vector3(0, 10, 0),
+          Viewport1.TransformUnderMouse.Translation
+        );
+    end;
+
+    Exit(true); // key was handled
+  end;
+
+  // instantiate model on key press, push it along sight
+  if Event.IsKey(keyZ) then
+  begin
+   NewTransform := TransformLoad('castle-data:/my_box.castle-transform', FreeAtStop);
+   NewTransform.Translation := Viewport1.Camera.Translation +
+     Viewport1.Camera.Direction * 3;
+   NewTransform.Direction := Viewport1.Camera.Direction;
+   // could be done cleaner by loading NewTransform in new owner and looking for name
+   Body := NewTransform.Items[0].FindBehavior(TCastleRigidBody) as TCastleRigidBody;
+   Viewport1.Items.Add(NewTransform);
+   Body.ApplyImpulse(
+     Viewport1.Camera.Direction * 0.5,
+     Viewport1.Camera.Translation);
+  end;
+end;
+
+*/
     return result;
 }
 
--- examples/delphi/cpp_builder/window/code/GameViewMain.h
+++ examples/delphi/cpp_builder/window/code/GameViewMain.h
@@ -5,29 +5,30 @@
 //---------------------------------------------------------------------------
 #include "CastleUIControls.hpp"
 #include "CastleControls.hpp"
+#include "CastleSoundEngine.hpp"
 //TODO #include "CastleScene.hpp"
 //---------------------------------------------------------------------------
 class TViewMain : public TCastleView
 {
 __published:
-	// Components designed using CGE editor.
-	// These fields will be automatically initialized at Start.
-	TCastleLabel* LabelFps;
-	TCastleButton* Button1;
-	TCastleButton* ButtonMove;
-	TCastleButton* ButtonLoadBunny;
-	//TODO TCastleSound* SoundZap;
-	//TODO TCastleScene* Scene1;
-	//TODO TCastleViewport* Viewport1;
+    // Components designed using CGE editor.
+    // These fields will be automatically initialized at Start.
+    TCastleLabel* LabelFps;
+    TCastleButton* ButtonMakeSound;
+    TCastleButton* ButtonMove;
+    TCastleButton* ButtonLoadBunny;
+    TCastleSound* SoundZap;
+    //TODO TCastleScene* Scene1;
+    //TODO TCastleViewport* Viewport1;
 private:
-	__fastcall void ClickButton1(TObject* Sender);
-	__fastcall void ClickMove(TObject* Sender);
-	__fastcall void ClickBunny(TObject* Sender);
+    __fastcall void ClickMakeSound(TObject* Sender);
+    __fastcall void ClickMove(TObject* Sender);
+    __fastcall void ClickBunny(TObject* Sender);
 public:
-	__fastcall TViewMain(TComponent* Owner);
-	virtual __fastcall void Start();
-	virtual __fastcall void Update(const float SecondsPassed, bool &HandleInput);
-	virtual __fastcall bool Press(const TInputPressRelease Event);
+    __fastcall TViewMain(TComponent* Owner);
+    virtual __fastcall void Start();
+    virtual __fastcall void Update(const float SecondsPassed, bool &HandleInput);
+    virtual __fastcall bool Press(const TInputPressRelease Event);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TViewMain *ViewMain;
