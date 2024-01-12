883caf0e13b4093aadc01fe26ed856b8561bba39
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-01-12 02:13:27 +0100

    TCastleWindow.SetMousePosition on Delphi/Linux
    WIP - mouse reverts to previous location for unknown reason, also FmxMousePosSet should account for widget screen, display

--- src/delphi/castlefmxutils.pas
+++ src/delphi/castlefmxutils.pas
@@ -20,7 +20,7 @@ unit CastleFmxUtils;
 
 interface
 
-uses FMX.Dialogs,
+uses Types, FMX.Dialogs,
   CastleFileFilters;
 
 { Convert file filters into FMX Dialog.Filter, Dialog.FilterIndex.
@@ -43,7 +43,13 @@ procedure FileFiltersToDialog(FFList: TFileFilterList;
   const Dialog: TOpenDialog; const AllFields: boolean = true); overload;
 { @groupEnd }
 
+{$ifdef LINUX}
+procedure FmxSetMousePos(const Point: TPointF);
+{$endif}
+
 implementation
+
+uses CTypes, CastleLog;
 
 procedure FileFiltersToDialog(const FileFilters: string;
   const Dialog: TOpenDialog; const AllFields: boolean = true);
@@ -67,5 +73,42 @@ begin
   Dialog.Filter := OutFilter;
   Dialog.FilterIndex := OutFilterIndex;
 end;
+
+{$ifdef LINUX}
+type
+  PGdkDevice = Pointer;
+  PGdkScreen = Pointer;
+  PGdkDisplay = Pointer;
+  PGdkDeviceManager = Pointer;
+
+procedure gdk_device_warp(Device: PGdkDevice; Screen: PGdkScreen; X, Y: CInt); cdecl; external 'libgtk-3.so.0';
+
+function gdk_display_get_default: PGdkDisplay; cdecl; external 'libgtk-3.so.0';
+function gdk_display_get_device_manager(display: PGdkDisplay): PGdkDeviceManager; cdecl; external 'libgtk-3.so.0';
+function gdk_device_manager_get_client_pointer(manager: PGdkDeviceManager): PGdkDevice; cdecl; external 'libgtk-3.so.0';
+
+function gdk_screen_get_default: PGdkScreen; cdecl; external 'libgtk-3.so.0';
+
+procedure FmxSetMousePos(const Point: TPointF);
+var
+  Display: PGdkDisplay;
+  DeviceManager: PGdkDeviceManager;
+  Device: PGdkDevice;
+  Screen: PGdkScreen;
+begin
+  // TODO: use Display, Screen of my widget
+  // Maybe move this to _gtk3.inc include
+
+  // Following https://stackoverflow.com/questions/24844489/how-to-use-gdk-device-get-position
+  Display := gdk_display_get_default;
+  DeviceManager := gdk_display_get_device_manager(Display);
+  Device := gdk_device_manager_get_client_pointer(DeviceManager);
+
+  Screen := gdk_screen_get_default;
+
+  //WritelnLog('Mouse', 'Warping mouse to %f %f', [Point.X, Point.Y]);
+  gdk_device_warp(Device, Screen, Round(Point.X), Round(Point.Y));
+end;
+{$endif}
 
 end.
