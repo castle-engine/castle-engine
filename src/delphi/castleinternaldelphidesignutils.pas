fc2a38e8222dbdd25c2f7f0ae6d6b2dda2ad5e58
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-01-30 10:41:26 +0100

    Proper 4 CGE menu items

--- src/delphi/castleinternaldelphidesignutils.pas
+++ src/delphi/castleinternaldelphidesignutils.pas
@@ -24,6 +24,25 @@ procedure Register;
 
 implementation
 
+{ Documentation:
+
+  Embarcadero:
+  https://docwiki.embarcadero.com/RADStudio/Athens/en/Extending_the_IDE_Using_the_Tools_API
+  https://docwiki.embarcadero.com/RADStudio/Athens/en/Adding_an_Item_to_the_Main_Menu_of_the_IDE
+  https://docwiki.embarcadero.com/RADStudio/Athens/en/Creating_or_Extending_a_Package_to_Use_the_Tools_API
+  https://docwiki.embarcadero.com/RADStudio/Athens/en/Obtaining_Tools_API_Services
+
+  Others:
+  https://www.gexperts.org/open-tools-api-faq/#menuitem
+    Note: Do not actually search by menu item Caption, because IDE may be localized,
+    instead hardcode the Name.
+    Embarcadero docs say so.
+  https://www.davidghoyle.co.uk/WordPress/?p=777
+
+  See sources, e.g.
+  C:\Program Files (x86)\Embarcadero\Studio\22.0\source\ToolsAPI\ToolsAPI.pas
+}
+
 uses SysUtils, Classes,
   ToolsAPI, // design-time only unit
   Vcl.Menus, Vcl.Dialogs,
@@ -39,7 +58,10 @@ end;
 type
   TCastleDelphiIdeIntegration = class(TComponent)
   strict private
-    procedure TestMenuItemClick(Sender: TObject);
+    procedure ClickChangeEnginePath(Sender: TObject);
+    procedure ClickOpenEditor(Sender: TObject);
+    procedure ClickAddPaths(Sender: TObject);
+    procedure ClickRemovePaths(Sender: TObject);
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -48,28 +70,55 @@ type
 constructor TCastleDelphiIdeIntegration.Create(AOwner: TComponent);
 var
   Services: INTAServices;
-  MainMenu: TMainMenu;
-  TestMenuItem: TMenuItem;
+  CgeMenu, ChangeEnginePathMenu, OpenEditorMenu, AddPathsMenu, RemovePathsMenu: TMenuItem;
 begin
   inherited ;
   Services := BorlandIDEServices as INTAServices;
-  MainMenu := Services.MainMenu;
 
-  // TODO: Work in progress to add menu in proper place
-  // TestMenuItem := TMenuItem.Create(Self);
-  // TestMenuItem.Caption := 'Castle Test';
-  // TestMenuItem.OnClick := TestMenuItemClick;
+  ChangeEnginePathMenu := TMenuItem.Create(Self);
+  ChangeEnginePathMenu.Caption := 'Change Engine Path...';
+  // Configure the path of Castle Game Engine (should have subdirectories like src, examples).
+  ChangeEnginePathMenu.OnClick := ClickChangeEnginePath;
 
-  // MainMenu.Items.Insert(0, TestMenuItem);
+  OpenEditorMenu := TMenuItem.Create(Self);
+  OpenEditorMenu.Caption := 'Open Editor';
+  OpenEditorMenu.OnClick := ClickOpenEditor;
+
+  AddPathsMenu := TMenuItem.Create(Self);
+  AddPathsMenu.Caption := 'Configure Currrent Project to Use Engine';
+  AddPathsMenu.OnClick := ClickAddPaths;
+
+  RemovePathsMenu := TMenuItem.Create(Self);
+  RemovePathsMenu.Caption := 'Remove Engine Configuration from the Currrent Project';
+  RemovePathsMenu.OnClick := ClickRemovePaths;
+
+  CgeMenu := TMenuItem.Create(Self);
+  CgeMenu.Caption := 'Castle Game Engine';
+  CgeMenu.Add(ChangeEnginePathMenu);
+  CgeMenu.Add(OpenEditorMenu);
+  CgeMenu.Add(AddPathsMenu);
+  CgeMenu.Add(RemovePathsMenu);
+
+  // Use hardcoded 'ToolsMenu', this is good acoording to
+  // https://docwiki.embarcadero.com/RADStudio/Athens/en/Adding_an_Item_to_the_Main_Menu_of_the_IDE
+  Services.AddActionMenu('ToolsMenu', nil, CgeMenu, true, true);
 end;
 
 destructor TCastleDelphiIdeIntegration.Destroy;
 begin
-  // will automatically free TestMenuItem owned by this
+  // will automatically free menu items owned by this
   inherited;
 end;
 
-procedure TCastleDelphiIdeIntegration.TestMenuItemClick(Sender: TObject);
+procedure TCastleDelphiIdeIntegration.ClickChangeEnginePath(Sender: TObject);
+begin
+end;
+
+procedure TCastleDelphiIdeIntegration.ClickOpenEditor(Sender: TObject);
+begin
+end;
+
+procedure TCastleDelphiIdeIntegration.ClickAddPaths(Sender: TObject);
 var
   OptionName: TOTAOptionName;
   ProjectOptions: IOTAProjectOptions;
@@ -103,6 +152,10 @@ begin
       ReportFileName
     ]));
   finally FreeAndNil(Report) end;
+end;
+
+procedure TCastleDelphiIdeIntegration.ClickRemovePaths(Sender: TObject);
+begin
 end;
 
 { initialization / finalization ---------------------------------------------- }
