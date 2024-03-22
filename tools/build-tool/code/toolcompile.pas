c687039b7b0e2fc1f44a5f778b56cd989c067dec
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-03-22 04:48:37 +0000

    Fix iOS builds crashing in DEBUG mode

--- tools/build-tool/code/toolcompile.pas
+++ tools/build-tool/code/toolcompile.pas
@@ -683,6 +683,11 @@ begin
           FpcOptions.Add('-g');
           FpcOptions.Add('-gl');
           FpcOptions.Add('-dDEBUG');
+          { Disable -Ct (Stack checking) added to fpc.cfg in default
+            fpcupdeluxe installation when DEBUG is defined.
+            Because it crashes when application is run on iPhone. }
+          if IsIOS then
+            FpcOptions.Add('-Ct-');
         end;
       {$ifndef COMPILER_CASE_ANALYSIS}
       else raise EInternalError.Create('CompileFpc: Mode?');
