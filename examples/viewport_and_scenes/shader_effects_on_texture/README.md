f40aa7d9f18b17a87acb2b57537c2cf7b859a2fb
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-06-30 01:32:24 +0200

    Example of shader effect on a texture

---
+++ examples/viewport_and_scenes/shader_effects_on_texture/README.md
@@ -0,0 +1,27 @@
+# Shader Effects On Texture
+
+Example demonstrating how to apply shader effects on a texture.
+
+_Shader effects_ are a technique in _Castle Game Engine_ to add a shader code (using GLSL, _OpenGL Shading Language_) to something, to modify its appearance. The shader code defines a special function `PLUG_xxx` which is then called from the engine to apply the effect.
+
+For more information about shader effects see:
+
+- https://castle-engine.io/compositing_shaders.php
+- https://castle-engine.io/compositing_shaders_doc/html/
+- and the important example [examples/viewport_and_scenes/shader_effects](https://github.com/castle-engine/castle-engine/tree/master/examples/viewport_and_scenes/shader_effects).
+
+This particular example focuses on a special kind of "shader effect" that you can add to a texture node in _Castle Game Engine_. Such effect changes the effective texture RGBA (color, opacity) read by the engine. You can use it to apply various effects to the texture, like color correction, grayscale, sepia, manipulate effective alpha (used for blending) etc.
+
+Using [Castle Game Engine](https://castle-engine.io/).
+
+## Building
+
+Compile by:
+
+- [CGE editor](https://castle-engine.io/editor). Just use menu items _"Compile"_ or _"Compile And Run"_.
+
+- Or use [CGE command-line build tool](https://castle-engine.io/build_tool). Run `castle-engine compile` in this directory.
+
+- Or use [Lazarus](https://www.lazarus-ide.org/). Open in Lazarus `shader_effects_on_texture_standalone.lpi` file and compile / run from Lazarus. Make sure to first register [CGE Lazarus packages](https://castle-engine.io/lazarus).
+
+- Or use [Delphi](https://www.embarcadero.com/products/Delphi). Open in Delphi `shader_effects_on_texture_standalone.dproj` file and compile / run from Delphi. See [CGE and Delphi](https://castle-engine.io/delphi) documentation for details.
---
+++ examples/viewport_and_scenes/shader_effects_on_texture/code/gameviewmain.pas
@@ -0,0 +1,121 @@
+{ Main view, where most of the application logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  This template code is in public domain, unlike most other CGE code which
+  is covered by BSD or LGPL (see https://castle-engine.io/license). }
+unit GameViewMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleComponentSerialize, CastleTimeUtils, X3DNodes,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleScene,
+  CastleTransform;
+
+type
+  { Main view, where most of the application logic takes place. }
+  TViewMain = class(TCastleView)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    ScenesParent: TCastleTransform;
+    SceneTexturedSampleThing2, SceneTexturedSampleThing3: TCastleScene;
+  private
+    WorldTime: TFloatTime;
+    { Field needed only to pass information
+      from SceneAddTextureEffect to NodeAddTextureEffect. }
+    EffectShaderUrl: String;
+    procedure NodeAddTextureEffect(Node: TX3DNode);
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+  end;
+
+var
+  ViewMain: TViewMain;
+
+implementation
+
+uses SysUtils,
+  CastleRenderOptions;
+
+{ TViewMain ----------------------------------------------------------------- }
+
+constructor TViewMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+end;
+
+procedure TViewMain.NodeAddTextureEffect(Node: TX3DNode);
+var
+  TextureNode: TAbstractSingleTextureNode;
+  Effect: TEffectNode;
+  EffectPart: TEffectPartNode;
+begin
+  // can be safely cast, as we enumerate only TAbstractSingleTextureNode
+  TextureNode := Node as TAbstractSingleTextureNode;
+
+  Effect := TEffectNode.Create;
+  Effect.Language := slGLSL;
+
+  { Add TEffectPartNode which actually contains shader code.
+    You can load shader code from file,
+    you could also set it explicitly from string using EffectPart.Contents := '...'; }
+  EffectPart := TEffectPartNode.Create;
+  EffectPart.ShaderType := stFragment;
+  EffectPart.SetUrl([EffectShaderUrl]);
+  Effect.SetParts([EffectPart]);
+
+  { This applies the effect to the specific texture,
+    which allows PLUG_texture_color to be available
+    ( see https://castle-engine.io/compositing_shaders_doc/html/section.fragment_plugs.html ). }
+  TextureNode.SetEffects([Effect]);
+end;
+
+procedure TViewMain.Start;
+
+  { Add a shader effect that modifies effective texture RGBA. }
+  procedure SceneAddTextureEffect(const Scene: TCastleScene;
+    const AnEffectShaderUrl: String);
+  var
+    SavedAnimation: String;
+  begin
+    if Scene.RootNode <> nil then
+    begin
+      SavedAnimation := Scene.AutoAnimation;
+
+      EffectShaderUrl := AnEffectShaderUrl;
+      Scene.RootNode.EnumerateNodes(TAbstractSingleTextureNode,
+        {$ifdef FPC}@{$endif} NodeAddTextureEffect, false);
+
+      { Adding node to Scene turns off animation set by AutoAnimation, run it again.
+        The need to do this may disappear in future CGE versions. }
+      if SavedAnimation <> '' then
+        Scene.PlayAnimation(SavedAnimation, true);
+    end;
+  end;
+
+begin
+  inherited;
+  SceneAddTextureEffect(SceneTexturedSampleThing2, 'castle-data:/shaders/texture_effect_sepia.fs');
+  SceneAddTextureEffect(SceneTexturedSampleThing3, 'castle-data:/shaders/texture_effect_invert.fs');
+end;
+
+procedure TViewMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+
+  { Rotate all 3 scenes (by single parent ScenesParent) around X axis,
+    just to show some interesting lighting. }
+  WorldTime := WorldTime + SecondsPassed;
+  ScenesParent.Rotation := Vector4(1, 0, 0, WorldTime);
+end;
+
+end.
