cbf6c5d1189f6e55086d67a627e3ee36ec086a98
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-07-27 07:58:49 +0200

    Save pictures using modern API on Android Q

--- tools/build-tool/data/android/integrated-services/photo_service/app/src/main/java/io/castleengine/ServicePhotoService.java
+++ tools/build-tool/data/android/integrated-services/photo_service/app/src/main/java/io/castleengine/ServicePhotoService.java
@@ -21,11 +21,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
-import android.widget.Toast;
+//import android.widget.Toast;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.provider.MediaStore;
+import android.os.Build;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -46,6 +51,62 @@ public class ServicePhotoService extends ServiceAbstract
     {
         super(activity);
     }
+
+    /* Add image to MediaStore using Android Q API.
+       See
+       https://stackoverflow.com/questions/57726896/mediastore-images-media-insertimage-deprecated
+       https://developer.android.com/training/data-storage/shared/media
+    */
+    private void imageSaveQ(String sourceImagePath)
+        throws IOException, MalformedURLException
+    {
+        logInfo(CATEGORY, "Saving image using modern (>= Q) API " + sourceImagePath);
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, getFileBaseName(sourceImagePath));
+        values.put(MediaStore.MediaColumns.DATA, sourceImagePath);
+        values.put(MediaStore.MediaColumns.IS_PENDING, 1);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        Uri newUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        OutputStream outputStream = resolver.openOutputStream(newUri);
+        URL srcImageUrl = new URL(sourceImagePath);
+        copyUrlToStream(srcImageUrl, outputStream);
+
+        values.clear();
+        values.put(MediaStore.MediaColumns.IS_PENDING, 0);
+        resolver.update(newUri, values, null, null);
+    }
+
+    /* Add image using deprecated API. */
+    @SuppressWarnings("deprecation")
+    private void imageSaveDeprecated(String sourceImagePath)
+        throws IOException
+    {
+        URL srcImageUrl = new URL(sourceImagePath);
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "Image_" + timeStamp + "." + getFileExt(sourceImagePath);
+
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File dstImage = new File(storageDir, imageFileName);
+
+        // Make sure the Pictures directory exists.
+        storageDir.mkdirs();
+
+        //String dstPhotoPath = dstImage.getAbsolutePath();
+
+        copyUrlToFile(srcImageUrl, dstImage);
+
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(dstImage);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+
+        // let application do notification
+        //Toast.makeText(getActivity(), "Image saved", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean messageReceived(String[] parts)
     {
@@ -53,40 +114,28 @@ public class ServicePhotoService extends ServiceAbstract
         {
             String sourceImagePath = parts[1];
             try {
-                URL srcImageUrl = new URL(sourceImagePath);
-
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-                String imageFileName = "Image_" + timeStamp + "." + getFileExt(sourceImagePath);
-
-                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                File dstImage = new File(storageDir, imageFileName);
-
-                // Make sure the Pictures directory exists.
-                storageDir.mkdirs();
-
-                //String dstPhotoPath = dstImage.getAbsolutePath();
-
-                copyUrlToFile(srcImageUrl, dstImage);
-
-                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                Uri contentUri = Uri.fromFile(dstImage);
-                mediaScanIntent.setData(contentUri);
-                getActivity().sendBroadcast(mediaScanIntent);
-
-                Toast.makeText(getActivity(), "Image saved", Toast.LENGTH_SHORT).show();
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    imageSaveQ(sourceImagePath);
+                } else {
+                    imageSaveDeprecated(sourceImagePath);
+                }
             } catch (Exception e) {
                 logWarning(CATEGORY, "Error writing screenshot: " + e.toString());
             }
-
             return true;
         }
         else
             return false;
     }
 
-    private static String getFileExt(String fileName) {
+    private static String getFileExt(String fileName)
+    {
         return fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+    }
+
+    private static String getFileBaseName(String fileName)
+    {
+        return fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
     }
 
     /*private static void copyFile(File src, File dst) throws IOException {
@@ -108,13 +157,13 @@ public class ServicePhotoService extends ServiceAbstract
         }
     }*/
 
-    private static void copyUrlToFile(URL src, File dst) throws IOException {
+    private static void copyUrlToStream(URL src, OutputStream out) throws IOException
+    {
         InputStream in = src.openStream();
         try {
-            OutputStream out = new FileOutputStream(dst);
             try {
                 // Transfer bytes from in to out
-                byte[] buf = new byte[1024];
+                byte[] buf = new byte[1024 * 1024];
                 int len;
                 while ((len = in.read(buf)) > 0) {
                     out.write(buf, 0, len);
@@ -125,5 +174,10 @@ public class ServicePhotoService extends ServiceAbstract
         } finally {
             in.close();
         }
+    }
+
+    private static void copyUrlToFile(URL src, File dst) throws IOException
+    {
+        copyUrlToStream(src, new FileOutputStream(dst));
     }
 }
