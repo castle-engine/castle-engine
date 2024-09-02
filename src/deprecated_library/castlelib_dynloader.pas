19b64d6d0376064bf5c04629bb132b6fc5a2c065
Jan Adamec <jadamec@gmail.com>
2024-09-02 08:46:35 +0200

    Add function to get Castle Engine version to library interface

--- src/deprecated_library/castlelib_dynloader.pas
+++ src/deprecated_library/castlelib_dynloader.pas
@@ -1,5 +1,5 @@
 {
-  Copyright 2008-2023 Jan Adamec, Michalis Kamburelis.
+  Copyright 2008-2024 Jan Adamec, Michalis Kamburelis.
 
   This file is part of "Castle Game Engine".
 
@@ -263,6 +263,7 @@ procedure CGE_Finalize(); cdecl; external 'castleengine';
 procedure CGE_Open(flags: cUInt32; InitialWidth, InitialHeight, Dpi: cUInt32); cdecl; external 'castleengine';
 procedure CGE_Close(); cdecl; external 'castleengine';
 procedure CGE_GetOpenGLInformation(szBuffer: pchar; nBufSize: cInt32); cdecl; external 'castleengine';
+procedure CGE_GetCastleEngineVersion(szBuffer: pchar; nBufSize: cInt32); cdecl; external 'castleengine';
 procedure CGE_Resize(uiViewWidth, uiViewHeight: cUInt32); cdecl; external 'castleengine';
 procedure CGE_Render(); cdecl; external 'castleengine';
 procedure CGE_SaveScreenshotToFile(szFile: pcchar); cdecl; external 'castleengine';
