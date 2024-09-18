8f04521610668295090c3fa2094c1c48f69ff734
Jan Adamec <jadamec@gmail.com>
2024-09-18 14:07:24 +0200

    Remove macos compile script for deprecated_library, as long as mac_universal_binary attribute remains in Manifest.xml

--- src/deprecated_library/castleengine_compile_macos.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# ----------------------------------------------------------------------------
-# Compile the library as universal binary for macOS (x86_64 and arm64 slices).
-# ----------------------------------------------------------------------------
-
-castle-engine compile --target macos
