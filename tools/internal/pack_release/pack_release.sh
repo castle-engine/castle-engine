49c18ab49f54d241ea3aae6dd475850f156e6879
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-11-06 23:54:51 +0100

    CASTLE_PACK_DISABLE_LAZARUS_VERSION_CHECK

--- tools/internal/pack_release/pack_release.sh
+++ tools/internal/pack_release/pack_release.sh
@@ -105,23 +105,25 @@ check_lazarus_version ()
   local LAZARUS_VERSION=`lazbuild --version | grep --invert-match 'using config file' | tr -d '\r'`
   echo "Lazarus version: ${LAZARUS_VERSION}"
 
-  # Note that we have to support Lazarus 3.0,
-  # since it's the last supported by https://github.com/gcarreno/setup-lazarus for now,
-  # see https://github.com/gcarreno/setup-lazarus/issues/30 .
-  if [ "${LAZARUS_VERSION}" '!=' '3.0' -a \
-       "${LAZARUS_VERSION}" '!=' '3.2' -a \
-       "${LAZARUS_VERSION}" '!=' '3.4' -a \
-       "${LAZARUS_VERSION}" '!=' '3.5' ]; then
-    echo "pack_release: Incorrect Lazarus version to pack release, see ${LAZARUS_VERSION}"
-    exit 1
-  fi
-
-  # To avoid https://gitlab.com/freepascal.org/lazarus/lazarus/-/merge_requests/291
-  # we need Lazarus 3.5 on macOS.
-  if [ "`uname -s`" '=' 'Darwin' ]; then
-    if [ "${LAZARUS_VERSION}" '!=' '3.5' ]; then
-      echo "pack_release: macOS: Incorrect Lazarus version to pack release, see ${LAZARUS_VERSION}"
+  if [ "${CASTLE_PACK_DISABLE_LAZARUS_VERSION_CHECK:-}" '!=' 'true' ]; then
+    # Note that we have to support Lazarus 3.0,
+    # since it's the last supported by https://github.com/gcarreno/setup-lazarus for now,
+    # see https://github.com/gcarreno/setup-lazarus/issues/30 .
+    if [ "${LAZARUS_VERSION}" '!=' '3.0' -a \
+        "${LAZARUS_VERSION}" '!=' '3.2' -a \
+        "${LAZARUS_VERSION}" '!=' '3.4' -a \
+        "${LAZARUS_VERSION}" '!=' '3.5' ]; then
+      echo "pack_release: Incorrect Lazarus version to pack release, see ${LAZARUS_VERSION}"
       exit 1
+    fi
+
+    # To avoid https://gitlab.com/freepascal.org/lazarus/lazarus/-/merge_requests/291
+    # we need Lazarus 3.5 on macOS.
+    if [ "`uname -s`" '=' 'Darwin' ]; then
+      if [ "${LAZARUS_VERSION}" '!=' '3.5' ]; then
+        echo "pack_release: macOS: Incorrect Lazarus version to pack release, see ${LAZARUS_VERSION}"
+        exit 1
+      fi
     fi
   fi
 }
