a538d6524b7f8d871db4adfb9635f57703cc7ae0
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-11-15 06:08:30 +0100

    Simplify: remove the unused JS switcher between engine versions at API docs

--- doc/pasdoc/html-parts/body-end.php
+++ doc/pasdoc/html-parts/body-end.php
@@ -2,31 +2,6 @@
 
 require_once 'cge-www-configure.php';
 
-/* Read current CGE version from src/base/castleversion.inc */
-function read_cge_version()
-{
-    $handle = fopen('../../../src/base/castleversion.inc', 'rt');
-    if ($handle == false) {
-        throw new Exception('Error: failed to open castleversion.inc');
-    }
-    $line = fgets($handle);
-    if ($line === false) {
-        throw new Exception('Error: cannot read line using fgets()');
-    }
-    fclose($handle);
-    return trim($line, "\t\n'");
-}
-
-// Right now we only show 7.0-alpha.XXX.snapshot version
-$cgeVersions = array(
-    '7.0-alpha.3.snapshot' => array('url' => CASTLE_PROD_URL . 'apidoc/html/'),
-);
-
-$cgeCurrentVersion = read_cge_version();
-if (!array_key_exists($cgeCurrentVersion, $cgeVersions)) {
-    throw new Exception('Current version ' . $cgeCurrentVersion . ' has no entry in cgeVersions table');
-}
-
 echo '</div> <!-- .pasdoc-container -->';
 
 // end div, just like castle_footer when $castle_sidebar is empty.
@@ -35,21 +10,3 @@ echo '</div> <!-- .pasdoc-container -->';
 echo '</div>';
 
 echo_shared_body_end();
-
-/* Add JavaScript code specially for PasDoc docs. */
-echo "<script type='text/javascript'>
-var cgeCurrentVersion = '$cgeCurrentVersion';
-var cgeVersions = [
-";
-$cgeVersionsStr = '';
-foreach ($cgeVersions as $versionId => $versionInfo) {
-    $versionUrl = $versionInfo['url'];
-    $cgeVersionsStr .= "    {'id': '$versionId', 'url': '$versionUrl'},\n";
-}
-$cgeVersionsStr = trim($cgeVersionsStr, "\n,"); // remove last comma
-echo $cgeVersionsStr . "
-];
-</script>
-";
-?>
-<script type="text/javascript" src="<?php echo page_requisite('cge-pasdoc.js'); ?>"></script>
