1b399edaec23900a2ccaf9c4fae7a0c5306c1e32
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-12-01 23:27:28 +0100

    WebGL context initialization, query info, make animation using requestAnimationFrame, basic Render and Update (next: move this logic to WASM code)

--- tools/build-tool/data/web/program_js.dpr
+++ tools/build-tool/data/web/program_js.dpr
@@ -27,12 +27,143 @@
   See https://castle-engine.io/web for usage docs of engine developers.
   This gets automatically generated and build by
   "castle-engine compile --target=web" tool.
+
+  This also initializes WebGL context and for now
+  (TODO - this should rely on WebAssembly code)
+  does sample rendering, update and animation loop.
 }
 
-uses Classes, SysUtils,
+uses Classes, SysUtils, Math,
   // pas2js-specific units
   WasiEnv, Web, BrowserApp, WebWidget, HtmlWidgets, JS,
-  WebAssembly, WasiHostApp, BrowserConsole;
+  WebAssembly, WasiHostApp, BrowserConsole, WebGL;
+
+{ Rendering and animation using WebGL from Pas2js -------------------------- }
+
+var
+  { Variables set by GLContextOpen, read-only (for now) by other code }
+	GL: TJSWebGLRenderingContext;
+  Canvas: TJSHTMLCanvasElement;
+	ContextWidth, ContextHeight: GLSizeI;
+
+procedure GLContextOpen;
+
+	function StrArrayJoin(const A: TStringDynArray): String;
+	var
+		I: Integer;
+	begin
+		Result := '';
+		for I := 0 to High(A) do
+			Result += A[I] + ', ';
+	end;
+
+begin
+  Canvas := TJSHTMLCanvasElement(document.getElementById('castle-canvas'));
+
+	GL := TJSWebGLRenderingContext(canvas.getContext('webgl'));
+	if GL = nil then
+	  raise Exception.Create('Failed to load WebGL');
+
+	ContextWidth := GL.drawingBufferWidth;
+	ContextHeight := GL.drawingBufferHeight;
+
+	Writeln('Context Width x Height: ', ContextWidth, ' x ', ContextHeight);
+	Writeln('Context basic information:', LineEnding,
+	  '  Version: ', GL.GetParameter(GL.VERSION), LineEnding,
+		'  Shading Language Version: ', GL.GetParameter(GL.SHADING_LANGUAGE_VERSION), LineEnding,
+		'  Renderer: ', GL.GetParameter(GL.RENDERER), LineEnding,
+		'  Vendor: ', GL.GetParameter(GL.VENDOR)
+	);
+	Writeln('Context limits:', LineEnding,
+		'  Max Viewport Dimensions: ', GL.GetParameter(GL.MAX_VIEWPORT_DIMS), LineEnding,
+		'  Max Texture Size: ', GL.GetParameter(GL.MAX_TEXTURE_SIZE), LineEnding,
+		'  Max Texture Image Units: ', GL.GetParameter(GL.MAX_TEXTURE_IMAGE_UNITS), LineEnding,
+		'  Max Vertex Texture Image Units: ', GL.GetParameter(GL.MAX_VERTEX_TEXTURE_IMAGE_UNITS), LineEnding,
+		'  Max Combined Texture Image Units: ', GL.GetParameter(GL.MAX_COMBINED_TEXTURE_IMAGE_UNITS), LineEnding,
+		'  Max Vertex Uniform Vectors: ', GL.GetParameter(GL.MAX_VERTEX_UNIFORM_VECTORS), LineEnding,
+		'  Max Fragment Uniform Vectors: ', GL.GetParameter(GL.MAX_FRAGMENT_UNIFORM_VECTORS), LineEnding,
+		'  Max Varying Vectors: ', GL.GetParameter(GL.MAX_VARYING_VECTORS), LineEnding,
+		'  Max Vertex Attributes: ', GL.GetParameter(GL.MAX_VERTEX_ATTRIBS)
+		// ...
+	);
+	Writeln('Context attributes: ', LineEnding,
+		'  Alpha: ', GL.getContextAttributes.alpha, LineEnding,
+		'  Depth: ', GL.getContextAttributes.depth, LineEnding,
+		'  Stencil: ', GL.getContextAttributes.stencil, LineEnding,
+		'  Antialias: ', GL.getContextAttributes.antialias, LineEnding,
+		'  PremultipliedAlpha: ', GL.getContextAttributes.premultipliedAlpha, LineEnding,
+		'  PreserveDrawingBuffer: ', GL.getContextAttributes.preserveDrawingBuffer, LineEnding,
+		'  PowerPreference: ', GL.getContextAttributes.powerPreference, LineEnding,
+		'  FailIfMajorPerformanceCaveat: ', GL.getContextAttributes.failIfMajorPerformanceCaveat
+	);
+	Writeln('Supported Extensions: ', LineEnding,
+		'  Count: ', Length(GL.getSupportedExtensions), LineEnding,
+		'  List: ', StrArrayJoin(GL.getSupportedExtensions)
+	);
+end;
+
+var
+  LifeTime: Double = 0;
+
+procedure Update(const DeltaTime: Float);
+begin
+  LifeTime += DeltaTime;
+end;
+
+procedure Render;
+var
+  Col: Float;
+begin
+  Col := Frac(LifeTime);
+
+  GL.clearColor(Col, Col, 0.0, 1);
+  GL.viewport(0, 0, ContextWidth, ContextHeight);
+  GL.clear(GL.COLOR_BUFFER_BIT);
+  // GL.flush; // pointless when using requestAnimationFrame, according to MDN
+end;
+
+var
+  CanvasAnimationHandler: Integer = 0;
+  HasPreviousTime: Boolean = False;
+  PreviousTime: TJSDOMHighResTimeStamp;
+
+{ Callback for requestAnimationFrame
+  ( https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame ) }
+procedure AnimationFrame(Time: TJSDOMHighResTimeStamp);
+var
+  DeltaTime: Float;
+begin
+  { TODO: recreate context.
+    See https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/isContextLost }
+	if GL.isContextLost then
+    raise Exception.Create('Context has been lost');
+
+  // calculate DeltaTime, update HasPreviousTime and PreviousTime
+  if HasPreviousTime then
+    DeltaTime := (Time - PreviousTime) / 1000
+  else
+  begin
+    DeltaTime := 1 / 60; // arbitrary initial value
+    HasPreviousTime := True;
+  end;
+  PreviousTime := Time;
+
+  // update and render
+	Update(DeltaTime);
+	Render;
+
+  // schedule next frame
+	if CanvasAnimationHandler <> 0 then
+		CanvasAnimationHandler := window.requestAnimationFrame(@AnimationFrame);
+end;
+
+procedure RunWebGLAnimation;
+begin
+  GLContextOpen;
+	CanvasAnimationHandler := window.requestAnimationFrame(@AnimationFrame);
+end;
+
+{ Main program -------------------------------------------------------------- }
 
 type
   TMyApplication = class(TWASIHostApplication)
@@ -65,6 +196,8 @@ procedure TMyApplication.DoRun;
 begin
   WriteLn('Starting WebAssembly program from ${EXECUTABLE_NAME}.wasm');
   StartWebAssembly('${EXECUTABLE_NAME}.wasm');
+  RunWebGLAnimation;
+
   { Terminate doesn't really do anything we critically need
     (Application.Run ends anyway), but makes it clear that we're done by setting
     Terminated = true. }
@@ -84,7 +217,7 @@ begin
   Application.Initialize; // doesn't do anything by default
   Application.Run;
 
-  WriteLn('Pas2js program finished (all the control is now in WebAssembly).');
+  WriteLn('Normal flow of Pas2js program finished (though registered callbacks, like requestAnimationFrame and when WASM accesses environment exposed by JS, may still run).');
 
   { Important: *Do not* free Application instance here!
     It would make WebAssembly program fail, as it tries to acccess
