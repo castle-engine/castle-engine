8e9e91e875f90d453a52b63e99a725119783e205
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-01-23 14:25:25 +0100

    Saving IFC started, next: fill TIfcBuildingElementProxy

--- src/scene/load/ifc/castleifc.pas
+++ src/scene/load/ifc/castleifc.pas
@@ -32,14 +32,16 @@ type
   {$I castleifc_ifc_types.inc}
   {$I castleifc_ifc_standard_types.inc}
   {$I castleifc_json.inc}
-  {$I castleifc_x3d.inc}
+  {$I castleifc_load_to_x3d.inc}
+  {$I castleifc_save_from_x3d.inc}
   {$undef read_interface_types}
 
 {$define read_interface}
 {$I castleifc_ifc_types.inc}
 {$I castleifc_ifc_standard_types.inc}
 {$I castleifc_json.inc}
-{$I castleifc_x3d.inc}
+{$I castleifc_load_to_x3d.inc}
+{$I castleifc_save_from_x3d.inc}
 {$undef read_interface}
 
 implementation
@@ -52,62 +54,18 @@ uses TypInfo, RttiUtils,
 {$I castleifc_ifc_types.inc}
 {$I castleifc_ifc_standard_types.inc}
 {$I castleifc_json.inc}
-{$I castleifc_x3d.inc}
+{$I castleifc_load_to_x3d.inc}
+{$I castleifc_save_from_x3d.inc}
 {$undef read_implementation}
 
-{ Loading -------------------------------------------------------------------- }
-
-{ Main routine that converts IFC -> X3D nodes, doing most of the work. }
-function LoadIfc(const Stream: TStream; const BaseUrl: String): TX3DRootNode;
-
-  {.$define DEBUG_IFC_ROUND_TRIP}
-
-  {$ifdef DEBUG_IFC_ROUND_TRIP}
-  { Only for debugging purposes: save back the IFC data to JSON.
-    This is an easy test whether IfcJsonLoad understood everything correctly,
-    and whether IfcJsonSave saves everything correctly. }
-  procedure DebugSaveBack(const IfcFile: TIfcFile);
-  var
-    JsonObj: TJsonObject;
-    JsonString: String;
-    Stream: TStream;
-  begin
-    Stream := UrlSaveStream(DeleteUriExt(BaseUrl) + '-debug-roundtrip.ifcjson');
-    try
-      JsonObj := IfcJsonSave(IfcFile);
-      try
-        JsonString := JsonObj.FormatJSON;
-        WriteStr(Stream, JsonString);
-      finally FreeAndNil(JsonObj) end;
-    finally FreeAndNil(Stream) end;
-  end;
-  {$endif DEBUG_IFC_ROUND_TRIP}
-
-var
-  JsonParser: TJsonParser;
-  Json: TJsonData;
-  IfcFile: TIfcFile;
-begin
-  JsonParser := TJsonParser.Create(Stream, [joComments]);
-  try
-    Json := JsonParser.Parse;
-    try
-      IfcFile := IfcJsonLoad(Json);
-      try
-        Result := IfcToX3D(IfcFile, BaseUrl);
-        {$ifdef DEBUG_IFC_ROUND_TRIP}
-        DebugSaveBack(IfcFile);
-        {$endif DEBUG_IFC_ROUND_TRIP}
-      finally FreeAndNil(IfcFile) end;
-    finally FreeAndNil(Json) end;
-  finally FreeAndNil(JsonParser) end;
-end;
+{ TModelFormat registration -------------------------------------------------- }
 
 var
   ModelFormat: TModelFormat;
 initialization
   ModelFormat := TModelFormat.Create;
   ModelFormat.OnLoad := {$ifdef FPC}@{$endif} LoadIfc;
+  ModelFormat.OnSave := {$ifdef FPC}@{$endif} SaveIfc;
 
   // These are own own MIME types, as specs don't define any MIME type for IFC.
   // https://technical.buildingsmart.org/standards/ifc/ifc-formats/
