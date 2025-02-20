48f9d7e377e40a3a1ee8c26dbef869e301bdacfa
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-02-20 16:50:49 +0100

    List types in IFC XSD

--- tools/internal/generate_ifc/generate_ifc.dpr
+++ tools/internal/generate_ifc/generate_ifc.dpr
@@ -14,9 +14,9 @@
 }
 { Generate Pascal code IFC API. See README.md for details. }
 
-uses SysUtils, Classes,
+uses SysUtils, Classes, Dom, XmlRead,
   {$ifdef FPC} OpenSSLSockets, {$endif} // support HTTPS
-  CastleDownload, CastleXmlUtils;
+  CastleClassUtils, CastleDownload, CastleXmlUtils;
 
 const
   SchemaUrl = 'https://standards.buildingsmart.org/IFC/RELEASE/IFC4_3/HTML/IFC4X3_ADD2.xsd';
@@ -39,8 +39,69 @@ begin
   end;
 end;
 
+procedure HandleSimpleType(Node: TDomElement);
+begin
+  Writeln('Simple type: ', Node.AttributeString('name'));
+end;
+
+procedure HandleComplexType(Node: TDomElement);
+begin
+  Writeln('Complex type: ', Node.AttributeString('name'));
+end;
+
+procedure HandleOtherType(Node: TDomElement);
+begin
+  Writeln('Other type: ', Node.AttributeString('name'));
+end;
+
+{ Just like UrlReadXml, but forces encoding from ASCII to UTF-8.
+  Reason: IFC XSD uses ASCII encoding,
+
+    <?xml version='1.0' encoding='ASCII'?>
+
+  which causes
+
+    Encoding 'ASCII' is not supported (in file "IFC4X3_ADD2.xsd")
+
+  error from FPC XMLRead (because TXMLDecodingSource.SetEncoding
+  does not support ASCII). }
+function UrlReadXml_ForceEncodingUtf8(const Url: String): TXmlDocument;
+var
+  Stream: TStream;
+  Str: String;
+  NewStream: TStringStream;
+begin
+  Stream := Download(Url);
+  try
+    Str := ReadGrowingStreamToDefaultString(Stream);
+    Str := StringReplace(Str, 'encoding=''ASCII''', 'encoding=''UTF-8''', []);
+    NewStream := TStringStream.Create(Str);
+    try
+      ReadXmlFile(Result, NewStream);
+    finally FreeAndNil(NewStream) end;
+  finally FreeAndNil(Stream) end;
+end;
+
+var
+  Doc: TXmlDocument;
+  I: TXMLElementIterator;
 begin
   DownloadSchema;
 
-
+  Doc := UrlReadXml_ForceEncodingUtf8(SchemaFile);
+  try
+    I := Doc.DocumentElement.ChildrenIterator;
+    try
+      while I.GetNext do
+      begin
+        if I.Current.TagName8 = 'xs:simpleType' then
+          HandleSimpleType(I.Current)
+        else
+        if I.Current.TagName8 = 'xs:complexType' then
+          HandleComplexType(I.Current)
+        else
+          HandleOtherType(I.Current);
+      end;
+    finally FreeAndNil(I) end;
+  finally FreeAndNil(Doc) end;
 end.
