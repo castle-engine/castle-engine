b84c02b8681dbdddf8a554e5a85bdb4058ed4850
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-03-03 16:30:23 +0100

    Workarouds for some shaders that fail on web -- for now, disable some stuff, to run demos

--- src/scene/castlerendererinternalshader.pas
+++ src/scene/castlerendererinternalshader.pas
@@ -1911,6 +1911,14 @@ var
   begin
     if FFogEnabled then
     begin
+      {$ifdef CASTLE_WEBGL}
+      { Shader fails compilation with
+          EGLSLShaderCompileError: Vertex shader not compiled:
+          0(13) : error C1503: undefined variable "_ucastle_FogFragCoord" }
+      WritelnWarning('WebGL', 'Fog is not supported with WebGL yet');
+      Exit;
+      {$endif}
+
       case FFogCoordinateSource of
         fcDepth           : CoordinateSource := '-vertex_eye.z';
         fcPassedCoordinate: CoordinateSource := 'castle_FogCoord';
