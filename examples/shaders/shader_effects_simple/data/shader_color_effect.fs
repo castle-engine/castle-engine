c2fdc2c951c91de408c80c84d3072aa75a34492d
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-05-27 06:42:58 +0200

    "more red" -> just red

--- examples/shaders/shader_effects_simple/data/shader_color_effect.fs
+++ examples/shaders/shader_effects_simple/data/shader_color_effect.fs
@@ -2,7 +2,7 @@
 // See https://castle-engine.io/shaders .
 void PLUG_fragment_modify(inout vec4 fragment_color)
 {
-  // Make the color "more red" by setting
+  // Make the color red by setting
   // - the red color channel to the overall intensity
   // - the green and blue channels to zero.
   float intensity = (fragment_color.r + fragment_color.g + fragment_color.b) / 3.0;
@@ -21,7 +21,7 @@ uniform float red_intensity;
 // Modify the fragment color.
 void PLUG_fragment_modify(inout vec4 fragment_color)
 {
-  // Make the color "more red" by setting
+  // Make the color red by setting
   // - the red color channel to the overall intensity
   // - the green and blue channels to zero.
   float intensity = (fragment_color.r + fragment_color.g + fragment_color.b) / 3.0;
