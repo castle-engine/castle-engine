7bb730650cbb32cb0b33b2c04ed7d10987fcda9b
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-05-29 18:25:53 +0200

    Fix TPhotoService to handle castle-config protocol

--- src/services/castlephotoservice.pas
+++ src/services/castlephotoservice.pas
@@ -26,23 +26,56 @@ type
   { TPhotoService enables to store/retrieve images to/from the system Photos
     app. It may also request user to take a new photo with the camera.
 
-    At the moment, only storing images is implemented.
-    }
+    At the moment, only storing images is implemented. }
   TPhotoService = class(TComponent)
   public
-    class procedure StoreImage(APath: string);
+    class procedure StoreImage(const ImageUrl: string);
   end;
 
 implementation
 
-uses CastleMessaging, CastleUriUtils;
+uses CastleMessaging, CastleUriUtils, CastleLog;
 
-class procedure TPhotoService.StoreImage(APath: string);
+class procedure TPhotoService.StoreImage(const ImageUrl: string);
+var
+  ResolvedUrl: String;
+  {$if defined(ANDROID) or defined(CASTLE_IOS)}
+  ResolvedFileName: String;
+  {$endif}
 begin
+  {$if defined(ANDROID) or defined(CASTLE_IOS)}
+  { Android ( ServicePhotoService.java ) and iOS ( PhotoService.m )
+    services handle URLs, but not Castle Game Engine URLs with protocols
+    like "castle-config", only URLs with standard protocols like "file".
+
+    So resolve them to a filename and back, knowing that 'castle-config:/'
+    is always just resolved to regular files on Android and iOS.
+    This means doing funny UriToFilenameSafe + FilenameToUriSafe.
+
+    Note: When ApplicationConfigOverride was set, we just cannot
+    guarantee that TPhotoService works in the current approach.
+
+    Note: Alternative URL conversion would use ResolveCastleConfigUrl,
+    though then we need to list all possible CGE-specific protocols,
+    which can be application specific (if user registers TCastleZip
+    or TCastleMemoryFileSystem).
+    See https://castle-engine.io/url .
+  }
+  ResolvedFileName := UriToFilenameSafe(ImageUrl);
+  if ResolvedFileName = '' then
+  begin
+    WritelnWarning('TPhotoService.StoreImage: Unable to resolve ImageUrl "%s" to a filename, which we need for Android and iOS.', [ImageUrl]);
+    Exit;
+  end;
+  ResolvedUrl := FilenameToUriSafe(ResolvedFileName);
+  {$else}
+  ResolvedUrl := ImageUrl;
+  {$endif}
+
   Messaging.Send([
     'photoservice-store-image',
-    APath,
-    UriMimeType(APath)
+    ResolvedUrl,
+    UriMimeType(ResolvedUrl)
   ]);
 end;
 
