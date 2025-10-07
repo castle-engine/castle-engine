4a216fd9257886baa45f5850a2d40fa5473461d8
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-10-07 22:30:06 +0200

    Test script to force textures to be 512x512

---
+++ examples/3d_games/walking_adventure/data/smaller_textures.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+echo '----------------------------------------------------------------------'
+echo 'Before:'
+
+find . \
+  -type f \
+  '(' -iname '*.png' -or -iname '*.jpg' ')' \
+  '(' -not -ipath '*castle_game_engine_icon.png' ')' \
+  '(' -not -ipath '*/skybox/*' ')' \
+  -exec identify -format '%w %h %f\n' {} \;
+
+echo '----------------------------------------------------------------------'
+echo 'Converting'
+
+find . \
+  -type f \
+  '(' -iname '*.png' -or -iname '*.jpg' ')' \
+  '(' -not -ipath '*castle_game_engine_icon.png' ')' \
+  '(' -not -ipath '*/skybox/*' ')' \
+  -exec convert {} -resize 512x512 {} \;
+
+echo '----------------------------------------------------------------------'
+echo 'After:'
+
+find . \
+  -type f \
+  '(' -iname '*.png' -or -iname '*.jpg' ')' \
+  '(' -not -ipath '*castle_game_engine_icon.png' ')' \
+  '(' -not -ipath '*/skybox/*' ')' \
+  -exec identify -format '%w %h %f\n' {} \;
