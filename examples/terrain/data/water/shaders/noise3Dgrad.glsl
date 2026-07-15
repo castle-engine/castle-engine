d73ef87a5f3b02df1eca9ea0a7e37006e7bbf766
Michalis Kamburelis <michalis@castle-engine.io>
2026-07-16 01:59:39 +0200

    We need highp for water / noise calculation on WebGL

--- examples/terrain/data/water/shaders/noise3Dgrad.glsl
+++ examples/terrain/data/water/shaders/noise3Dgrad.glsl
@@ -1,5 +1,5 @@
 //
-// Description : Array and textureless GLSL 2D/3D/4D simplex 
+// Description : Array and textureless GLSL 2D/3D/4D simplex
 //               noise functions.
 //      Author : Ian McEwan, Ashima Arts.
 //  Maintainer : stegu
@@ -8,21 +8,26 @@
 //               Distributed under the MIT License. See LICENSE file.
 //               https://github.com/ashima/webgl-noise
 //               https://github.com/stegu/webgl-noise
-// 
+//
+// Adjusted for Castle Game Engine by Michalis Kamburelis, 2026-2026.
+// - We need highp precision for WebGL.
+//   Testcase: otherwise noise for large terrain values gets flat,
+//   resulting in dark flat water surface on examples/terrain/
+//   (on Chrome/Vivaldi, not on Firefox).
 
-vec3 mod289(vec3 x) {
+highp vec3 mod289(highp vec3 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-vec4 mod289(vec4 x) {
+highp vec4 mod289(highp vec4 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-vec4 permute(vec4 x) {
+highp vec4 permute(highp vec4 x) {
      return mod289(((x*34.0)+1.0)*x);
 }
 
-vec4 taylorInvSqrt(vec4 r)
+highp vec4 taylorInvSqrt(highp vec4 r)
 {
   return 1.79284291400159 - 0.85373472095314 * r;
 }
@@ -30,7 +35,7 @@ vec4 taylorInvSqrt(vec4 r)
 float snoise(vec3 v, out vec3 gradient)
 {
   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
-  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+  const highp vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
 
 // First corner
   vec3 i  = floor(v + dot(v, C.yyy) );
@@ -51,10 +56,10 @@ float snoise(vec3 v, out vec3 gradient)
   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
 
 // Permutations
-  i = mod289(i); 
-  vec4 p = permute( permute( permute( 
+  i = mod289(i);
+  highp vec4 p = permute( permute( permute(
              i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
            + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
 
 // Gradients: 7x7 points over a square, mapped onto an octahedron.
@@ -62,26 +67,26 @@ float snoise(vec3 v, out vec3 gradient)
   float n_ = 0.142857142857; // 1.0/7.0
   vec3  ns = n_ * D.wyz - D.xzx;
 
-  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+  highp vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
 
-  vec4 x_ = floor(j * ns.z);
-  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+  highp vec4 x_ = floor(j * ns.z);
+  highp vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
 
-  vec4 x = x_ *ns.x + ns.yyyy;
-  vec4 y = y_ *ns.x + ns.yyyy;
-  vec4 h = 1.0 - abs(x) - abs(y);
+  highp vec4 x = x_ *ns.x + ns.yyyy;
+  highp vec4 y = y_ *ns.x + ns.yyyy;
+  highp vec4 h = 1.0 - abs(x) - abs(y);
 
-  vec4 b0 = vec4( x.xy, y.xy );
-  vec4 b1 = vec4( x.zw, y.zw );
+  highp vec4 b0 = vec4( x.xy, y.xy );
+  highp vec4 b1 = vec4( x.zw, y.zw );
 
-  //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
-  //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
-  vec4 s0 = floor(b0)*2.0 + 1.0;
-  vec4 s1 = floor(b1)*2.0 + 1.0;
-  vec4 sh = -step(h, vec4(0.0));
+  //highp vec4 s0 = highp vec4(lessThan(b0,0.0))*2.0 - 1.0;
+  //highp vec4 s1 = highp vec4(lessThan(b1,0.0))*2.0 - 1.0;
+  highp vec4 s0 = floor(b0)*2.0 + 1.0;
+  highp vec4 s1 = floor(b1)*2.0 + 1.0;
+  highp vec4 sh = -step(h, vec4(0.0));
 
-  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+  highp vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  highp vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
 
   vec3 p0 = vec3(a0.xy,h.x);
   vec3 p1 = vec3(a0.zw,h.y);
@@ -89,20 +94,20 @@ float snoise(vec3 v, out vec3 gradient)
   vec3 p3 = vec3(a1.zw,h.w);
 
 //Normalise gradients
-  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  highp vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
   p3 *= norm.w;
 
 // Mix final noise value
-  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-  vec4 m2 = m * m;
-  vec4 m4 = m2 * m2;
-  vec4 pdotx = vec4(dot(p0,x0), dot(p1,x1), dot(p2,x2), dot(p3,x3));
+  highp vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  highp vec4 m2 = m * m;
+  highp vec4 m4 = m2 * m2;
+  highp vec4 pdotx = vec4(dot(p0,x0), dot(p1,x1), dot(p2,x2), dot(p3,x3));
 
 // Determine noise gradient
-  vec4 temp = m2 * m * pdotx;
+  highp vec4 temp = m2 * m * pdotx;
   gradient = -8.0 * (temp.x * x0 + temp.y * x1 + temp.z * x2 + temp.w * x3);
   gradient += m4.x * p0 + m4.y * p1 + m4.z * p2 + m4.w * p3;
   gradient *= 42.0;
